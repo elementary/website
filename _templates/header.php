7733296a63f0b6760e4168e5a67d4b69d34748a0
Cassidy James Blaede <cassidy@system76.com>
2015-01-12 15:04:00 -0700

    Add Twitter, Google+, and Facebook meta tags; fixes #70

--- _templates/header.php
+++ _templates/header.php
@@ -3,9 +3,21 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
         <meta name="description" content="<?php echo !empty($page['description']) ? $page['description'] : $sitewide['description']; ?>">
         <meta name="author" content="<?php echo !empty($page['author']) ? $page['author'] : $sitewide['author']; ?>">
+
+        <meta name="twitter:card" value="summary">
+        <meta name="twitter:title" value="<?php echo !empty($page['title']) ? $page['title'] : $sitewide['title']; ?>">
+        <meta name="twitter:description" value="<?php echo !empty($page['description']) ? $page['description'] : $sitewide['description']; ?>">
+        <meta name="twitter:site" content="@elementary">
+        <meta name="twitter:creator" content="@elementary">
+
+        <meta property="og:title" content="<?php echo !empty($page['title']) ? $page['title'] : $sitewide['title']; ?>" />
+        <meta property="og:description" content="<?php echo !empty($page['description']) ? $page['description'] : $sitewide['description']; ?>" />
+
         <title><?php echo !empty($page['title']) ? $page['title'] : $sitewide['title']; ?></title>
+
         <link rel="shortcut icon" href="favicon.ico">
         <link rel="stylesheet" type="text/css" media="all" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300">
         <link rel="stylesheet" type="text/css" media="all" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
--- _templates/sitewide.php
+++ _templates/sitewide.php
@@ -1,5 +1,6 @@
 <?php
 
-$sitewide['title'] = 'elementary OS';
+$sitewide['title'] = 'elementary OS Freya';
 $sitewide['author'] = 'elementary LLC';
-$sitewide['description'] = 'elementary OS, a fast and open replacement for Windows and OS X';
+$sitewide['description'] = 'A fast and open replacement for Windows and OS X. Pay what you want or download for free.';
+
