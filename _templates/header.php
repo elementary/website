8cd8750e7c5ca9db442587b62f7234551f860712
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2015-01-12 20:56:35 +0000

    Add basic templating. Fix #42, Fix #65, Fix #74

---
+++ _templates/header.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="<?php echo !empty($page['description']) ? $page['description'] : $sitewide['description']; ?>">
+        <meta name="author" content="<?php echo !empty($page['author']) ? $page['author'] : $sitewide['author']; ?>">
+        <title><?php echo !empty($page['title']) ? $page['title'] : $sitewide['title']; ?></title>
+        <link rel="shortcut icon" href="favicon.ico">
+        <link rel="stylesheet" type="text/css" media="all" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300">
+        <link rel="stylesheet" type="text/css" media="all" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" media="all" href="styles/main.css">
+    </head>
+    <body>
+        <div id="content-container">
---
+++ _templates/sitewide.php
@@ -0,0 +1,5 @@
+<?php
+
+$sitewide['title'] = 'elementary OS';
+$sitewide['author'] = 'elementary LLC';
+$sitewide['description'] = 'elementary OS, a fast and open replacement for Windows and OS X';
