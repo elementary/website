cad06c7bd4c70d06d896fb8e73c28cee854f1ab9
Daniel Foré <daniel@elementaryos.org>
2015-01-12 19:16:18 -0800

    alert styling

--- 404.php
+++ 404.php
@@ -4,10 +4,14 @@
     include '_templates/header.php';
 ?>
 
+<div class="row alert">
+    <i class="column warning fa fa-warning"></i>
+	<div class="column alert">
+        <h1>The page you’re looking for can’t be found</h1>
+        <h2>There isn’t anything located here. Check the web address for mispelled words and try again.</h2>
+    </div>
+</div>
 <div class="row">
-    <p class="error-label">404</p>
-    <h1>The page you’re looking for can’t be found</h1>
-    <h2>There isn’t anything located here. Check the web address for mispelled words and try again.</h2>
     <a class="button suggested-action" href="/">Go to Home Page</a>
 </div>
 
