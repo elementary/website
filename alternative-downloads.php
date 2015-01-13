4238820b8a8a09b858e76bfc4d890a1bd0ca2ece
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2015-01-13 22:22:35 +0000

    Add Magnet Links

--- alternative-downloads.php
+++ alternative-downloads.php
@@ -12,14 +12,13 @@
         tr=https%3A%2F%2Fashrise.com%3A443%2Fannounce&
         tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce&
         tr=udp%3A%2F%2Ftracker.ccc.de%3A80%2Fannounce&
-        tr=udp%3A%2F%2Ftracker.istole.it%3A80%2Fannounce&
         tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&
         tr=udp%3A%2F%2Ftracker.publicbt.com%3A80%2Fannounce&
         xs=http%3A%2F%2Felementaryos.org%2Fdownloads%2Felementaryos-unstable-amd64.20140810.iso.torrent&
-        ws=http%3A%2F%2Fsuberb-sea2.dl.sourceforge.net%2Fproject%2Felementaryos%2Fstable%2Felementaryos-unstable-amd64.20140810.iso&
-        ws=http%3A%2F%2Fignum.dl.sourceforge.net%2Fproject%2Felementaryos%2Fstable%2Felementaryos-unstable-amd64.20140810.iso&
-        ws=http%3A%2F%2Fheanet.dl.sourceforge.net%2Fproject%2Felementaryos%2Fstable%2Felementaryos-unstable-amd64.20140810.iso&
-        ws=http%3A%2F%2Fcitylan.dl.sourceforge.net%2Fproject%2Felementaryos%2Fstable%2Felementaryos-unstable-amd64.20140810.iso
+        ws=http%3A%2F%2Fsuberb-sea2.dl.sourceforge.net%2Fproject%2Felementaryos%2Funstable%2Felementaryos-unstable-amd64.20140810.iso&
+        ws=http%3A%2F%2Fignum.dl.sourceforge.net%2Fproject%2Felementaryos%2Funstable%2Felementaryos-unstable-amd64.20140810.iso&
+        ws=http%3A%2F%2Fheanet.dl.sourceforge.net%2Fproject%2Felementaryos%2Funstable%2Felementaryos-unstable-amd64.20140810.iso&
+        ws=http%3A%2F%2Fcitylan.dl.sourceforge.net%2Fproject%2Felementaryos%2Funstable%2Felementaryos-unstable-amd64.20140810.iso
     */
 
 ?>
@@ -30,9 +29,9 @@
             <div class="row">
                 <h2>Freya Beta 1</h2>
                 <br>
-                <p class="text-center"><a href="#" class="button flat">32 bit Direct</a> <a href="#" class="button flat">64 bit Direct</a></p>
+                <p class="text-center"><a href="http://downloads.sourceforge.net/project/elementaryos/unstable/elementaryos-unstable-i386.20140810.iso" class="button flat">32 bit Direct</a> <a href="http://downloads.sourceforge.net/project/elementaryos/unstable/elementaryos-unstable-amd64.20140810.iso" class="button flat">64 bit Direct</a></p>
                 <br>
-                <p class="text-center"><a href="#" class="button flat">32 bit Magnet</a> <a href="#" class="button flat">64 bit Magnet</a></p>
+                <p class="text-center"><a href="magnet:?xt=urn:btih:3dbed253ea2f8d23d8098bc7214f53c4c1201924&dn=elementaryos-unstable-i386.20140810.iso&tr=https%3A%2F%2Fashrise.com%3A443%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.ccc.de%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80%2Fannounce&xs=http%3A%2F%2Felementaryos.org%2Fdownloads%2Felementaryos-unstable-i386.20140810.iso.torrent&ws=http%3A%2F%2Fsuberb-sea2.dl.sourceforge.net%2Fproject%2Felementaryos%2Funstable%2Felementaryos-unstable-i386.20140810.iso&ws=http%3A%2F%2Fignum.dl.sourceforge.net%2Fproject%2Felementaryos%2Funstable%2Felementaryos-unstable-i386.20140810.iso&ws=http%3A%2F%2Fheanet.dl.sourceforge.net%2Fproject%2Felementaryos%2Funstable%2Felementaryos-unstable-i386.20140810.iso&ws=http%3A%2F%2Fcitylan.dl.sourceforge.net%2Fproject%2Felementaryos%2Funstable%2Felementaryos-unstable-i386.20140810.iso" class="button flat">32 bit Magnet</a> <a href="magnet:?xt=urn:btih:5d3dd0d33aef66d1683cc9f7fac321ce04e93cdd&dn=elementaryos-unstable-amd64.20140810.iso&tr=https%3A%2F%2Fashrise.com%3A443%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.ccc.de%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80%2Fannounce&xs=http%3A%2F%2Felementaryos.org%2Fdownloads%2Felementaryos-unstable-amd64.20140810.iso.torrent&ws=http%3A%2F%2Fsuberb-sea2.dl.sourceforge.net%2Fproject%2Felementaryos%2Funstable%2Felementaryos-unstable-amd64.20140810.iso&ws=http%3A%2F%2Fignum.dl.sourceforge.net%2Fproject%2Felementaryos%2Funstable%2Felementaryos-unstable-amd64.20140810.iso&ws=http%3A%2F%2Fheanet.dl.sourceforge.net%2Fproject%2Felementaryos%2Funstable%2Felementaryos-unstable-amd64.20140810.iso&ws=http%3A%2F%2Fcitylan.dl.sourceforge.net%2Fproject%2Felementaryos%2Funstable%2Felementaryos-unstable-amd64.20140810.iso" class="button flat">64 bit Magnet</a></p>
             </div>
 <?php
     include '_templates/footer.html';
