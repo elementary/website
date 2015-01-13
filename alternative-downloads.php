e9c83b14dad82ee5d04d72f4a624340a7e44d1e3
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2015-01-13 21:01:55 +0000

    Change downloads title

--- alternative-downloads.php
+++ alternative-downloads.php
@@ -1,27 +1,11 @@
 <?php
 
     include '_templates/sitewide.php';
-    $page['title'] = 'Downloads';
+    $page['title'] = 'Alternative Downloads';
     include '_templates/header.php';
 
-    /*  Luna 64
-        magnet:?
-        xt=urn:btih:5c5978d6a76b960bb0504433ff6b408b183ebf38&
-        dn=elementaryos-stable-amd64.20130810.iso&
-        tr=https%3A%2F%2Fashrise.com%3A443%2Fannounce&
-        tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce&
-        tr=udp%3A%2F%2Ftracker.ccc.de%3A80%2Fannounce&
-        tr=udp%3A%2F%2Ftracker.istole.it%3A80%2Fannounce&
-        tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&
-        tr=udp%3A%2F%2Ftracker.publicbt.com%3A80%2Fannounce&
-        xs=http%3A%2F%2Felementaryos.org%2Fdownloads%2Felementaryos-stable-amd64.20130810.iso.torrent&
-        ws=http%3A%2F%2Fsuberb-sea2.dl.sourceforge.net%2Fproject%2Felementaryos%2Fstable%2Felementaryos-stable-amd64.20130810.iso&
-        ws=http%3A%2F%2Fignum.dl.sourceforge.net%2Fproject%2Felementaryos%2Fstable%2Felementaryos-stable-amd64.20130810.iso&
-        ws=http%3A%2F%2Fheanet.dl.sourceforge.net%2Fproject%2Felementaryos%2Fstable%2Felementaryos-stable-amd64.20130810.iso&
-        ws=http%3A%2F%2Fcitylan.dl.sourceforge.net%2Fproject%2Felementaryos%2Fstable%2Felementaryos-stable-amd64.20130810.iso
-    */
-
     /*
+        Magnet URL Template
         magnet:?
         xt=urn:btih:HEXHASHHERE&
         dn=elementaryos-unstable-amd64.20140810.iso&
