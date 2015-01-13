9b871b990d71ec07da87e0377f4909fea4fd5e83
khonquer <khonkala@mail.com>
2015-01-12 23:48:32 -0600

    Converted to php
    Added changes to php file.

--- index.php
+++ index.php
@@ -8,6 +8,16 @@
                 <button id="download" class="suggested-action" onclick=window.open("http://downloads.sourceforge.net/project/elementaryos/unstable/elementaryos-unstable-amd64.20140810.iso")>Download Freya Beta</button>
                 <p class="small-label">886.0 MB (64 bit PC or Mac)</p>
             </div>
+            <div id="overlay"></div>
+            <div id="overlay-content">
+                <label>Download elementary OS</label>
+                <form action="">
+                    <input type="radio" name="bit" value="32">32Bit or
+                    <input type="radio" name="bit" value="64" checked="checked">64Bit
+                </form>
+                <button id="overlay-download" class="suggested-action">Download Freya Beta</button>
+                <p><a href="#">Download Freya using Torrent</a></p>
+            </div>
 <?php
     include '_templates/footer.html';
 ?>
