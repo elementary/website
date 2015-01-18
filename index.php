cdc89a67863f520df1d99875ea7816aab6ba5993
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2015-01-18 23:19:16 +0000

    Compress buttons

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
                     <button class="small-button payment-button target-amount checked">25</button>
                     <button class="small-button payment-button target-amount">50</button>
                     <div class="column">
-                        <input type="text" id="payment" class="button small-button target-amount" placeholder="Custom">
+                        <input type="text" id="pay-custom" class="button small-button target-amount" placeholder="Custom">
                         <p class="small-label focus-reveal text-center">Enter any whole dollar amount.</p>
                     </div>
                 </div>
