432c75f79da5c6603787444ac9b821b79eff48f0
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2015-01-19 20:13:56 +0000

    Set minimum at , not $.5

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
                     <button value="25" class="small-button payment-button target-amount checked">25</button>
                     <button value="50" class="small-button payment-button target-amount">50</button>
                     <div class="column">
-                        <input type="number" step="0.01" min="0.50" max="999999.99" id="pay-custom" class="button small-button target-amount" placeholder="Custom">
+                        <input type="number" step="0.01" min="1.00" max="999999.99" id="pay-custom" class="button small-button target-amount" placeholder="Custom">
                         <p class="small-label focus-reveal text-center">Enter any dollar amount.</p>
                     </div>
                 </div>
