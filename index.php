9fd1e864eafffb1d3696646351023a8a3e9fdb89
Daniel Foré <daniel@elementaryos.org>
2015-01-20 17:04:07 -0800

    remove the hero from all rows

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
             <div class="row">
                 <img alt="elementary OS" id="logotype" src="images/logotype.svg">
                 <h2>A fast and open replacement for Windows and OS X</h2>
+            </div>
                 <img class="hero" src="images/notebook.png">
+            <div class="row">
                 <button id="download" class="suggested-action" onclick=window.open("http://downloads.sourceforge.net/project/elementaryos/unstable/elementaryos-unstable-amd64.20140810.iso")>Download Freya Beta</button>
                 <p class="small-label">886.0 MB (64-bit PC or Mac)</p>
                 <p class="small-label"><a href="alternative-downloads">Alternative Downloads</a></p>
