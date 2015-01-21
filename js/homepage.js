942af28f7b3cbb3df862c2ac0aabe44f76976c96
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2015-01-21 23:46:24 +0000

    Actually add homepage.js

---
+++ js/homepage.js
@@ -0,0 +1,14 @@
+$('#download').click(function(){
+    console.log('Clicked #download');
+    $(function(){
+        $('.open-modal').leanModal({
+            // Top in Pixels as INT.
+            // Perhaps set to nonsense and style in CSS.
+            top: 'NOTHANKS',
+            closeButton: '.close',
+            overlayOpacity: 0.9
+        });
+    });
+    $('.open-modal').click();
+});
+console.log('Loaded homepage.js');
