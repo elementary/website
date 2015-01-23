9196a5db73f5ad125dfde1b9e6a10ff19c345106
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2015-01-23 20:23:54 +0000

    Sanctify JS

--- js/homepage.js
+++ js/homepage.js
@@ -1,5 +1,5 @@
 var stripe_key = '';
-var payment_minimum = 150; // Let's make the minimum $1.50 for now
+var payment_minimum = 100; // Let's make the minimum $1.00 for now
 
 $('#download').click(function(){
     payment_amount = 2000;
@@ -28,9 +28,9 @@ function do_stripe_payment (amount) {
 
 function process_payment (amount, token) {
     payment_http = new XMLHttpRequest();
-    payment_http.open("POST","./backend/payment.php",true);
-    payment_http.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-    payment_http.send("amount=" + amount + "&token=" + token.id);
+    payment_http.open('POST','./backend/payment.php',true);
+    payment_http.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+    payment_http.send('amount=' + amount + '&token=' + token.id);
 }
 
 function open_download_overlay () {
@@ -41,11 +41,11 @@ function open_download_overlay () {
 
 // Get the stripe key from the server
 key_http = new XMLHttpRequest();
-key_http.open("GET","./backend/payment.php",true);
+key_http.open('GET','./backend/payment.php',true);
 key_http.onreadystatechange = function() {
     if (key_http.readyState == 4 && key_http.status == 200) {
         stripe_key = key_http.responseText;
-        console.log("Striep key is: " + stripe_key);
+        console.log('Striep key is: ' + stripe_key);
     }
 } 
 key_http.send();
