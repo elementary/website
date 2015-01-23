9c020bd6d35b021612834e6ec6ea8a2cff8cda82
Daniel Foré <daniel@elementaryos.org>
2015-01-22 20:04:31 -0800

    move dialog out of main layout

--- index.php
+++ index.php
@@ -15,21 +15,6 @@
             <div class="row">
                 <button id="download" class="suggested-action">Download Freya Beta</button>
                 <p class="small-label">886.0 MB (for PC or Mac)</p>
-                <div id="download-modal" class="modal">
-                    <h1>Choose a Download</h1>
-                    <h2>We recommend 64-bit for most modern computers.</h2>
-                    <div class="row actions">
-                        <div class="column linked">
-                            <a class="button" href="http://downloads.sourceforge.net/project/elementaryos/unstable/elementaryos-unstable-i386.20140810.iso">Freya Beta 32-bit</a>
-                            <a class="button" href="magnet:?xt=urn:btih:3dbed253ea2f8d23d8098bc7214f53c4c1201924&dn=elementaryos-unstable-i386.20140810.iso&tr=https%3A%2F%2Fashrise.com%3A443%2Fphoenix%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.ccc.de%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80%2Fannounce&xs=http%3A%2F%2Felementaryos.org%2Fdownloads%2Felementaryos-unstable-i386.20140810.iso.torrent&ws=http%3A%2F%2Fsuberb-sea2.dl.sourceforge.net%2Fproject%2Felementaryos%2Funstable%2Felementaryos-unstable-i386.20140810.iso&ws=http%3A%2F%2Fignum.dl.sourceforge.net%2Fproject%2Felementaryos%2Funstable%2Felementaryos-unstable-i386.20140810.iso&ws=http%3A%2F%2Fheanet.dl.sourceforge.net%2Fproject%2Felementaryos%2Funstable%2Felementaryos-unstable-i386.20140810.iso&ws=http%3A%2F%2Fcitylan.dl.sourceforge.net%2Fproject%2Felementaryos%2Funstable%2Felementaryos-unstable-i386.20140810.iso"><i class="fa fa-magnet"></i></a>
-                        </div>
-                        <div class="column linked">
-                            <a class="button suggested-action" href="http://downloads.sourceforge.net/project/elementaryos/unstable/elementaryos-unstable-amd64.20140810.iso">Freya Beta 64-bit</a>
-                            <a class="button suggested-action" href="magnet:?xt=urn:btih:5d3dd0d33aef66d1683cc9f7fac321ce04e93cdd&dn=elementaryos-unstable-amd64.20140810.iso&tr=https%3A%2F%2Fashrise.com%3A443%2Fphoenix%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.ccc.de%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80%2Fannounce&xs=http%3A%2F%2Felementaryos.org%2Fdownloads%2Felementaryos-unstable-amd64.20140810.iso.torrent&ws=http%3A%2F%2Fsuberb-sea2.dl.sourceforge.net%2Fproject%2Felementaryos%2Funstable%2Felementaryos-unstable-amd64.20140810.iso&ws=http%3A%2F%2Fignum.dl.sourceforge.net%2Fproject%2Felementaryos%2Funstable%2Felementaryos-unstable-amd64.20140810.iso&ws=http%3A%2F%2Fheanet.dl.sourceforge.net%2Fproject%2Felementaryos%2Funstable%2Felementaryos-unstable-amd64.20140810.iso&ws=http%3A%2F%2Fcitylan.dl.sourceforge.net%2Fproject%2Felementaryos%2Funstable%2Felementaryos-unstable-amd64.20140810.iso"><i class="fa fa-magnet"></i></a>
-                        </div>
-                    </div>
-                </div>
-                <a style="display:none;" class="open-modal" href="#download-modal">Modal</a>
             </div>
             <div class="row">
                 <div class="column third">
@@ -45,6 +30,21 @@
                     <p>“Lightweight and fast… Completely community-based, and has a real flair for design and appearances.”</p>
                 </div>
             </div>
+            <div id="download-modal" class="modal">
+                <h1>Choose a Download</h1>
+                <h2>We recommend 64-bit for most modern computers.</h2>
+                <div class="row actions">
+                    <div class="column linked">
+                        <a class="button" href="http://downloads.sourceforge.net/project/elementaryos/unstable/elementaryos-unstable-i386.20140810.iso">Freya Beta 32-bit</a>
+                        <a class="button" href="magnet:?xt=urn:btih:3dbed253ea2f8d23d8098bc7214f53c4c1201924&dn=elementaryos-unstable-i386.20140810.iso&tr=https%3A%2F%2Fashrise.com%3A443%2Fphoenix%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.ccc.de%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80%2Fannounce&xs=http%3A%2F%2Felementaryos.org%2Fdownloads%2Felementaryos-unstable-i386.20140810.iso.torrent&ws=http%3A%2F%2Fsuberb-sea2.dl.sourceforge.net%2Fproject%2Felementaryos%2Funstable%2Felementaryos-unstable-i386.20140810.iso&ws=http%3A%2F%2Fignum.dl.sourceforge.net%2Fproject%2Felementaryos%2Funstable%2Felementaryos-unstable-i386.20140810.iso&ws=http%3A%2F%2Fheanet.dl.sourceforge.net%2Fproject%2Felementaryos%2Funstable%2Felementaryos-unstable-i386.20140810.iso&ws=http%3A%2F%2Fcitylan.dl.sourceforge.net%2Fproject%2Felementaryos%2Funstable%2Felementaryos-unstable-i386.20140810.iso"><i class="fa fa-magnet"></i></a>
+                    </div>
+                    <div class="column linked">
+                        <a class="button suggested-action" href="http://downloads.sourceforge.net/project/elementaryos/unstable/elementaryos-unstable-amd64.20140810.iso">Freya Beta 64-bit</a>
+                        <a class="button suggested-action" href="magnet:?xt=urn:btih:5d3dd0d33aef66d1683cc9f7fac321ce04e93cdd&dn=elementaryos-unstable-amd64.20140810.iso&tr=https%3A%2F%2Fashrise.com%3A443%2Fphoenix%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.ccc.de%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80%2Fannounce&xs=http%3A%2F%2Felementaryos.org%2Fdownloads%2Felementaryos-unstable-amd64.20140810.iso.torrent&ws=http%3A%2F%2Fsuberb-sea2.dl.sourceforge.net%2Fproject%2Felementaryos%2Funstable%2Felementaryos-unstable-amd64.20140810.iso&ws=http%3A%2F%2Fignum.dl.sourceforge.net%2Fproject%2Felementaryos%2Funstable%2Felementaryos-unstable-amd64.20140810.iso&ws=http%3A%2F%2Fheanet.dl.sourceforge.net%2Fproject%2Felementaryos%2Funstable%2Felementaryos-unstable-amd64.20140810.iso&ws=http%3A%2F%2Fcitylan.dl.sourceforge.net%2Fproject%2Felementaryos%2Funstable%2Felementaryos-unstable-amd64.20140810.iso"><i class="fa fa-magnet"></i></a>
+                    </div>
+                </div>
+            </div>
+            <a style="display:none;" class="open-modal" href="#download-modal"></a>
 <?php
     include '_templates/footer.html';
 ?>
--- js/homepage.js
+++ js/homepage.js
@@ -1,9 +1,3 @@
-$('.js-target-jquery-leanmodal-overlay').on('scroll touchmove mousewheel', function (e) {
-    e.preventDefault();
-    e.stopPropagation();
-    return false;
-});
-
 $('#download').click(function(){
     console.log('Clicked #download');
     $(function(){
