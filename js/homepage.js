1a4b38a681c89f08bf939d5bcb54fc6b11f9f82f
Lewis Goddard <lewisgoddard@eustasy.org>
2015-01-23 17:21:22 +0000

    Remove unnecessary function wrapper.

--- js/homepage.js
+++ js/homepage.js
@@ -1,8 +1,6 @@
 $('#download').click(function(){
     console.log('Clicked #download');
-    $(function(){
-        $('.open-modal').leanModal();
-    });
+    $('.open-modal').leanModal();
     $('.open-modal').click();
 });
 console.log('Loaded homepage.js');
