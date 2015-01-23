34219814a4e303ded510291ebdd0dce3940769b1
Fabian Thoma <fabian@elementaryos.org>
2015-01-23 19:11:45 +0100

    Started stripe addition from scratch, reusing the old code, payment amount buttons not added yet

---
+++ backend/config.example.php
@@ -0,0 +1,9 @@
+<?php
+// Example of config file, add your own values here, all are imaginary
+
+$config = array(
+	"stripe_sk"		=> 'sk_test_hoigesrjgoisrhgilgjrsfjs', 
+	"stripe_pk"		=> 'pk_test_hoigesrjgoisrhgilgjrsfjs'
+);
+
+?>
---
+++ backend/payment.php
@@ -0,0 +1,27 @@
+<?php
+require_once('./lib/Stripe.php');
+require_once('./config.php');
+
+Stripe::setApiKey($config['stripe_sk']);
+
+
+if (isset($_POST['token'])) {
+    $token  = $_POST['token'];
+    $amount = $_POST['amount'];
+
+    // Create the charge on Stripe's servers - this will charge the user's card
+    try {
+        $charge = Stripe_Charge::create(array(
+            "amount" => $amount,
+            "currency" => "usd",
+            "card" => $token,
+            "description" => "elementary OS download")
+        );
+        echo "OK";
+    } catch(Stripe_CardError $e) {
+        echo "error";
+    }
+} else { 
+    echo $config['stripe_pk'];
+}
+
