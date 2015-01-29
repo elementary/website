667916e91cfe87056de8f84be593992c5285d7f1
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2015-01-29 12:36:32 +0000

    Add Privacy Policy

---
+++ privacy-policy.php
@@ -0,0 +1,45 @@
+<?php
+    include '_templates/sitewide.php';
+    $page['title'] = 'Privacy Policy';
+    include '_templates/header.php';
+?>
+            <div class="row" style="max-width:1000px;">
+                <h1>Privacy Policy</h1>
+                <h2>Do Not Track</h2>
+                <p>This site honors the do-not-track setting found in most modern browsers, and disables Google Universal Analytics when it is present. Unfortunately, we cannot remove other tracking methods without severely impacting the use of the site. Should you wish to do so, reference the section on disabling cookies.</p>
+                <p>Below you will find links to enabling DNT in your browser.</p>
+                <ul>
+                    <li><a target="_blank" href="https://support.google.com/chrome/answer/114836">Google Chrome</a></li>
+                    <li><a target="_blank" href="https://www.mozilla.org/en-US/firefox/dnt/">Mozilla Firefox</a></li>
+                    <li><a target="_blank" href="http://windows.microsoft.com/en-GB/internet-explorer/use-tracking-protection">Microsoft Internet Explorer</a></li>
+                    <li><a target="_blank" href="http://help.opera.com/Windows/12.10/en/notrack.html">Opera</a></li>
+                </ul>
+                <h2>Cookies</h2>
+                <p class="text-center"><strong>You can choose to disable or selectively turn off any cookies or third-party cookies in your browser settings.</strong></p>
+                <p>This site uses cookies for many small incremental improvements. You may find the services function without them, but at a reduced usability. For example, the site will not remember if you have previously paid for elementary OS, and by default you will be asked to pay again.</p>
+                <p><strong>CloudFlare</strong> stores cookie to log behavioral elements to analyze for potential threats. The personally identifiable information they store is usually deleted within 4 hours. Cookies enable them to recognize you as a verified human being, and not a robot.</p>
+                <p><strong>Google Universal Analytics</strong> stores cookies are used to collect information about how visitors use this website, which are then utilized to help improve it. The cookies collect information in an anonymous form, including the number of visitors to the site, where visitors have come to the site from and the pages they visited.</p>
+                <p><strong>Stripe</strong> uses cookies to remember your last order, or the country you're from so it knows what card types to offer for payments.</p>
+                <p>Our third party services privacy policies can viewed at the links below.</p>
+                <ul>
+                    <li><a target="_blank" href="https://www.cloudflare.com/security-policy">CloudFlare (Distribution and Security)</a></li>
+                    <li><a target="_blank" href="http://www.google.co.uk/intl/en/analytics/learn/privacy.html">Google Universal Analytics</a></li>
+                    <li><a target="_blank" href="https://stripe.com/privacy">Stripe (Payment Processor)</a></li>
+                </ul>
+                <p><strong>Manage Cookies</strong></p>
+                <p>As you have already visited our site, you may wish to manage cookies already set in your browser. Links to the relevant instructions can be found below.</p>
+                <ul>
+                    <li><a target="_blank" href="https://support.google.com/chrome/answer/95647">Google Chrome</a></li>
+                    <li>Mozilla Firefox
+                        <ul>
+                            <li><a target="_blank" href="https://support.mozilla.org/en-US/kb/enable-and-disable-cookies-website-preferences">Disable Cookies</a></li>
+                            <li><a target="_blank" href="https://support.mozilla.org/en-US/kb/delete-cookies-remove-info-websites-stored">Manage Cookies</a></li>
+                        </ul>
+                    </li>
+                    <li><a target="_blank" href="http://windows.microsoft.com/en-gb/internet-explorer/delete-manage-cookies">Microsoft Internet Explorer</a></li>
+                    <li><a target="_blank" href="http://help.opera.com/Windows/12.10/en/cookies.html">Opera</a></li>
+                </ul>
+            </div>
+<?php
+    include '_templates/footer.html';
+?>
