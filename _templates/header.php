fe9e5f2242e92c09508e6ceff39654a3ad602acd
Rory J <rory@rjsanderson.com>
2015-01-29 19:46:51 +0000

    Add android chrome favicon

--- _templates/header.php
+++ _templates/header.php
@@ -24,6 +24,8 @@
         <title><?php echo !empty($page['title']) ? $page['title'] : $sitewide['title']; ?></title>
 
         <link rel="shortcut icon" href="favicon.ico">
+        <link rel="icon" type="image/png" href="images/android-192.png" sizes=192x192>
+        <link rel="icon" type="image/png" href="images/256.png" sizes=256x256>
         <link rel="apple-touch-icon" href="images/launcher-icons/apple-touch-icon.png">
         <link rel="stylesheet" type="text/css" media="all" href="http://fonts.googleapis.com/css?family=Open+Sans:400,600,300">
         <link rel="stylesheet" type="text/css" media="all" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
@@ -66,3 +68,4 @@
         </nav>
 
         <div id="content-container">
+
