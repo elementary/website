7bba9a098cc997b9067bb3bfd32587f29963db23
Katie M. K. Blaede <katiekecso@gmail.com>
2015-01-28 21:40:55 -0700

    Updated embed code (removed border)

--- store.php
+++ store.php
@@ -8,7 +8,7 @@
                 <h2>Our store currently only supports US orders.</h2>
             </div>
             <div class="row">
-                <a href='https://mkt.com/elementary-llc' class='sq-embed-menu' data-menu-item-images='large' data-menu-accent-color='04a9dc' data-menu-template='column' data-menu-border='show' data-menu-item-descriptions='show' >Order Online</a>
+                <a href='https://mkt.com/elementary-llc' class='sq-embed-menu' data-menu-item-images='large' data-menu-accent-color='04a9dc' data-menu-template='column' data-menu-border='hide' data-menu-item-descriptions='show' >Order Online</a>
                 <script src="https://cdn.sq-api.com/market/embed.js" charset="utf-8"></script>
             </div>
 
