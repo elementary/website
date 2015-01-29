8a51e2f0227e6715d546d268aa8a834ebd4a8e64
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2015-01-29 12:05:45 +0000

    Anonymize IPs and utilize SSL.

--- _templates/header.php
+++ _templates/header.php
@@ -44,6 +44,8 @@
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
             })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
             ga('create', 'UA-19280770-1', 'auto');
+            ga('set', 'forceSSL', true);
+            ga('set', 'anonymizeIp', true);
             ga('require', 'displayfeatures');
             ga('send', 'pageview');
         </script>
