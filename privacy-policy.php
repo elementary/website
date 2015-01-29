3cca481b9efc2ef8ade370a62bde0e5174fc2886
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2015-01-29 18:43:10 +0000

    Update Copy

--- privacy-policy.php
+++ privacy-policy.php
@@ -6,26 +6,13 @@
             <div class="row" style="max-width:1000px;">
                 <h1>Privacy Policy</h1>
                 <h2>Do Not Track</h2>
-                <p>This site honors the do-not-track setting found in most modern browsers, and disables Google Universal Analytics when it is present. Unfortunately, we cannot remove other tracking methods without severely impacting the use of the site. Should you wish to do so, reference the section on disabling cookies.</p>
-                <p>Below you will find links to enabling DNT in your browser.</p>
-                <ul>
-                    <li><a target="_blank" href="https://support.google.com/chrome/answer/114836">Google Chrome</a></li>
-                    <li><a target="_blank" href="https://www.mozilla.org/en-US/firefox/dnt/">Mozilla Firefox</a></li>
-                    <li><a target="_blank" href="http://windows.microsoft.com/en-GB/internet-explorer/use-tracking-protection">Microsoft Internet Explorer</a></li>
-                    <li><a target="_blank" href="http://help.opera.com/Windows/12.10/en/notrack.html">Opera</a></li>
-                </ul>
+                <p>This site honors the do-not-track setting found in most modern browsers and disables Google Analytics when it is present. We cannot automatically remove other tracking methods without severely impacting the use of the site. Should you wish to manually manage these tracking methods, reference the section on disabling cookies.</p>
                 <h2>Cookies</h2>
                 <p class="text-center"><strong>You can choose to disable or selectively turn off any cookies or third-party cookies in your browser settings.</strong></p>
-                <p>This site uses cookies for many small incremental improvements. You may find the services function without them, but at a reduced usability. For example, the site will not remember if you have previously paid for elementary OS, and by default you will be asked to pay again.</p>
-                <p><strong>CloudFlare</strong> stores cookie to log behavioral elements to analyze for potential threats. The personally identifiable information they store is usually deleted within 4 hours. Cookies enable them to recognize you as a verified human being, and not a robot.</p>
-                <p><strong>Google Universal Analytics</strong> stores cookies are used to collect information about how visitors use this website, which are then utilized to help improve it. The cookies collect information in an anonymous form, including the number of visitors to the site, where visitors have come to the site from and the pages they visited.</p>
-                <p><strong>Stripe</strong> uses cookies to remember your last order, or the country you're from so it knows what card types to offer for payments.</p>
-                <p>Our third party services privacy policies can viewed at the links below.</p>
-                <ul>
-                    <li><a target="_blank" href="https://www.cloudflare.com/security-policy">CloudFlare (Distribution and Security)</a></li>
-                    <li><a target="_blank" href="http://www.google.co.uk/intl/en/analytics/learn/privacy.html">Google Universal Analytics</a></li>
-                    <li><a target="_blank" href="https://stripe.com/privacy">Stripe (Payment Processor)</a></li>
-                </ul>
+                <p>This site uses cookies for incremental improvements. You may find the services function without them but at a reduced usability. For example, the site will not remember if you have previously paid for elementary OS; by default you will be asked to pay again.</p>
+                <p><strong>CloudFlare</strong> stores cookies to log behavioral elements and analyze potential threats. For more information, see the <a class="read-more" target="_blank" href="https://www.cloudflare.com/security-policy">CloudFlare Privacy & Security Policy</a></p>
+                <p><strong>Google Analytics</strong> stores cookies to collect information—including the number of visitors to this site, from where they were referred, and the pages they visit—in an anonymous form. This information is used to help improve the site. For more information, see Google's article on <a class="read-more" target="_blank" href="https://support.google.com/analytics/answer/6004245">Safeguarding Your Data</a></p>
+                <p><strong>Stripe</strong> uses cookies to remember your last order and your country so it knows what card types to offer for payments. For more information, see <a class="read-more" target="_blank" href="https://stripe.com/privacy">Stripe's Privacy Policy</a></p>
                 <p><strong>Manage Cookies</strong></p>
                 <p>As you have already visited our site, you may wish to manage cookies already set in your browser. Links to the relevant instructions can be found below.</p>
                 <ul>
