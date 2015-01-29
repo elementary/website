f36a426324f3b9883f0aedc15faa74bc74414d41
KeitIG <pierre.de.la.martiniere@gmail.com>
2015-01-29 13:10:47 +0100

    Create developer page

--- _templates/header.php
+++ _templates/header.php
@@ -26,6 +26,7 @@
         <link rel="shortcut icon" href="favicon.ico">
         <link rel="apple-touch-icon" href="images/launcher-icons/apple-touch-icon.png">
         <link rel="stylesheet" type="text/css" media="all" href="http://fonts.googleapis.com/css?family=Open+Sans:400,600,300">
+        <link rel="stylesheet" type="text/css" media="all" href="http://fonts.googleapis.com/css?family=Raleway:400,100,300">
         <link rel="stylesheet" type="text/css" media="all" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
         <link rel="stylesheet" type="text/css" media="all" href="styles/main.css">
 
@@ -48,7 +49,7 @@
         </script>
 
     </head>
-    <body>
+    <body class="page-<?php echo basename($_SERVER['PHP_SELF'], ".php"); ?>">
         <nav>
             <ul class="left">
                 <li><a href="/"><span class="logomark"></span></a></li>
---
+++ developer.php
@@ -0,0 +1,42 @@
+<?php
+    include '_templates/sitewide.php';
+    $page['title'] = 'Developer';
+    $page['scripts'] .= '<link rel="stylesheet" type="text/css" media="all" href="styles/developer.css">';
+    include '_templates/header.php';
+?>
+            <div class="row">
+                <div class="column half developer-sum">
+                    <h1>Develop</h1>
+                    <h2>Your ideas into code</h2>
+                    <p>Built on the best open source technology, elementary is an extremely developer-friendly platform. We provide existing and prospective developers the best available resources and documentation to ensure that your apps really shine.</p>
+                </div>
+                <div class="column half developer-sections">
+                    <div id="dev-section-1" class="dev-section">
+                        <a href="" title="Design">
+                            <h3>Design</h3>
+                            <p>Read the Human Interface Guidelines (HIG) and learn how to make your app a first-class experience.</p>
+                        </a>
+                    </div>
+                    <div id="dev-section-2" class="dev-section">
+                        <a href="" title="Code">
+                            <h3>Code</h3>
+                            <p>Learn about our Application Programming Interface (APIs), and discover all the built-in technologies available to you.</p>
+                        </a>
+                    </div>
+                    <div id="dev-section-3" class="dev-section">
+                        <a href="" title="Manage">
+                            <h3>Manage</h3>
+                            <p>Use Launchpad to manage bug reports and collaborate with other developers.</p>
+                        </a>
+                    </div>
+                    <div id="dev-section-4" class="dev-section">
+                        <a href="" title="Distribute">
+                            <h3>Distribute</h3>
+                            <p>Set up an Ubuntu My Apps account and publish your apps into the Software Center.</p>
+                        </a>
+                    </div>
+                </div>
+            </div>
+<?php
+    include '_templates/footer.html';
+?>
