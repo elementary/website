68a770c98defc67748a53621780a0873cbb9bf43
Emersion <contact@emersion.fr>
2015-01-30 07:57:05 +0100

    Adds some comments

--- _templates/i18n.php
+++ _templates/i18n.php
@@ -1,4 +1,5 @@
 <?php
+// Index French strings for testing purposes
 $translations = array(
     'A fast and open replacement for Windows and OS X' => 'Un remplaçant rapide et ouvert pour Windows et OS X',
     'Download Freya Beta' => 'Télécharger Freya Beta',
@@ -10,6 +11,9 @@ $translations = array(
     'installation guide' => 'guide d\'installation'
 );
 
+/**
+ * Translate a string. Returns the original string if no translation was found.
+ */
 function translate($string) {
     global $translations;
 
@@ -20,29 +24,39 @@ function translate($string) {
     }
 }
 
+/**
+ * Translate a HTML string. This includes a minimalist XML parser.
+ * Can be provided a $translate callback to override default behaviour.
+ * (Useful to extract strings from a file for instance.)
+ */
 function translate_html($input, $translate = 'translate') {
-    $output = '';
+    $output = ''; // Output HTML string
 
+    // Tags that doesn't contain translatable text
     $tagsBlacklist = array('script', 'kbd');
 
+    // Attributes that can be translated
     $attrsWhitelist = array(
         array('input', 'placeholder'),
         array('a', 'title'),
         array('img', 'alt')
     );
 
-    $inTag = false;
-    $tagContents = '';
-    $inTagName = false;
-    $tagName = '';
-    $inAttrName = false;
-    $attrName = '';
-    $inAttrValue = false;
-    $attrValue = '';
-    for ($i = 0; $i < strlen($input); $i++) {
-        $char = $input[$i];
+    $inTag = false; // Are we in a <tag [here]>?
+    $tagContents = ''; // The tag contents: <tag>contents[here]</tag>
+    $inTagName = false; // Are we in a <ta[here]g> ?
+    $tagName = ''; // The current tag name
+    $inAttrName = false; // Are we in a <tag attr[here]="blah">?
+    $attrName = ''; // The current attribute name
+    $inAttrValue = false; // Are we in a <tag attr="blah[here]">?
+    $attrValue = ''; // The current attribute value
 
-        if (!$inTag && $char == '<') {
+    // Begin parsing input HTML
+    for ($i = 0; $i < strlen($input); $i++) {
+        $char = $input[$i]; // Current char
+
+        if (!$inTag && $char == '<') { // Begins a <tag>
+            // Ouput the last tag contents, translate it if possible
             $tagContents = trim($tagContents);
             if (!empty($tagContents) && !in_array($tagName, $tagsBlacklist)) {
                 $output .= $translate($tagContents);
@@ -53,23 +67,24 @@ function translate_html($input, $translate = 'translate') {
             $tagName = '';
             $tagContents = '';
         }
-        if ($inTag && $char == '>') {
+        if ($inTag && $char == '>') { // Ends a <tag>
             $inTag = false;
             $inTagName = false;
             $inAttrName = false;
             $attrName = '';
             $tagContents = '';
         }
-        if ($inTag && !$inAttrValue && $char == ' ') {
+        if ($inTag && !$inAttrValue && $char == ' ') { // Begins an attribute name
             $inTagName = false;
             $inAttrName = true;
             $attrName = '';
         }
-        if ($inTag && $inAttrName && $char == '=') {
+        if ($inTag && $inAttrName && $char == '=') { // Begins an attribute value
             $inAttrName = false;
             $inAttrValue = true;
             $attrValue = '';
         }
+        // Ends an attribute value
         if ($inTag && $inAttrValue && $char == '"' && $input[$i-1] != '=') {
             $inAttrValue = false;
 
@@ -86,17 +101,23 @@ function translate_html($input, $translate = 'translate') {
             }
         }
 
+        // Append char
+
+        // In a tag name or in an attribute name
         if ($inTagName && $char != '<') {
             $tagName .= $char;
         }
         if ($inAttrName && $char != ' ') {
             $attrName .= $char;
         }
+
         if ($inAttrValue && $char != '=' && $char != '"') {
+            // In an attribute value, buffer it
             $attrValue .= $char;
         } elseif (!$inTag && $char != '>') {
+            // In a tag contents, buffer it
             $tagContents .= $char;
-        } else {
+        } else { // Otherwise, just append it to the output
             $output .= $char;
         }
     }
@@ -104,11 +125,17 @@ function translate_html($input, $translate = 'translate') {
     return $output;
 }
 
+/**
+ * Begin to translate outputted HTML.
+ */
 function begin_html_i18n() {
     ob_start(function ($input) {
         return translate_html($input);
     });
 }
+/**
+ * End outputted HTML translation.
+ */
 function end_html_i18n() {
     ob_end_flush();
 }
