1000f5133ea785282a1469f6315762fc6a1cee52
Cassidy James Blaede <cassidy@system76.com>
2015-01-31 16:50:35 -0700

    Update some support text and styling

--- support.php
+++ support.php
@@ -10,25 +10,25 @@
             <div class="row">
                 <a class="column half" href="#">
                     <h2><i class="fa fa-question-circle"></i> FAQ</h2>
-                    <p>Has your question been answered already? Check out answers to some of the most common questions.</p>
+                    <p>Has your question been asked already? Check out answers to some of the most common questions we get.</p>
                 </a>
                 <a class="column half" href="/installation">
                     <h2><i class="fa fa-download"></i> Installation</h2>
-                    <p>Get help installing elementary OS on your computer.</p>
+                    <p>Get help installing elementary OS on your computer by following our step-by-step guide.</p>
                 </a>
                 <a class="column half" href="https://plus.google.com/communities/104613975513761463450" target="_blank_">
                     <h2><i class="fa fa-google-plus"></i> Google+</h2>
-                    <p>Communicate with other elementary OS users in our Google+ community.</p>
+                    <p>Communicate with other elementary OS users in our Google+ community. Find crowd-sourced support, screenshots, the latest news, and more.</p>
                 </a>
                 <a class="column half" href="http://www.reddit.com/r/elementaryos/" target="_blank">
                     <h2><i class="fa fa-reddit"></i> reddit</h2>
-                    <p>Discuss elementary OS in our official subreddit.</p>
+                    <p>Discuss elementary OS with other fans and followers in our official subreddit. Ask the community for help or just chat about the OS.</p>
                 </a>
             </div>
             <div class="row">
                 <a class="column" href="https://bugs.launchpad.net/elementary/+filebug" target="_blank">
                     <h2><i class="fa fa-bug"></i> Report a bug</h2>
-                    <p>Unable to resolve your issue? Let our developers know.</p>
+                    <p>Unable to resolve your issue? Let our developers know by filing a bug on Launchpad.</p>
                 </a>
             </div>
 
