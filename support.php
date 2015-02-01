50e76ec35caf20c38eca91b31473dbb2772670cf
Cassidy James Blaede <cassidy@system76.com>
2015-01-31 20:59:07 -0700

    relative icon paths

--- support.php
+++ support.php
@@ -38,57 +38,57 @@
     <h2>Get support for a specific app</h2>
 
     <a class="app" href="#" target="_blank">
-        <img src="/images/icons/apps/64/office-calendar.svg" />
+        <img src="images/icons/apps/64/office-calendar.svg" />
         <span>Calendar</span>
     </a>
 
     <a class="app" href="#" target="_blank">
-        <img src="/images/icons/apps/64/accessories-camera.svg" />
+        <img src="images/icons/apps/64/accessories-camera.svg" />
         <span>Camera</span>
     </a>
 
     <a class="app" href="#" target="_blank">
-        <img src="/images/icons/apps/64/system-file-manager.svg" />
+        <img src="images/icons/apps/64/system-file-manager.svg" />
         <span>Files</span>
     </a>
 
     <a class="app" href="#" target="_blank">
-        <img src="/images/icons/apps/64/internet-mail.svg" />
+        <img src="images/icons/apps/64/internet-mail.svg" />
         <span>Geary</span>
     </a>
 
     <a class="app" href="#" target="_blank">
-        <img src="/images/icons/apps/64/midori.svg" />
+        <img src="images/icons/apps/64/midori.svg" />
         <span>Midori</span>
     </a>
 
     <a class="app" href="#" target="_blank">
-        <img src="/images/icons/apps/64/multimedia-audio-player.svg" />
+        <img src="images/icons/apps/64/multimedia-audio-player.svg" />
         <span>Music</span>
     </a>
 
     <a class="app" href="#" target="_blank">
-        <img src="/images/icons/apps/64/multimedia-photo-manager.svg" />
+        <img src="images/icons/apps/64/multimedia-photo-manager.svg" />
         <span>Photos</span>
     </a>
 
     <a class="app" href="#" target="_blank">
-        <img src="/images/icons/apps/64/accessories-text-editor.svg" />
+        <img src="images/icons/apps/64/accessories-text-editor.svg" />
         <span>Scratch</span>
     </a>
 
     <a class="app" href="#" target="_blank">
-        <img src="/images/icons/apps/64/preferences-desktop.svg" />
+        <img src="images/icons/apps/64/preferences-desktop.svg" />
         <span>System Settings</span>
     </a>
 
     <a class="app" href="#" target="_blank">
-        <img src="/images/icons/apps/64/utilities-terminal.svg" />
+        <img src="images/icons/apps/64/utilities-terminal.svg" />
         <span>Terminal</span>
     </a>
 
     <a class="app" href="#" target="_blank">
-        <img src="/images/icons/apps/64/multimedia-video-player.svg" />
+        <img src="images/icons/apps/64/multimedia-video-player.svg" />
         <span>Videos</span>
     </a>
 </div>
