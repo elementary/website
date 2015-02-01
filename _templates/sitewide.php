f340062e9fafc893c24158af231437b14cd58efe
Emersion <contact@emersion.fr>
2015-02-01 20:56:41 +0100

    Adds website root path autodetection

--- _templates/sitewide.php
+++ _templates/sitewide.php
@@ -10,3 +10,37 @@ $sitewide['title'] = 'elementary';
 $sitewide['author'] = 'elementary LLC';
 $sitewide['description'] = 'A fast and open replacement for Windows and OS X. Pay what you want or download for free.';
 $sitewide['theme-color'] = '#3892E0';
+
+// Autodetect website root path
+$serverRoot = $_SERVER['DOCUMENT_ROOT'];
+$websiteRoot = dirname(__DIR__);
+$requestUri = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+
+$sitewide['root'] = '';
+if ($serverRoot == $websiteRoot) {
+	$sitewide['root'] = '/';
+} elseif (strpos($websiteRoot, $serverRoot) === 0) {
+	$sitewide['root'] = substr($websiteRoot, strlen($serverRoot));
+} else {
+	$websiteArray = explode('/', $websiteRoot);
+	$reqArray = array_filter(explode('/', $requestUri));
+	foreach ($websiteArray as $websiteOffset => $websiteDir) {
+		foreach ($reqArray as $reqOffset => $reqDir) {
+			if ($websiteDir != $reqDir) {
+				continue;
+			}
+
+			$max = min(count($websiteArray) - $websiteOffset, count($reqArray) - $reqOffset);
+			for ($i = 0; $i < $max; $i++) {
+				if ($websiteArray[$websiteOffset + $i] != $reqArray[$reqOffset + $i]) {
+					break 2; // Doesn't match
+				}
+			}
+
+			// Matches
+			$rootArray = array_slice($reqArray, 0, $reqOffset - $max + 1);
+			$sitewide['root'] = '/'.implode('/', $rootArray).'/';
+			break 2;
+		}
+	}
+}
