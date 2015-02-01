eec91973082ff1f43fbf635a222338979af65093
Emersion <contact@emersion.fr>
2015-02-01 10:04:18 +0100

    Adds query params support in router

--- router.php
+++ router.php
@@ -6,19 +6,20 @@
  * your local PHP development server. NOT intended for production whatsoever.
  */
 
-$target = null;
+// Allow query parameters to be appended to the request
+$requestUri = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 
-if ($_SERVER["REQUEST_URI"] == '/') {
+if ($requestUri == '/') {
     return false; // Serve the homepage as-is
-} elseif (preg_match('/\./', $_SERVER["REQUEST_URI"])) { // has period in filename
-    $target = '.'.$_SERVER["REQUEST_URI"];
+} elseif (preg_match('/\./', $requestUri)) { // has period in filename
+    $target = '.'.$requestUri;
     if (!file_exists($target)) {
         include '404.php';
     } else {
         return false; // Serve the requested resource as-is
     }
 } else {
-    $target = '.'.$_SERVER["REQUEST_URI"].'.php'; // Rewrite extension-less files as php files
+    $target = '.'.$requestUri.'.php'; // Rewrite extension-less files as php files
     if (file_exists($target)) {
         include $target;
     } else {
