19762c0cee7f1fa2ce12def3607ae113f0d691ea
Emersion <contact@emersion.fr>
2015-02-11 08:02:33 +0100

    Fixes popover position on mobile

--- scripts/popover.js
+++ scripts/popover.js
@@ -2,12 +2,24 @@ $(function () {
 	$(document).on('click', '.popover > a', function (event) {
 		event.preventDefault();
 
-		var $popover = $(event.target).parent();
+		var $link = $(event.target)
+		var $popover = $link.parent();
+		var $content = $popover.find('.popover-content');
 		$popover.addClass('active');
 
+		var popoverPos = ($link.offset().left - $popover.offset().left) / 2;
+		if (popoverPos == 0) {
+			popoverPos -= 100 - $link.width() / 2;
+		}
+		$content.css({ left: popoverPos });
+
 		$(document).one('click', function (event) {
+			if (!$(event.target).is('.popover-content *')) {
+				event.stopImmediatePropagation();
+				event.preventDefault();
+			}
+			
 			$popover.removeClass('active');
-			event.stopImmediatePropagation();
 		});
 	});
 });
