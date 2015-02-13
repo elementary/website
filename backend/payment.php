b7d6bbd79f379bc99305539446f6a93d0b0629bc
Alex Bradaric <alex@bradaric.com>
2015-02-13 22:24:42 +0100

    Encodes email for payment.php POST request and casts a wider catch() block on Stripe_Charge::create() [fixes #381]

--- backend/payment.php
+++ backend/payment.php
@@ -4,7 +4,6 @@ require_once('./config.php');
 
 Stripe::setApiKey($config['stripe_sk']);
 
-
 if (isset($_POST['token'])) {
     $token  = $_POST['token'];
     $amount = $_POST['amount'];
@@ -16,16 +15,17 @@ if (isset($_POST['token'])) {
         'description' => 'elementary OS download',
         'receipt_email' => $receipt
     );
-    // var_dump($options);
 
     // Create the charge on Stripe's servers - this will charge the user's card
     try {
         $charge = Stripe_Charge::create($options);
         // Set an insecure, HTTP only cookie for 10 years in the future.
-        setcookie('has_paid_freya', $amount, time()+315360000, '/', '.elementaryos.org', 0, 1);
+        setcookie('has_paid_freya', $amount, time() + 315360000, '/', '', 0, 1);
+        setcookie('has_paid_freya', $amount, time() + 315360000, '/', '.elementaryos.org', 0, 1);
         echo 'OK';
-    } catch(Stripe_CardError $e) {
-        echo 'error';
+    }
+    catch (Exception $e) {
+        echo $e->getMessage();
     }
 } else {
     echo $config['stripe_pk'];
