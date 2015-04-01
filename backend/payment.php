bad58b77326b62dd211e71940c3cfc7c90ba6b98
Lewis Goddard <lewisgoddard@eustasy.org>
2015-04-01 14:40:31 +0100

    Update payment.php

--- backend/payment.php
+++ backend/payment.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once('./lib/Stripe.php');
-require_once('./backend/config.php');
+require_once('./config.php');
 //require_once('../../../backend/config.php');
 
 Stripe::setApiKey($config['stripe_sk']);
