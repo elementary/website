0871311e447b2ebb5f5d98d63fb33e4a00fbb159
David Overcash <funnylookinhat@gmail.com>
2015-04-01 06:08:09 -0600

    Add email receipts for stripe checkout.

--- backend/payment.php
+++ backend/payment.php
@@ -8,6 +8,11 @@ Stripe::setApiKey($config['stripe_sk']);
 if (isset($_POST['token'])) {
     $token  = $_POST['token'];
     $amount = $_POST['amount'];
+    $email = $_POST['email'];
+
+    // I would add some validation here just to be on the safe side - 
+    // You rarely want to pass $_POST data straight into something without 
+    // being fairly certain of what it is.
 
     // Create the charge on Stripe's servers - this will charge the user's card
     try {
@@ -15,8 +20,9 @@ if (isset($_POST['token'])) {
             'amount' => $amount,
             'currency' => 'usd',
             'card' => $token,
-            'description' => 'elementary OS download')
-        );
+            'description' => 'elementary OS download',
+            'receipt_email' => $email,
+        ));
         // Set an insecure, HTTP only cookie for 10 years in the future.
         setcookie('has_paid_freya', $amount, time()+315360000, '/', '.elementary.io', 0, 1);
         echo 'OK';
