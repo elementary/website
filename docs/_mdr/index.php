1018f0e43ad081ba32b5f8cc7092ccf8e49e93d0
Lewis Goddard <lewisgoddard@eustasy.org>
2015-04-05 11:07:01 +0100

    Update index.php

--- docs/_mdr/index.php
+++ docs/_mdr/index.php
@@ -10,6 +10,7 @@ if (
         // List pages
 
         // Header
+        require_once $MDR['Core'].'/function.url_to_title.php';
         $page['title'] = url_to_title(basename($Request['Directory']));
         include $Templates['Header'];
         echo '<div class="row">';
