6b26aa58d82e673ba072140f4274dc60065ff89f
Lewis Goddard <lewisgoddard@eustasy.org>
2015-04-14 11:38:04 +0100

    Finalise branch_root()

--- backend/function.branchname.php
+++ backend/function.branchname.php
@@ -3,11 +3,8 @@ function branch_root() {
     $Branch = '//'.filter_input(INPUT_SERVER, 'HTTP_HOST').'/';
     $Request_URI = filter_input(INPUT_SERVER, 'REQUEST_URI');
     $Request_URI = explode('/', $Request_URI);
-    var_dump($Request_URI);
     if ( $Request_URI[1] == 'branch' ) {
         $Branch .= 'branch/'.$Request_URI[2];
     }
     return $Branch;
 }
-
-echo branch_root();
