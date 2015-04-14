11bcbdf86527e67d194880741c615a7192702011
Lewis Goddard <lewisgoddard@eustasy.org>
2015-04-14 11:35:10 +0100

    Update function.branchname.php

--- backend/function.branchname.php
+++ backend/function.branchname.php
@@ -1,14 +1,13 @@
 <?php
 function branch_root() {
+    $Branch = '//'.filter_input(INPUT_SERVER, 'HTTP_HOST').'/';
     $Request_URI = filter_input(INPUT_SERVER, 'REQUEST_URI');
     $Request_URI = explode('/', $Request_URI);
     var_dump($Request_URI);
     if ( $Request_URI[1] == 'branch' ) {
-        $Branch = $Request_URI[3];
-    } else {
-        $Branch = false;
+        $Branch .= 'branch/'.$Request_URI[2];
     }
-    return '//'.filter_input(INPUT_SERVER, 'HTTP_HOST').'/'.$Branch;
+    return $Branch;
 }
 
 echo branch_root();
