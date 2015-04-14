e2958f6742a36f0e74c415e26b033a33b1ffcadd
Lewis Goddard <lewisgoddard@eustasy.org>
2015-04-14 11:48:54 +0100

    Update function.branchname.php

--- backend/function.branchname.php
+++ backend/function.branchname.php
@@ -1,5 +1,5 @@
 <?php
-function branch_root() {
+function branch_name() {
     $Branch = '//'.filter_input(INPUT_SERVER, 'HTTP_HOST').'/';
     $Request_URI = filter_input(INPUT_SERVER, 'REQUEST_URI');
     $Request_URI = explode('/', $Request_URI);
