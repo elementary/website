3ba668188b8f6bd6ea6bc122cb1d55a0ba6e3c9b
Lewis Goddard <lewisgoddard@eustasy.org>
2015-04-21 10:29:19 +0100

    Update config.loader.php

--- backend/config.loader.php
+++ backend/config.loader.php
@@ -2,8 +2,11 @@
 
 if ( substr($_SERVER['REQUEST_URI'], 0, 8) == '/branch/' ) {
     // for Branches
-    require_once(__DIR__.'/../../../backend/config.php');
+    require_once __DIR__.'/../../../backend/config.php';
+} else if ( is_readable(__DIR__.'/config.php') ) {
+    // for Configured MASTER
+    require_once __DIR__.'/config.php';
 } else {
-    // for MASTER
-    include(__DIR__.'/config.php');
+    // for un-configured Lobal
+    require_once __DIR__.'/config.example.php';
 }
