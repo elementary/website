7b4454f7ead11cf966102be500d312b76c652067
Lewis Goddard <lewisgoddard@eustasy.org>
2015-04-21 10:24:04 +0100

    payment.php to use config.loader

--- backend/payment.php
+++ backend/payment.php
@@ -1,6 +1,6 @@
 <?php
-require_once(__DIR__.'/lib/Stripe.php');
-require_once(__DIR__.'/config.loader.php');
+require_once __DIR__.'/lib/Stripe.php';
+require_once __DIR__.'/config.loader.php';
 
 Stripe::setApiKey($config['stripe_sk']);
 
