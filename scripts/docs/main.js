2701a11b4d10d28abdb57a6f9507601833506d9e
Cassidy James Blaede <cassidy@system76.com>
2015-04-24 15:54:50 -0600

    Fix next-page link

--- scripts/docs/main.js
+++ scripts/docs/main.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
 
     // Anchor headings
     $('h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]').each(function() {
-        $(this).wrapInner('<a href="'+window.location.pathname+'#'+$(this).attr('id')+'"></a>');
+        $(this).wrapInner('<a class="heading-link" href="'+window.location.pathname+'#'+$(this).attr('id')+'"></a>');
     })
 
     // Sidebar
