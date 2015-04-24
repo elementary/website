4f514baca8b2a2fe8b5a9dffdacf1d20d8a9ca5f
Cassidy James Blaede <cassidy@system76.com>
2015-04-24 15:00:32 -0600

    Fix subheadings in sidebar

--- scripts/docs/main.js
+++ scripts/docs/main.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
 
     // Anchor headings
     $('h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]').each(function() {
-        $(this).wrap('<a href="#'+$(this).attr('id')+'"></a>');
+        $(this).wrapInner('<a href="#'+$(this).attr('id')+'"></a>');
     })
 
     // Sidebar
