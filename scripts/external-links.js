d67adf8896a9f1355f36b856b9f791e0ea44204c
Lewis Goddard <lewisgoddard@eustasy.org>
2015-05-07 09:54:40 +0100

    Reverse

--- scripts/external-links.js
+++ scripts/external-links.js
@@ -1,7 +1,7 @@
 $(function () {
     $('a').not('[href*="mailto:"]').each(function () {
         var href = this.href;
-        if ( href.indexOf(window.location.host) != -1 ) {
+        if ( href.indexOf(window.location.host) == -1 ) {
             $(this).attr('target', '_blank');
         }
     });
