2ea7156128286c3b08d00a88b4faaffa95e150ac
Lewis Goddard <lewisgoddard@eustasy.org>
2015-05-07 09:50:09 +0100

    Fix double {

--- scripts/external-links.js
+++ scripts/external-links.js
@@ -1,5 +1,5 @@
 $(function () {
-    $('a').not('[href*="mailto:"]').each(function () {{
+    $('a').not('[href*="mailto:"]').each(function () {
         var href = this.href;
         if ( href.indexOf(window.location.host) != -1 ) {
             $(this).attr('target', '_blank');
