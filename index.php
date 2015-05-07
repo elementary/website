f3a2c0057bc0fb3956618cb85b04d778dbea6363
Ashesh Kumar <user501254@gmail.com>
2015-05-07 15:25:37 +0530

    Colored thirdparty-logos on hover

--- index.php
+++ index.php
@@ -56,15 +56,15 @@
             <div class="row">
                 <h4 id="the-press">What the press is saying about elementary OS:</h4>
                 <div class="column third">
-                    <a href="http://www.wired.com/2013/11/elementaryos/" target="_blank"><img class="h1" src="images/thirdparty-logos/wired.svg" data-l10n-off alt="WIRED" /></a>
+                    <a href="http://www.wired.com/2013/11/elementaryos/" target="_blank"><img class="h1" src="images/thirdparty-logos/gray_wired.svg" onmouseover="this.src='images/thirdparty-logos/wired.svg'"onmouseout="this.src='images/thirdparty-logos/gray_wired.svg'"  data-l10n-off alt="WIRED" /></a>
                     <a class="inline-tweet" href="http://twitter.com/home/?status=&ldquo;elementary OS is different… a beautiful and powerful operating system that will run well even on old PCs&rdquo; —@WIRED http://elementary.io" target="_blank">&ldquo;elementary OS is different… a beautiful and powerful operating system that will run well even on old PCs&rdquo;</a>
                 </div>
                 <div class="column third">
-                    <a href="http://www.maclife.com/article/columns/future_os_x_may_be_more_elementary_ios_7" target="_blank"><img class="h1" src="images/thirdparty-logos/maclife.svg" data-l10n-off alt="Mac|Life" /></a>
+                    <a href="http://www.maclife.com/article/columns/future_os_x_may_be_more_elementary_ios_7" target="_blank"><img class="h1" src="images/thirdparty-logos/gray_maclife.svg" onmouseover="this.src='images/thirdparty-logos/maclife.svg'"onmouseout="this.src='images/thirdparty-logos/gray_maclife.svg'" data-l10n-off alt="Mac|Life" /></a>
                     <a class="inline-tweet" href="http://twitter.com/home/?status=&ldquo;a fast, low-maintenance platform that can be installed virtually anywhere&rdquo; —@MacLife http://elementary.io" target="_blank">&ldquo;a fast, low-maintenance platform that can be installed virtually anywhere&rdquo;</a>
                 </div>
                 <div class="column third">
-                    <a href="http://lifehacker.com/how-to-move-on-after-windows-xp-without-giving-up-your-1556573928" target="_blank"><img class="h1" src="images/thirdparty-logos/lifehacker.svg" data-l10n-off alt="Lifehacker" /></a>
+                    <a href="http://lifehacker.com/how-to-move-on-after-windows-xp-without-giving-up-your-1556573928" target="_blank"><img class="h1" src="images/thirdparty-logos/gray_lifehacker.svg" onmouseover="this.src='images/thirdparty-logos/lifehacker.svg'"onmouseout="this.src='images/thirdparty-logos/gray_lifehacker.svg'" data-l10n-off alt="Lifehacker" /></a>
                     <a class="inline-tweet" href="http://twitter.com/home/?status=&ldquo;Lightweight and fast… Completely community-based, and has a real flair for design and appearances.&rdquo; —@lifehacker http://elementary.io" target="_blank">&ldquo;Lightweight and fast… Completely community-based, and has a real flair for design and appearances.&rdquo;</a>
                 </div>
             </div>
