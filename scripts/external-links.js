ce90d91616c71ddd1329f5f1f40515db0dd2c3b8
Lewis Goddard <lewisgoddard@eustasy.org>
2015-05-13 17:33:20 +0100

    Update external-links.js

--- scripts/external-links.js
+++ scripts/external-links.js
@@ -1,8 +1,12 @@
 $(function () {
+    var current_page = window.location.pathname;
     $('a').not('[href*="mailto:"]').each(function () {
         var href = this.href;
         if ( href.indexOf(window.location.host) == -1 ) {
             $(this).attr('target', '_blank');
+        }
+        if ( href == current_page ) {
+            $(this).addClass('current-page');
         }
     });
 });
