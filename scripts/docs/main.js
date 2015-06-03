a2b462ad92015ee2bf849de58618c2f63e2e81f1
Blake Kostner <btkostner@gmail.com>
2015-06-02 20:37:57 -0700

    changed doc sidebar to work with hash update

--- scripts/docs/main.js
+++ scripts/docs/main.js
@@ -93,50 +93,13 @@ $(document).ready(function() {
             $sidebar.toggleClass('nav-visible', (scrollTop < navHeight));
             $sidebar.toggleClass('footer-visible', (scrollTop + $(window).height() > footerScrollTop));
 
-            var $current = null;
-            if (scrollTop >= nextTarget) {
-                $headings.each(function () {
-                    var headingScrollTop = Math.floor($(this).offset().top);
-                    if (headingScrollTop > scrollTop) {
-                        $current = $(this).prevAll('h1').first() || $(this);
-                        prevTarget = nextTarget;
-                        nextTarget = headingScrollTop;
-                        return false;
-                    }
-                });
-                if (!$current) {
-                    $current = $headings.last();
-                    prevTarget = nextTarget;
-                    nextTarget = Number.POSITIVE_INFINITY;
-                }
-                if (!prevTarget) {
-                    prevTarget = Math.floor($current.first().offset().top);
-                }
-            }
-            if (scrollTop < prevTarget) {
-                $($headings.get().reverse()).each(function () {
-                    var headingScrollTop = $(this).offset().top;
-                    if (headingScrollTop < scrollTop) {
-                        $current = $(this);
-                        prevTarget = Math.floor(headingScrollTop);
-                        nextTarget = prevTarget;
-                        return false;
-                    }
-                });
-                if (!$current) {
-                    $current = $headings.first();
-                    prevTarget = 0;
-                    nextTarget = $($headings[1]).offset().top;
-                }
-            }
-            if ($current) {
-                $sidebarItems.removeClass('active');
-                var $activeItem = $sidebarItems.children('a[href="#'+$current.attr('id')+'"]').parent();
-                $activeItem.addClass('active');
-                if ($activeItem.next().is('ul')) {
-                    $activeItem.next().addClass('active');
-                }
-            }
+            $('ul.sidebar .active').removeClass('active');
+            var $currentLink = $('ul.sidebar a[href$="#'+currentSection.id+'"]')
+            if ($currentLink.parent().parent().is('.sidebar')) {
+                $currentLink.parent().addClass('active');
+            } else {
+              ($currentLink.parent().parent().addClass('active'));
+            };
         });
         $(window).scroll(); // Trigger event
     }
