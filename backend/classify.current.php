e9e3974a275c5cf0c90271b767743fd6427e3e86
Lewis Goddard <lewisgoddard@eustasy.org>
2015-06-14 21:40:28 +0100

    Fix things.

--- backend/classify.current.php
+++ backend/classify.current.php
@@ -2,10 +2,12 @@
 
 include_once 'classify.functions.php';
 
-if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-	$ip = $_SERVER['HTTP_CLIENT_IP'];
-} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+if (!empty($_SERVER['HTTP_CF_CONNECTING_IP'])) {
+	$ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
+} else if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
 	$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+	$ip = $_SERVER['HTTP_CLIENT_IP'];
 } else {
 	$ip = $_SERVER['REMOTE_ADDR'];
 }
--- backend/classify.get.php
+++ backend/classify.get.php
@@ -4,10 +4,12 @@ include_once 'classify.functions.php';
 
 if (!empty($_GET['IP'])) {
 	$ip = $_GET['IP'];
+} else if (!empty($_SERVER['HTTP_CF_CONNECTING_IP'])) {
+	$ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
+} else if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+	$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
 } else if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
 	$ip = $_SERVER['HTTP_CLIENT_IP'];
-} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-	$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
 } else {
 	$ip = $_GET['REMOTE_ADDR'];
 }
