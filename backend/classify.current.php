840ef87c50aa55ed28efce0d59aa3837e67b0b47
Lewis Goddard <lewisgoddard@eustasy.org>
2015-06-16 20:28:03 +0100

    Fix indentation, add comments

--- backend/classify.current.php
+++ backend/classify.current.php
@@ -3,19 +3,19 @@
 include_once 'classify.functions.php';
 
 if (!empty($_SERVER['HTTP_CF_CONNECTING_IP'])) {
-	$ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
+    $ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
 } else if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-	$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
 } else if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-	$ip = $_SERVER['HTTP_CLIENT_IP'];
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
 } else {
-	$ip = $_SERVER['REMOTE_ADDR'];
+    $ip = $_SERVER['REMOTE_ADDR'];
 }
 
 $region = ipCheck($ip);
 if ( is_array($region) ) {
-	$hash = ipHash($ip);
-	$region = $region[$hash];
+    $hash = ipHash($ip);
+    $region = $region[$hash];
 }
 
 date_default_timezone_set('UTC');
--- backend/classify.get.php
+++ backend/classify.get.php
@@ -3,20 +3,20 @@
 include_once 'classify.functions.php';
 
 if (!empty($_GET['IP'])) {
-	$ip = $_GET['IP'];
+    $ip = $_GET['IP'];
 } else if (!empty($_SERVER['HTTP_CF_CONNECTING_IP'])) {
-	$ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
+    $ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
 } else if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-	$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
 } else if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-	$ip = $_SERVER['HTTP_CLIENT_IP'];
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
 } else {
-	$ip = $_GET['REMOTE_ADDR'];
+    $ip = $_GET['REMOTE_ADDR'];
 }
 
 $region = ipCheck($ip, true);
 if ( is_array($region) ) {
-	$hash = ipHash($ip, true);
-	$region = $region[$hash];
+    $hash = ipHash($ip, true);
+    $region = $region[$hash];
 }
 echo $region;
