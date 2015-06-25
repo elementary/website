bd61a3e1e039b8711fe3d0cd1c2796ccb5183a70
Lewis Goddard <lewisgoddard@eustasy.org>
2015-06-25 12:39:28 +0100

    Fix Translation Checker for Recursive folders

--- backend/translations-checker.php
+++ backend/translations-checker.php
@@ -7,12 +7,29 @@ function isJson($filename) {
 }
 
 function translationFilename($filename) {
-    $filearr = explode('/', $filename);
+    $filearr = explode('lang', $filename);
     end($filearr);
-    return prev($filearr).'/'.next($filearr);
+    return current($filearr);
 }
 
-$translation_files = glob(__DIR__.'/../lang/*/*.json');
+function globRecursive($Pattern, $Flags = 0) {
+    // Search in the Current Directory
+    $Return = glob($Pattern, $Flags);
+    // FOREACHDIRECTORY
+    // Search in ALL sub-directories.
+    foreach (glob(dirname($Pattern).'/*', GLOB_ONLYDIR | GLOB_NOSORT) as $Directory) {
+        // This is a recursive function.
+        // Usually, THIS IS VERY BAD.
+        // For searching recursively however,
+        // it does make some sense.
+        if ( strpos($Directory, '/_') === false ) {
+            $Return = array_merge($Return, globRecursive($Directory.'/'.basename($Pattern), $Flags));
+        }
+    } // FOREACHDIRECTORY
+    return $Return;
+}
+
+$translation_files = globRecursive(__DIR__.'/../lang/*/*.json');
 $valid_files = array();
 $invalid_files = array();
 
