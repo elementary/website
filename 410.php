3e0bf89074c59c27dbb6207a2b4dcf99c13296d7
Lewis Goddard <lewisgoddard@eustasy.org>
2015-07-22 19:38:05 +0100

    Do what cassidy said, mostly.

--- 410.php
+++ 410.php
@@ -10,11 +10,11 @@
 
 <div class="row">
     <div class="column alert">
-        <i class="warning fa fa-warning"></i>
+        <i class="warning fa fa-clock-o"></i>
     </div>
     <div class="column alert">
-        <h3>The download link you clicked has expired</h3>
-        <p>Download links are valid for 3 days.<br>Please go back to our homepage and download again.</p>
+        <h3>Whoops! That link has expired.</h3>
+        <p>If you're trying to download elementary OS, please head back to the home page. If you were looking for something else, feel free to <a href="https://github.com/elementary/mvp/issues/new">file an issue on our GitHub</a>.</p>
     </div>
     <div class="row">
         <a class="button suggested-action" href="/">Go to Home Page</a>
