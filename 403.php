7cb3d9436718a1bdcc457b92d234769f69516953
Richard Denton <isdampe@gmail.com>
2015-07-25 19:36:25 +1000

    Adds friendlier in-page title

--- 403.php
+++ 403.php
@@ -14,7 +14,7 @@
         <i class="warning fa fa-warning"></i>
     </div>
     <div class="column alert">
-        <h3>Access Denied.</h3>
+        <h3>Sorry, access is denied.</h3>
         <p>You don't have permission to view the requested resource.</p>
     </div>
     <div class="row">
