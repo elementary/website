58768c990abaadf949b9f78277985e71be7f75ec
Lewis Goddard <lewisgoddard@eustasy.org>
2015-07-30 20:58:02 +0100

    Fix #768

--- store.php
+++ store.php
@@ -11,6 +11,9 @@
                 <a href='https://mkt.com/elementary-llc' class='sq-embed-menu' data-menu-item-images='large' data-menu-accent-color='04a9dc' data-menu-template='column' data-menu-border='hide' data-menu-item-descriptions='show' >Order Online</a>
                 <script src="https://cdn.sq-api.com/market/embed.js" charset="utf-8"></script>
             </div>
+            <div class="row">
+                <p>All apparel is in US sizes. American Apparel sizes run a bit snug; for sizing details, see <a href="http://www.americanapparel.net/sizing/default.asp?chart=mu.shirts"></a>this chart</a>.</p>
+            </div>
 
 <?php
     include $template['footer'];
