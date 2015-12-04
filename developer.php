61afa78ff6b46140ba56c85303deb9abd67d368e
Daniel Foré <daniel@elementary.io>
2015-12-03 21:42:39 -0800

    simplify

--- developer.php
+++ developer.php
@@ -13,24 +13,22 @@
     <h4>Learn to design, develop, and publish apps for elementary OS</h4>
     <!--<a class="button suggested-action" href="https://myapps.developer.ubuntu.com/dev/click-apps/">Sign In or Register for MyApps</a>-->
 </section>
-<section class="body">
-    <div class="row">
-        <div class="column third"><a href="docs/code/getting-started">
-            <i class="fa fa-book"></i>
-            <h3>Documentation</h3>
-            <p>Get a basic app running, built, and ready for distribution with our Getting Started guide.</p>
-        </a></div>
-        <div class="column third"><a href="docs/human-interface-guidelines">
-            <i class="fa fa-pencil"></i>
-            <h3>Design</h3>
-            <p>Learn about the design principles that make up apps on elementary OS.</p>
-        </a></div>
-        <div class="column third"><a href="docs/code/reference">
-            <i class="fa  fa-code"></i>
-            <h3>Reference</h3>
-            <p>Get more info about code style, reporting issues, and proposing design changes.</p>
-        </a></div>
-    </div>
+<section class="row">
+    <div class="column third"><a href="docs/code/getting-started">
+        <i class="fa fa-book"></i>
+        <h3>Documentation</h3>
+        <p>Get a basic app running, built, and ready for distribution with our Getting Started guide.</p>
+    </a></div>
+    <div class="column third"><a href="docs/human-interface-guidelines">
+        <i class="fa fa-pencil"></i>
+        <h3>Design</h3>
+        <p>Learn about the design principles that make up apps on elementary OS.</p>
+    </a></div>
+    <div class="column third"><a href="docs/code/reference">
+        <i class="fa  fa-code"></i>
+        <h3>Reference</h3>
+        <p>Get more info about code style, reporting issues, and proposing design changes.</p>
+    </a></div>
 </section>
 <?php
     include $template['footer'];
