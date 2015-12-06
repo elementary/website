095e055fe82f6b05f78f9ff4c7f5206300d03048
Daniel Foré <daniel@elementary.io>
2015-12-06 14:49:53 -0800

    Avi's copy for GDA

--- developer.php
+++ developer.php
@@ -68,7 +68,7 @@
         </div>
         <div class="alert column">
             <h2>GDA</h2>
-            <p>Simple, GLib-based database management. It features a metadata extractor, SQL console application, even connect to a database hidden behind a web server. GDA supports all major database formats and can be extended to support others.</p>
+            <p>Simple, flexible database management. Supports remote and on-disk SQL databases, including SQLite. Comes with graphical and in-console SQL data browsers, a metadata extractor enabling object auto-discovery, and more.</p>
             <p><a class="read-more" href="http://valadoc.org/#!api=libgda-4.0/Gda">Reference for GDA</a></p>
         </div>
     </div>
