62f66f3cd667fb5e62a9a930878bb803d956d012
Raj <charanraj.tc@gmail.com>
2016-02-06 16:34:30 +0400

    added create customers in stripe done

--- backend/payment.php
+++ backend/payment.php
@@ -1,39 +1,87 @@
 <?php
-require_once __DIR__.'/lib/Stripe.php';
-require_once __DIR__.'/config.loader.php';
-require_once __DIR__.'/secure.functions.php';
+require_once __DIR__ . '/lib/Stripe.php';
+require_once __DIR__ . '/config.loader.php';
+require_once __DIR__ . '/secure.functions.php';
+require_once __DIR__ . '/stripe.cusid.functions.php';
 
 Stripe::setApiKey($config['stripe_sk']);
 
 if (isset($_POST['token'])) {
-    $token  = $_POST['token'];
-    $amount = $_POST['amount'];
-    $email = $_POST['email'];
+	$token = $_POST['token'];
+	$amount = $_POST['amount'];
+	$email = $_POST['email'];
 
-    // Create the charge on Stripe's servers - this will charge the user's card
-    try {
-        $charge = Stripe_Charge::create(array(
-            'amount' => $amount,
-            'currency' => 'usd',
-            'source' => $token,
-            // 'card' => $token,
-            'description' => 'elementary OS download',
-            'receipt_email' => $email,
-        ));
-        // var_dump($charge['paid']);
-        if($charge['paid']==true){
-            echo 'PAID';
-                // Set an insecure, HTTP only cookie for 10 years in the future.
-                setcookie('has_paid_'.$config['os-codename'].'', $amount, time() + 315360000, '/', '.elementary.local', 0, 1);
+	// Create the charge on Stripe's servers - this will charge the user's card
+	try {
 
-                //securing email in cookies for privacy protection 
-                setcookie('paid_'.$config['os-codename'].'_by',encrypt($email), time() + 315360000, '/', '.elementary.local', 0, 1);
-        }else {
-            echo 'OK';    
-        }
-    } catch(Stripe_CardError $e) {
-        echo 'error';
-    }
+		$stripecusid = customerHasStripeAccont($email);
+		$customer = null;
+
+		//check if the customer has stripe account with our db
+		if ($stripecusid === null) {
+			$customer = Stripe_Customer::create(array(
+				// "source" => $token ,
+				"email" => $email,
+				"source" => $token,
+			));
+			$stripecusid = $customer->id;
+		} else {
+
+			$customerdeltedatStripe = false;
+			try
+			{
+				// our db has customer account check same with stripe account
+				$customer = Stripe_Customer::retrieve($stripecusid);
+				$customerdeltedatStripe = $customer->deleted;
+				$stripecusid = null;
+			} catch (Stripe_InvalidRequestError $e) {
+				$customerdeltedatStripe = true;
+			}
+			if ($customerdeltedatStripe) {
+				// our db has account but stripe dont have account reference
+				$customer = Stripe_Customer::create(array(
+					"email" => $email,
+					"source" => $token,
+				));
+				$stripecusid = $customer->id;
+				//delte all ol refences to the customer
+				deleteStripeAccountReferences($email);
+			}
+		}
+
+		addStripeCustomerToDB($email, $customer->id);
+
+		// $stripecusid is only set when a new cousmore is added
+		if ($stripecusid == null) {
+			//add a source for that customer adds duplicate cards in th stripe db  TODO: try to check exitsing sources if not add one
+			$cardid = $customer->sources->create(array("source" => $token));
+			//set default source
+			$customer->default_source = $cardid->id;
+			$customer->save();
+		}
+
+		$charge = Stripe_Charge::create(array(
+			'amount' => $amount,
+			'currency' => 'usd',
+			'customer' => $customer->id,
+			'description' => 'elementary OS download',
+			'receipt_email' => $email,
+		));
+
+		// var_dump($charge['paid']);
+		if ($charge['paid'] == true) {
+			echo 'PAID';
+			// Set an insecure, HTTP only cookie for 10 years in the future.
+			setcookie('has_paid_' . $config['os-codename'] . '', $amount, time() + 315360000, '/', '.elementary.local', 0, 1);
+
+			//securing email in cookies for privacy protection
+			setcookie('paid_' . $config['os-codename'] . '_by', encrypt($email), time() + 315360000, '/', '.elementary.local', 0, 1);
+		} else {
+			echo 'OK';
+		}
+	} catch (Stripe_CardError $e) {
+		echo 'error';
+	}
 } else {
-    echo $config['stripe_pk'];
+	echo $config['stripe_pk'];
 }
