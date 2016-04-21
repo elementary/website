edfdd965b64e4a196eaa4fa02470b7ac68b86573
Lewis Goddard <lewisgoddard@eustasy.org>
2016-04-21 12:53:18 +0100

    Trim when too large

--- scripts/twitter-links.js
+++ scripts/twitter-links.js
@@ -1,5 +1,10 @@
 $(function () {
     $('.inline-tweet').each(function () {
-        $(this).prop('href', 'http://twitter.com/home/?status=' + encodeURIComponent($(this).text()) + $(this).data('tweet-suffix'));
+        var tweet = encodeURIComponent($(this).text() + $(this).data('tweet-suffix'));
+        if ( tweet.length > 140 ) {
+            var trim_length = 140 - $(this).data('tweet-suffix').length - 1;
+            tweet = encodeURIComponent($(this).text().substring(0, trim_length) + '…' + $(this).data('tweet-suffix'));
+        }
+        $(this).prop('href', 'http://twitter.com/home/?status=' + tweet);
     });
 });
