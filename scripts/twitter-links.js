0072685e4ff491d84afee16387a78d3935c162f5
Lewis Goddard <lewisgoddard@eustasy.org>
2016-04-23 01:01:51 +0100

    Trim tweets a little more

--- scripts/twitter-links.js
+++ scripts/twitter-links.js
@@ -2,7 +2,7 @@ $(function () {
     $('.inline-tweet').each(function () {
         var tweet = encodeURIComponent($(this).text() + $(this).data('tweet-suffix'));
         if ( tweet.length > 140 ) {
-            var trim_length = 140 - $(this).data('tweet-suffix').length - 1;
+            var trim_length = 140 - $(this).data('tweet-suffix').length - 5;
             tweet = encodeURIComponent($(this).text().substring(0, trim_length) + '…' + $(this).data('tweet-suffix'));
         }
         $(this).prop('href', 'http://twitter.com/home/?status=' + tweet);
