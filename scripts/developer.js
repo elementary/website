788c4fa0a46ee82278967c442d51f281a6b1c7c7
Lewis Goddard <lewisgoddard@eustasy.org>
2016-06-13 22:34:51 +0100

    Add tracking to developer

--- scripts/developer.js
+++ scripts/developer.js
@@ -4,10 +4,14 @@
 $(function() {
     $('#download').bind('click', function(e) {
         e.preventDefault();
-
         $('.open-modal').leanModal({
             closeButton: '.close-modal',
         });
         $('.open-modal').click();
+    });
+    $('#download-modal a.suggested-action').click(function(){
+        if (window.ga) {
+            ga('send', 'event', release_title + ' ' + release_version + ' Download (Beta)', 'Developer');
+        }
     });
 })
