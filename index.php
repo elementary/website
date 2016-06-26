b185489e5c623546bbc55e6952e10daf8aa2fb71
Lewis Goddard <lewisgoddard@eustasy.org>
2016-06-26 21:47:23 +0100

    Upgrade to leanModal 2.5

--- index.php
+++ index.php
@@ -230,19 +230,17 @@
             <span id="translate-download" style="display:none;" hidden>Download elementary OS</span>
             <span id="translate-purchase" style="display:none;" hidden>Purchase elementary OS</span>
             <div id="download-modal" class="modal">
-                <div class="modal-container">
-                    <i class="fa fa-close close-modal"></i>
-                    <h3>Choose a Download</h3>
-                    <p>We recommend 64-bit for most modern computers. For help and more info, see the <a class="read-more" href="docs/installation" target="_blank">installation guide</a></p>
-                    <div class="row actions">
-                        <div class="column linked">
-                            <a class="button close-modal" href="<?php echo $download_link; ?>elementaryos-0.3.2-stable-i386.20151209.iso">Freya 32-bit</a>
-                            <a class="button close-modal" title="Torrent Magnet Link" href="magnet:?xt=urn:btih:001b104e49d517cf7a41593a73c3861b7c8e34f8&dn=elementaryos-0.3.2-stable-i386.20151209.iso&tr=https%3A%2F%2Fashrise.com%3A443%2Fphoenix%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.ccc.de%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80%2Fannounce&ws=http:<?php echo $download_link; ?>elementaryos-0.3.2-stable-i386.20151209.iso"><i class="fa fa-magnet"></i></a>
-                        </div>
-                        <div class="column linked">
-                            <a class="button suggested-action close-modal" href="<?php echo $download_link; ?>elementaryos-0.3.2-stable-amd64.20151209.iso">Freya 64-bit</a>
-                            <a class="button suggested-action close-modal" title="Torrent Magnet Link" href="magnet:?xt=urn:btih:fce720af722a813a184c5550a924aaa60a8d9af1&dn=elementaryos-0.3.2-stable-amd64.20151209.iso&tr=https%3A%2F%2Fashrise.com%3A443%2Fphoenix%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.ccc.de%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80%2Fannounce&ws=http:<?php echo $download_link; ?>elementaryos-0.3.2-stable-amd64.20151209.iso"><i class="fa fa-magnet"></i></a>
-                        </div>
+                <i class="fa fa-close close-modal"></i>
+                <h3>Choose a Download</h3>
+                <p>We recommend 64-bit for most modern computers. For help and more info, see the <a class="read-more" href="docs/installation" target="_blank">installation guide</a></p>
+                <div class="row actions">
+                    <div class="column linked">
+                        <a class="button close-modal" href="<?php echo $download_link; ?>elementaryos-0.3.2-stable-i386.20151209.iso">Freya 32-bit</a>
+                        <a class="button close-modal" title="Torrent Magnet Link" href="magnet:?xt=urn:btih:001b104e49d517cf7a41593a73c3861b7c8e34f8&dn=elementaryos-0.3.2-stable-i386.20151209.iso&tr=https%3A%2F%2Fashrise.com%3A443%2Fphoenix%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.ccc.de%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80%2Fannounce&ws=http:<?php echo $download_link; ?>elementaryos-0.3.2-stable-i386.20151209.iso"><i class="fa fa-magnet"></i></a>
+                    </div>
+                    <div class="column linked">
+                        <a class="button suggested-action close-modal" href="<?php echo $download_link; ?>elementaryos-0.3.2-stable-amd64.20151209.iso">Freya 64-bit</a>
+                        <a class="button suggested-action close-modal" title="Torrent Magnet Link" href="magnet:?xt=urn:btih:fce720af722a813a184c5550a924aaa60a8d9af1&dn=elementaryos-0.3.2-stable-amd64.20151209.iso&tr=https%3A%2F%2Fashrise.com%3A443%2Fphoenix%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.ccc.de%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80%2Fannounce&ws=http:<?php echo $download_link; ?>elementaryos-0.3.2-stable-amd64.20151209.iso"><i class="fa fa-magnet"></i></a>
                     </div>
                 </div>
             </div>
--- scripts/jQuery.leanModal2.js
+++ scripts/jQuery.leanModal2.js
@@ -1,69 +1,131 @@
-// jQuery.leanModal2.js v2.2 (edited)
-// MIT Licensed by eustasy http://eustasy.org
+////	jQuery.leanModal2.js v2.5
+// MIT Licensed by eustasy https://eustasy.org
 // Based on leanModal v1.1 by Ray Stone - http://finelysliced.com.au
 
-// Wrap in an anonymous function.
+// ANONFUNC Wrap in an anonymous function.
 (function($){
 
-	// Function: Fade out the Overlay and a passed identifier
-	function leanModal_Close(modal_id) {
-		$(modal_id).animate({'opacity': 0}, 200, function() {
-			$(modal_id).removeClass('active')
-		})
-	}
-
-	// Define a new Extension.
+	////	Extend jQuery
+	// EXTENDFUNC
 	$.fn.extend({
+		// EXTENDFUNC_LEANMODAL
 		leanModal: function(options) {
 
-			// FORLINK For each targetted link.
-			return this.each(function() {
+			////	Default Options
+			// Set some Defaults.
+			var defaults = {
+				top: '15vh',
+				overlayOpacity: 0.7,
+				closeButton: '.js-leanmodal-close',
+				disableCloseOnOverlayClick: false,
+				disableCloseOnEscape: false,
+				fadeTime: 200,
+				modalCenter: true,
+			};
+			// Merge in any passed options.
+			options = $.extend(defaults, options);
 
+			////	Close the Modal
+			// FUNCTION: Fade out the overlay and a passed identifier.
+			function leanModal_Close(modal_id) {
+				$('.js-leanmodal-overlay').fadeOut(options.fadeTime);
+				$(modal_id).fadeOut(options.fadeTime);
+				$('.js-leanmodal-overlay').unbind('click');
+				$(document).off('keyup');
+			}
+
+			////	There can be only one.
+			// Overlay. If there isn't an overlay, add one.
+			if ( $('.js-leanmodal-overlay').length == 0 ) {
+				var style = 'background: #000; display: none; height: 100%; left: 0; position: fixed; top: 0; width: 100%; z-index: 100;';
+				var overlay = $('<div class="js-leanmodal-overlay" style="' + style + '"></div>');
+				$('body').append(overlay);
+			}
+
+			////	Everything is Linked
+			// FOREACHLINK For each targeted link.
+			return this.each(function() {
+				// Force this to look like a link.
 				$(this).css({ 'cursor': 'pointer' });
 
+				////	Command Override
+				// Override the existing click command,
+				// and insert this new one.
+				// CLICKOVERRIDE
 				$(this).unbind('click').click(function(e) {
 
-					// IFHREF Fetch the Modal_ID
-					if ( $(this).attr('href') ) {
-						var modal_id = $(this).attr('href');
-					// IFHREF Fall back to if no href data-open-modal.
-					} else if ( $(this).attr('data-modal-id') ) {
+					////	Select the Modal Identifier
+					// IFHREF Use data-open-modal if available
+					if ( $(this).attr('data-modal-id') ) {
 						var modal_id = $(this).attr('data-modal-id');
+					// IFHREF Fall back to href
+					} else if ( $(this).attr('href') ) {
+						var modal_id = $(this).attr('href');
+					// IFHREF Fail entirely.
 					} else {
 						return false;
 					} // IFHREF
 
-					// Set the function to close the overlay if you outside it.
-					$('.modal').click(function(e) {
-						if (e.target != this) return
-
-						leanModal_Close(modal_id);
-					});
-
-					// If a close button is set, link it to the close command.
+					////	Close with closeButton
+					// If `closeButton` is set,
+					// use it to call the close command.
 					if ( options.closeButton ) {
 						$(options.closeButton).click(function() {
 							leanModal_Close(modal_id);
 						});
 					}
 
-					// Close the modal on escape
-					$(document).on('keyup', function(evt) {
-						if (evt.keyCode == 27) {
+					////	Escape with `Esc`
+					// Close the modal when someone presses `Esc`,
+					// except when `disableCloseOnEscape` is set to `true`
+					if ( !options.disableCloseOnEscape ) {
+						$(document).on('keyup', function(evt) {
+							if ( evt.keyCode == 27 ) {
+								leanModal_Close(modal_id);
+							}
+						});
+					}
+
+					////	Envelope in Darkness
+					// Close the modal when someone clicks on the overlay,
+					// except when `disableCloseOnOverlayClick` is set to `true`
+					if ( !options.disableCloseOnOverlayClick ) {
+						$('.js-leanmodal-overlay').click(function() {
 							leanModal_Close(modal_id);
-						}
-					});
+						});
+					}
 
-					$(modal_id).addClass('active').animate({'opacity': 1}, 200)
+					////	Modal Positioning
+					// Position the modal centrally using JavaScript, because CSS sucks.
+					// Actually it doesn't, but it is hard to globally position.
+					var modal_height = $(modal_id).innerHeight();
+					var modal_width = $(modal_id).innerWidth();
+					if ( options.modalCenter ) {
+						$(modal_id).css({
+							'display': 'block',
+							//'left': 50 + '%',
+							//'margin-left': - ( modal_width / 2 ) + 'px',
+							'margin-left': '50%',
+							'transform': 'translateX(-50%)',
+							'opacity': 0,
+							'position': 'fixed',
+							'top': options.top,
+							'z-index': 11000,
+						});
+					}
 
+					////	Curtain Up
+					// Fade in the modal and overlay.
+					$('.js-leanmodal-overlay').css({ 'display': 'block', opacity: 0 });
+					$('.js-leanmodal-overlay').fadeTo(options.fadeTime, options.overlayOpacity);
+					$(modal_id).fadeTo(options.fadeTime, 1);
+
+					////	Default Prevention
 					// Prevent whatever the default was (probably scrolling).
 					e.preventDefault();
 
-				});
-			}); // FORLINK
-
-		}
-	});
-
-// Apparently this jQuery is important.
-})(jQuery);
+				}); // CLICKOVERRIDE
+			}); // FOREACHLINK
+		} // EXTENDFUNC_LEANMODAL
+	}); // EXTENDFUNC
+})(jQuery); // ANONFUNC This passes in `jQuery` as `$`
