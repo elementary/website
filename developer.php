f152219ffe264cc985cba72ffd4e4789268eb782
Daniel Foré <daniel@elementary.io>
2016-07-28 13:20:16 -0700

    use new network icon

--- developer.php
+++ developer.php
@@ -90,7 +90,7 @@
     </div>
     <div class="half">
         <div class="alert column">
-            <img src="images/developer/soup.svg" alt="Soup">
+            <img src="images/icons/48/preferences-system-network.svg" alt="Soup">
         </div>
         <div class="alert column">
             <h2>Soup</h2>
