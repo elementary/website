119241d07d0a3831265f3a3a599d16a9316ea2b6
Sam Hewitt <hewittsamuel@gmail.com>
2016-08-05 12:58:23 -0400

    update layout & copy

--- open-source.php
+++ open-source.php
@@ -15,13 +15,21 @@
     </div>
 </div>
 <div class="row">
-    <div class="platform-item full">
+    <div class="platform-item half">
         <img class="oss-logo" src="images/open-source/apps.svg" alt="Linux">
-        <h3 class="oss-title">elementary Applications</h3>
+        <h3 class="oss-title"><a href="https://elementary.io/get-involved#desktop-development">Apps</a></h3>
+        <p class="oss-subtitle">elementary OS Applications</p>
+        <div>
+            <span class="sub-item"><a href="https://launchpad.net/noise" target="_blank">Music</a></span>
+            <span class="sub-item"><a href="https://launchpad.net/pantheon-files" target="_blank">Files</a></span>
+            <span class="sub-item"><a href="https://launchpad.net/pantheon-photos" target="_blank">Photos</a></span>
+            <span class="sub-item"><a href="https://launchpad.net/pantheon-mail" target="_blank">Mail</a></span>
+            <span class="sub-item"><a href="https://launchpad.net/pantheon-terminal" target="_blank">Terminal</a></span>
+        </div>
     </div>
-    <div class="platform-item third">
+    <div class="platform-item half">
         <img class="oss-logo" src="images/open-source/desktop.svg" alt="Linux">
-        <h3 class="oss-title">Pantheon</h3>
+        <h3 class="oss-title"><a href="https://elementary.io/get-involved#desktop-development">Pantheon</a></h3>
         <p class="oss-subtitle">elementary OS Desktop</p>
         <div>
             <span class="sub-item"><a href="https://launchpad.net/gala" target="_blank">Gala</a></span>
@@ -29,6 +37,7 @@
             <span class="sub-item"><a href="https://launchpad.net/plank" target="_blank">Plank</a></span>
         </div>
     </div>
+    <hr class="dotted full">
     <div class="platform-item third">
         <img class="oss-logo" src="images/open-source/gtk.svg" alt="Linux">
         <h3 class="oss-title"><a href="http://gtk.org/" target="_blank">GTK+</a></h3>
@@ -38,7 +47,7 @@
             <span class="sub-item"><a href="https://cairographics.org/" target="_blank">Cairo</a></span>
         </div>
     </div>
-    <div class="platform-item third">
+    <div class="platform-item two-thirds">
         <img class="oss-logo" src="images/open-source/gnome.svg" alt="Linux">
         <h3 class="oss-title"><a href="https://gnome.org/" target="_blank">GNOME</a></h3>
         <p class="oss-subtitle">Desktop libraries</p>
@@ -60,7 +69,7 @@
     <div class="platform-item half">
         <img class="oss-logo" src="images/open-source/freedesktop.svg" alt="X11">
         <h3 class="oss-title"><a href="https://freedesktop.org/" target="_blank">FreeDesktop.org</a></h3>
-        <p class="oss-subtitle">Base technology &amp; backend</p>
+        <p class="oss-subtitle">Base Technology</p>
         <div>
             <span class="sub-item"><a href="https://www.freedesktop.org/wiki/Software/systemd/" target="_blank">systemd</a></span>
             <span class="sub-item"><a href="https://gstreamer.freedesktop.org/" target="_blank">GStreamer</a></span>
@@ -68,15 +77,23 @@
             <span class="sub-item"><a href="https://www.freedesktop.org/wiki/Software/dbus/" target="_blank">D-Bus</a></span>
         </div>
     </div>
+    <div class="platform-item two-thirds">
+        <img class="oss-logo" src="images/open-source/linux.svg" alt="Linux">
+        <h3 class="oss-title">Linux</h3>
+        <p class="oss-subtitle">Hardware support &amp; drivers</p>
+        <div>
+            <span class="sub-item"><a href="https://kernel.org/" target="_blank">Kernel</a></span>
+        </div>
+    </div>
     <div class="platform-item third">
         <img class="oss-logo" src="images/open-source/gnu.svg" alt="GNU">
         <h3 class="oss-title"><a href="http://gnu.org/" target="_blank">GNU</a></h3>
-        <p class="oss-subtitle">Operating system</p>
-    </div>
-    <div class="platform-item two-thirds">
-        <img class="oss-logo" src="images/open-source/linux.svg" alt="Linux">
-        <h3 class="oss-title">Linux Kernel</h3>
-        <p class="oss-subtitle">Device drivers, modules</p>
+        <p class="oss-subtitle">Complier &amp; Utilities</p>
+        <div>
+            <span class="sub-item"><a href="https://www.gnu.org/software/bash/" target="_blank">bash</a></span>
+            <span class="sub-item"><a href="https://www.gnu.org/software/gcc/" target="_blank">GCC</a></span>
+            <span class="sub-item"><a href="https://www.gnu.org/software/coreutils/" target="_blank">Coreutils</a></span>
+        </div>
     </div>
 </div>
 
