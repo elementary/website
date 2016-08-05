1d34d336eb1d17ad3d285b689b87f563ee3af673
Sam Hewitt <hewittsamuel@gmail.com>
2016-08-05 13:49:39 -0400

    update open-source page

--- open-source.php
+++ open-source.php
@@ -20,6 +20,8 @@
         <img class="oss-logo" src="images/open-source/apps.svg" alt="Applications">
         <p class="oss-subtitle">elementary OS Applications</p>
         <div>
+            <span class="sub-item"><a href="https://launchpad.net/maya" target="_blank">Calendar</a></span>
+            <span class="sub-item"><a href="https://launchpad.net/snap-elementary" target="_blank">Camera</a></span>
             <span class="sub-item"><a href="https://launchpad.net/pantheon-files" target="_blank">Files</a></span>
             <span class="sub-item"><a href="https://launchpad.net/pantheon-mail" target="_blank">Mail</a></span>
             <span class="sub-item"><a href="https://launchpad.net/noise" target="_blank">Music</a></span>
@@ -37,12 +39,13 @@
             <span class="sub-item"><a href="https://launchpad.net/gala" target="_blank">Gala</a></span>
             <span class="sub-item"><a href="https://launchpad.net/granite" target="_blank">Granite</a></span>
             <span class="sub-item"><a href="https://launchpad.net/plank" target="_blank">Plank</a></span>
+            <span class="sub-item"><a href="https://launchpad.net/slingshot" target="_blank">Slingshot</a></span>
             <span class="sub-item"><a href="https://launchpad.net/switchboard" target="_blank">Switchboard</a></span>
             <span class="sub-item"><a href="https://launchpad.net/wingpanel" target="_blank">Wingpanel</a></span>
         </div>
     </div>
     <hr class="dotted full">
-    <div class="platform-item half">
+    <div class="platform-item third">
         <a href="http://gtk.org/" target="_blank"><img class="oss-logo" src="images/open-source/gtk.svg" alt="GTK+"></a>
         <h3 class="oss-title">GTK+</h3>
         <p class="oss-subtitle">User Interface Toolkit</p>
@@ -52,15 +55,30 @@
             <span class="sub-item"><a href="https://cairographics.org/" target="_blank">Cairo</a></span>
         </div>
     </div>
-    <div class="platform-item half">
+    <div class="platform-item two-thirds">
         <a href="https://gnome.org/" target="_blank"><img class="oss-logo" src="images/open-source/gnome.svg" alt="GNOME"></a>
         <h3 class="oss-title">GNOME</h3>
         <p class="oss-subtitle">Desktop Libraries</p>
         <div>
-            <span class="sub-item"><a href="https://wiki.gnome.org/Projects/Vala" target="_blank">Vala</a></span>
-            <span class="sub-item"><a href="https://wiki.gnome.org/Projects/GLib" target="_blank">GLib</a></span>
             <span class="sub-item"><a href="https://wiki.gnome.org/Accessibility" target="_blank">ATK</a></span>
+            <span class="sub-item"><a href="https://wiki.gnome.org/Projects/dconf" target="_blank">D-Conf</a></span>
             <span class="sub-item"><a href="https://wiki.gnome.org/Projects/Clutter" target="_blank">Clutter</a></span>
+            <span class="sub-item"><a href="https://wiki.gnome.org/Projects/GLib" target="_blank">GLib</a></span>
+            <span class="sub-item"><a href="https://wiki.gnome.org/Projects/PolicyKit" target="_blank">PolicyKit</a></span>
+            <span class="sub-item"><a href="https://wiki.gnome.org/Projects/Vala" target="_blank">Vala</a></span>
+        </div>
+    </div>
+    <div class="platform-item two-thirds">
+        <a href="https://freedesktop.org/" target="_blank"><img class="oss-logo" src="images/open-source/freedesktop.svg" alt="FreeDesktop"></a>
+        <h3 class="oss-title">FreeDesktop.org</h3>
+        <p class="oss-subtitle">Base Technology</p>
+        <div>
+            <span class="sub-item"><a href="https://www.freedesktop.org/wiki/Software/dbus/" target="_blank">D-Bus</a></span>
+            <span class="sub-item"><a href="https://gstreamer.freedesktop.org/" target="_blank">GStreamer</a></span>
+            <span class="sub-item"><a href="https://www.freedesktop.org/wiki/Software/libinput/" target="_blank">libinput</a></span>
+            <span class="sub-item"><a href="https://www.freedesktop.org/wiki/Software/lightdm/" target="_blank">LightDM</a></span>
+            <span class="sub-item"><a href="https://www.freedesktop.org/wiki/Software/PulseAudio/" target="_blank">PulseAudio</a></span>
+            <span class="sub-item"><a href="https://www.freedesktop.org/wiki/Software/systemd/" target="_blank">systemd</a></span>
         </div>
     </div>
     <div class="platform-item third">
@@ -72,27 +90,7 @@
             <span class="sub-item"><a href="https://wayland.freedesktop.org/xserver.html" target="_blank">XWayland</a></span>
         </div>
     </div>
-    <div class="platform-item two-thirds">
-        <a href="https://freedesktop.org/" target="_blank"><img class="oss-logo" src="images/open-source/freedesktop.svg" alt="FreeDesktop"></a>
-        <h3 class="oss-title">FreeDesktop.org</h3>
-        <p class="oss-subtitle">Base Technology</p>
-        <div>
-            <span class="sub-item"><a href="https://www.freedesktop.org/wiki/Software/systemd/" target="_blank">systemd</a></span>
-            <span class="sub-item"><a href="https://gstreamer.freedesktop.org/" target="_blank">GStreamer</a></span>
-            <span class="sub-item"><a href="https://www.freedesktop.org/wiki/Software/PulseAudio/" target="_blank">PulseAudio</a></span>
-            <span class="sub-item"><a href="https://www.freedesktop.org/wiki/Software/dbus/" target="_blank">D-Bus</a></span>
-            <span class="sub-item"><a href="https://www.freedesktop.org/wiki/Software/libinput/" target="_blank">libinput</a></span>
-        </div>
-    </div>
-    <div class="platform-item two-thirds">
-        <img class="oss-logo" src="images/open-source/linux.svg" alt="Linux">
-        <h3 class="oss-title">Linux</h3>
-        <p class="oss-subtitle">Hardware Support &amp; drivers</p>
-        <div>
-            <span class="sub-item"><a href="https://kernel.org/" target="_blank">Kernel</a></span>
-        </div>
-    </div>
-    <div class="platform-item third">
+    <div class="platform-item half">
         <a href="http://gnu.org/" target="_blank"><img class="oss-logo" src="images/open-source/gnu.svg" alt="GNU"></a>
         <h3 class="oss-title">GNU</h3>
         <p class="oss-subtitle">Complier &amp; Core Utilities</p>
@@ -100,6 +98,14 @@
             <span class="sub-item"><a href="https://www.gnu.org/software/bash/" target="_blank">bash</a></span>
             <span class="sub-item"><a href="https://www.gnu.org/software/gcc/" target="_blank">GCC</a></span>
             <span class="sub-item"><a href="https://www.gnu.org/software/coreutils/" target="_blank">Coreutils</a></span>
+        </div>
+    </div>
+    <div class="platform-item half">
+        <img class="oss-logo" src="images/open-source/linux.svg" alt="Linux">
+        <h3 class="oss-title">Linux</h3>
+        <p class="oss-subtitle">Hardware Support &amp; drivers</p>
+        <div>
+            <span class="sub-item"><a href="https://kernel.org/" target="_blank">Kernel</a></span>
         </div>
     </div>
 </div>
