bc5d3c050863dd1d0761b6871b3044e00ea42499
Sam Hewitt <hewittsamuel@gmail.com>
2016-08-05 11:53:01 -0400

    updates to open-source page

--- open-source.php
+++ open-source.php
@@ -6,46 +6,76 @@
     include $template['alert'];
 ?>
 
-<div class="grid">
-    <div class="nested top-level"?>
-        <div id="logotype">
-            <?php include('images/logotype-os.svg'); ?>
+<div class="row">
+    <div id="logotype">
+        <?php include('images/logotype-os.svg'); ?>
+    </div>
+    <div class="platform-item full">
+        <img class="oss-logo" src="images/open-source/apps.svg" alt="Linux">
+        <h3 class="oss-title">elementary Applications</h3>
+    </div>
+    <div class="platform-item third">
+        <img class="oss-logo" src="images/open-source/desktop.svg" alt="Linux">
+        <h3 class="oss-title">Pantheon</h3>
+        <p class="oss-subtitle">elementary OS Desktop</p>
+        <div>
+            <span class="sub-item"><a href="https://launchpad.net/gala" target="_blank">Gala</a></span>
+            <span class="sub-item"><a href="https://launchpad.net/wingpanel" target="_blank">Wingpanel</a></span>
+            <span class="sub-item"><a href="https://launchpad.net/plank" target="_blank">Plank</a></span>
         </div>
-        <p>elementary OS is built and relies on many open source projects to exist.</p>
-        <div class="nested foundation">
-            <div class="two-thirds">
-                <img src="images/open-source/ubuntu.svg" alt="Ubuntu">
-                <h2>Ubuntu</h2>
-                <p>The Ubuntu LTS software repositories serve as a stable basis for elementary OS.</p>
-                <p><a class="read-more" target="_blank" href="http://ubuntu.com/">Ubuntu Homepage</a></p>
-            </div>
-            <div class="nested libraries">
-                <div class="two-thirds">
-                    <img class="oss-logo" src="images/open-source/gtk.svg" alt="GTK+">
-                    <img class="oss-logo" src="images/open-source/gnome.svg" alt="GNOME">
-                    <h2>GTK+ &amp; GNOME</h2>
-                    <p>elementary OS is built using many GNOME Project technologies and libraries. The graphical user interface toolkit (GTK+) enables the creation of elementary OS's modern, user-friendly interface.</p>
-                    <p><a class="read-more" target="_blank" href="http://gtk.org/">GTK+ Homepage</a></p>
-                    <p><a class="read-more" target="_blank" href="http://gnome.org/">GNOME Homepage</a></p>
-                </div>
-                <div class="nested display">
-                    <div class="two-thirds">
-                        <img class="oss-logo" src="images/open-source/xorg.svg" alt="X.Org">
-                        <h2>X.Org</h2>
-                        <p>The open source implementation of the X Window System and the X.org display server power the elementary OS desktop environment.</p>
-                        <p><a class="read-more white" href="http://x.org/">X.Org Homepage</a></p>
-                    </div>
-                    <div class="nested low-level">
-                        <div class="two-thirds">
-                            <img class="oss-logo" src="images/open-source/linux.svg" alt="Linux">
-                            <h2>Linux Kernel</h2>
-                            <p>elementary OS is built upon the robust Linux kernel so it can run on virtually any computer.</p>
-                            <p><a class="read-more" target="_blank" href="http://kernel.org/">Linux Kernel Homepage</a></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+    </div>
+    <div class="platform-item third">
+        <img class="oss-logo" src="images/open-source/gtk.svg" alt="Linux">
+        <h3 class="oss-title"><a href="http://gtk.org/" target="_blank">GTK+</a></h3>
+        <p class="oss-subtitle">User interface toolkit</p>
+        <div>
+            <span class="sub-item"><a href="http://www.pango.org/" target="_blank">Pango</a></span>
+            <span class="sub-item"><a href="https://cairographics.org/" target="_blank">Cairo</a></span>
         </div>
+    </div>
+    <div class="platform-item third">
+        <img class="oss-logo" src="images/open-source/gnome.svg" alt="Linux">
+        <h3 class="oss-title"><a href="https://gnome.org/" target="_blank">GNOME</a></h3>
+        <p class="oss-subtitle">Desktop libraries</p>
+        <div>
+            <span class="sub-item"><a href="https://wiki.gnome.org/Projects/GLib" target="_blank">GLib</a></span>
+            <span class="sub-item"><a href="https://wiki.gnome.org/Accessibility" target="_blank">ATK</a></span>
+            <span class="sub-item"><a href="https://wiki.gnome.org/Projects/Clutter" target="_blank">Clutter</a></span>
+        </div>
+    </div>
+    <div class="platform-item half">
+        <img class="oss-logo" src="images/open-source/xorg.svg" alt="X11">
+        <h3 class="oss-title"><a href="http://x.org/" target="_blank">X.org</a></h3>
+        <p class="oss-subtitle">Display Server &amp; Windowing System</p>
+        <div>
+            <span class="sub-item">xserver</span>
+            <span class="sub-item">XWayland</span>
+        </div>
+    </div>
+    <div class="platform-item half">
+        <img class="oss-logo" src="images/open-source/freedesktop.svg" alt="X11">
+        <h3 class="oss-title"><a href="https://freedesktop.org/" target="_blank">FreeDesktop.org</a></h3>
+        <p class="oss-subtitle">Base technology &amp; backend</p>
+        <div>
+            <span class="sub-item"><a href="https://www.freedesktop.org/wiki/Software/systemd/" target="_blank">systemd</a></span>
+            <span class="sub-item"><a href="https://gstreamer.freedesktop.org/" target="_blank">GStreamer</a></span>
+            <span class="sub-item"><a href="https://www.freedesktop.org/wiki/Software/PulseAudio/" target="_blank">PulseAudio</a></span>
+            <span class="sub-item"><a href="https://www.freedesktop.org/wiki/Software/dbus/" target="_blank">D-Bus</a></span>
+        </div>
+    </div>
+    <div class="platform-item third">
+        <img class="oss-logo" src="images/open-source/gnu.svg" alt="GNU">
+        <h3 class="oss-title"><a href="http://gnu.org/" target="_blank">GNU</a></h3>
+        <p class="oss-subtitle">Operating system</p>
+    </div>
+    <div class="platform-item two-thirds">
+        <img class="oss-logo" src="images/open-source/linux.svg" alt="Linux">
+        <h3 class="oss-title">Linux Kernel</h3>
+        <p class="oss-subtitle">Device drivers, modules</p>
+    </div>
+    <div class="platform-item full">
+        <img class="oss-logo" src="images/open-source/hardware.svg" alt="Linux">
+        <h3 class="oss-title">Hardware</h3>
     </div>
 </div>
 
