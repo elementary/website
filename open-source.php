987062ec1fbabd11d88c98ff701d7c1233ba01b0
Sam Hewitt <hewittsamuel@gmail.com>
2016-08-05 14:41:52 -0400

    tweaked focus styles for sub-item

--- open-source.php
+++ open-source.php
@@ -20,30 +20,30 @@
         <img class="oss-logo" src="images/icons/distributor-logo.svg" alt="Applications">
         <p class="oss-title">elementary OS Applications</p>
         <div>
-            <a class="sub-item button" href="https://launchpad.net/maya" target="_blank"><span>Calendar</span></a>
-            <a class="sub-item button" href="https://launchpad.net/snap-elementary" target="_blank"><span>Camera</span></a>
-            <a class="sub-item button" href="https://launchpad.net/pantheon-files" target="_blank"><span>Files</span></a>
-            <a class="sub-item button" href="https://launchpad.net/pantheon-mail" target="_blank"><span>Mail</span></a>
-            <a class="sub-item button" href="https://launchpad.net/noise" target="_blank"><span>Music</span></a>
-            <a class="sub-item button" href="https://launchpad.net/pantheon-photos" target="_blank"><span>Photos</span></a>
-            <a class="sub-item button" href="https://launchpad.net/scratch" target="_blank"><span>Scratch</span></a>
-            <a class="sub-item button" href="https://launchpad.net/pantheon-terminal" target="_blank"><span>Terminal</span></a>
-            <a class="sub-item button" href="https://launchpad.net/audience" target="_blank"><span>Videos</span></a>
+            <a class="sub-item" href="https://launchpad.net/maya" target="_blank"><span>Calendar</span></a>
+            <a class="sub-item" href="https://launchpad.net/snap-elementary" target="_blank"><span>Camera</span></a>
+            <a class="sub-item" href="https://launchpad.net/pantheon-files" target="_blank"><span>Files</span></a>
+            <a class="sub-item" href="https://launchpad.net/pantheon-mail" target="_blank"><span>Mail</span></a>
+            <a class="sub-item" href="https://launchpad.net/noise" target="_blank"><span>Music</span></a>
+            <a class="sub-item" href="https://launchpad.net/pantheon-photos" target="_blank"><span>Photos</span></a>
+            <a class="sub-item" href="https://launchpad.net/scratch" target="_blank"><span>Scratch</span></a>
+            <a class="sub-item" href="https://launchpad.net/pantheon-terminal" target="_blank"><span>Terminal</span></a>
+            <a class="sub-item" href="https://launchpad.net/audience" target="_blank"><span>Videos</span></a>
         </div>
     </div>
     <div class="platform-item full">
         <img class="oss-logo" src="images/icons/preferences-desktop-wallpaper.svg" alt="Desktop">
         <p class="oss-title">Pantheon Desktop Environment</p>
         <div>
-            <a class="sub-item button" href="https://launchpad.net/contractor" target="_blank"><span>Contractor</span></a>
-            <a class="sub-item button" href="https://launchpad.net/egtk" target="_blank"><span>eGTK</span></a>
-            <a class="sub-item button" href="https://launchpad.net/gala" target="_blank"><span>Gala</span></a>
-            <a class="sub-item button" href="https://launchpad.net/granite" target="_blank"><span>Granite</span></a>
-            <a class="sub-item button" href="https://github.com/elementary/icons" target="_blank"><span>Icons</span></a>
-            <a class="sub-item button" href="https://launchpad.net/plank" target="_blank"><span>Plank</span></a>
-            <a class="sub-item button" href="https://launchpad.net/slingshot" target="_blank"><span>Slingshot</span></a>
-            <a class="sub-item button" href="https://launchpad.net/switchboard" target="_blank"><span>Switchboard</span></a>
-            <a class="sub-item button" href="https://launchpad.net/wingpanel" target="_blank"><span>Wingpanel</span></a>
+            <a class="sub-item" href="https://launchpad.net/contractor" target="_blank"><span>Contractor</span></a>
+            <a class="sub-item" href="https://launchpad.net/egtk" target="_blank"><span>eGTK</span></a>
+            <a class="sub-item" href="https://launchpad.net/gala" target="_blank"><span>Gala</span></a>
+            <a class="sub-item" href="https://launchpad.net/granite" target="_blank"><span>Granite</span></a>
+            <a class="sub-item" href="https://github.com/elementary/icons" target="_blank"><span>Icons</span></a>
+            <a class="sub-item" href="https://launchpad.net/plank" target="_blank"><span>Plank</span></a>
+            <a class="sub-item" href="https://launchpad.net/slingshot" target="_blank"><span>Slingshot</span></a>
+            <a class="sub-item" href="https://launchpad.net/switchboard" target="_blank"><span>Switchboard</span></a>
+            <a class="sub-item" href="https://launchpad.net/wingpanel" target="_blank"><span>Wingpanel</span></a>
         </div>
     </div>
     <hr class="dotted full">
@@ -52,9 +52,9 @@
         <h3 class="oss-title">GTK+</h3>
         <p class="oss-subtitle">User Interface Toolkit</p>
         <div>
-            <a class="sub-item button" href="http://www.gtk.org/" target="_blank"><span>GTK+</span></a>
-            <a class="sub-item button" href="http://www.pango.org/" target="_blank"><span>Pango</span></a>
-            <a class="sub-item button" href="https://cairographics.org/" target="_blank"><span>Cairo</span></a>
+            <a class="sub-item" href="http://www.gtk.org/" target="_blank"><span>GTK+</span></a>
+            <a class="sub-item" href="http://www.pango.org/" target="_blank"><span>Pango</span></a>
+            <a class="sub-item" href="https://cairographics.org/" target="_blank"><span>Cairo</span></a>
         </div>
     </div>
     <div class="platform-item two-thirds">
@@ -62,12 +62,12 @@
         <h3 class="oss-title">GNOME</h3>
         <p class="oss-subtitle">Desktop Libraries</p>
         <div>
-            <a class="sub-item button" href="https://wiki.gnome.org/Accessibility" target="_blank"><span>ATK</span></a>
-            <a class="sub-item button" href="https://wiki.gnome.org/Projects/dconf" target="_blank"><span>D-Conf</span></a>
-            <a class="sub-item button" href="https://wiki.gnome.org/Projects/Clutter" target="_blank"><span>Clutter</span></a>
-            <a class="sub-item button" href="https://wiki.gnome.org/Projects/GLib" target="_blank"><span>GLib</span></a>
-            <a class="sub-item button" href="https://wiki.gnome.org/Projects/PolicyKit" target="_blank"><span>PolicyKit</span></a>
-            <a class="sub-item button" href="https://wiki.gnome.org/Projects/Vala" target="_blank"><span>Vala</span></a>
+            <a class="sub-item" href="https://wiki.gnome.org/Accessibility" target="_blank"><span>ATK</span></a>
+            <a class="sub-item" href="https://wiki.gnome.org/Projects/dconf" target="_blank"><span>D-Conf</span></a>
+            <a class="sub-item" href="https://wiki.gnome.org/Projects/Clutter" target="_blank"><span>Clutter</span></a>
+            <a class="sub-item" href="https://wiki.gnome.org/Projects/GLib" target="_blank"><span>GLib</span></a>
+            <a class="sub-item" href="https://wiki.gnome.org/Projects/PolicyKit" target="_blank"><span>PolicyKit</span></a>
+            <a class="sub-item" href="https://wiki.gnome.org/Projects/Vala" target="_blank"><span>Vala</span></a>
         </div>
     </div>
     <div class="platform-item two-thirds">
@@ -75,12 +75,12 @@
         <h3 class="oss-title">FreeDesktop.org</h3>
         <p class="oss-subtitle">Base Technology</p>
         <div>
-            <a class="sub-item button" href="https://www.freedesktop.org/wiki/Software/dbus/" target="_blank"><span>D-Bus</span></a>
-            <a class="sub-item button" href="https://gstreamer.freedesktop.org/" target="_blank"><span>GStreamer</span></a>
-            <a class="sub-item button" href="https://www.freedesktop.org/wiki/Software/libinput/" target="_blank"><span>libinput</span></a>
-            <a class="sub-item button" href="https://www.freedesktop.org/wiki/Software/lightdm/" target="_blank"><span>LightDM</span></a>
-            <a class="sub-item button" href="https://www.freedesktop.org/wiki/Software/PulseAudio/" target="_blank"><span>PulseAudio</span></a>
-            <a class="sub-item button" href="https://www.freedesktop.org/wiki/Software/systemd/" target="_blank"><span>systemd</span></a>
+            <a class="sub-item" href="https://www.freedesktop.org/wiki/Software/dbus/" target="_blank"><span>D-Bus</span></a>
+            <a class="sub-item" href="https://gstreamer.freedesktop.org/" target="_blank"><span>GStreamer</span></a>
+            <a class="sub-item" href="https://www.freedesktop.org/wiki/Software/libinput/" target="_blank"><span>libinput</span></a>
+            <a class="sub-item" href="https://www.freedesktop.org/wiki/Software/lightdm/" target="_blank"><span>LightDM</span></a>
+            <a class="sub-item" href="https://www.freedesktop.org/wiki/Software/PulseAudio/" target="_blank"><span>PulseAudio</span></a>
+            <a class="sub-item" href="https://www.freedesktop.org/wiki/Software/systemd/" target="_blank"><span>systemd</span></a>
         </div>
     </div>
     <div class="platform-item third">
@@ -88,8 +88,8 @@
         <h3 class="oss-title">X.org</h3>
         <p class="oss-subtitle">Display Server &amp; Windowing System</p>
         <div>
-            <a class="sub-item button" href="https://www.x.org/wiki/XServer/" target="_blank"><span>XServer</span></a>
-            <a class="sub-item button" href="https://wayland.freedesktop.org/xserver.html" target="_blank"><span>XWayland</span></a>
+            <a class="sub-item" href="https://www.x.org/wiki/XServer/" target="_blank"><span>XServer</span></a>
+            <a class="sub-item" href="https://wayland.freedesktop.org/xserver.html" target="_blank"><span>XWayland</span></a>
         </div>
     </div>
     <div class="platform-item half">
@@ -97,9 +97,9 @@
         <h3 class="oss-title">GNU</h3>
         <p class="oss-subtitle">Complier &amp; Core Utilities</p>
         <div>
-            <a class="sub-item button" href="https://www.gnu.org/software/bash/" target="_blank"><span>bash</span></a>
-            <a class="sub-item button" href="https://www.gnu.org/software/gcc/" target="_blank"><span>GCC</span></a>
-            <a class="sub-item button" href="https://www.gnu.org/software/coreutils/" target="_blank"><span>Coreutils</span></a>
+            <a class="sub-item" href="https://www.gnu.org/software/bash/" target="_blank"><span>bash</span></a>
+            <a class="sub-item" href="https://www.gnu.org/software/gcc/" target="_blank"><span>GCC</span></a>
+            <a class="sub-item" href="https://www.gnu.org/software/coreutils/" target="_blank"><span>Coreutils</span></a>
         </div>
     </div>
     <div class="platform-item half">
@@ -107,7 +107,7 @@
         <h3 class="oss-title">Linux</h3>
         <p class="oss-subtitle">Hardware Support &amp; drivers</p>
         <div>
-            <a class="sub-item button" href="https://kernel.org/" target="_blank"><span>Kernel</span></a>
+            <a class="sub-item" href="https://kernel.org/" target="_blank"><span>Kernel</span></a>
         </div>
     </div>
 </div>
