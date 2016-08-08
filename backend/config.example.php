8d86c0d2e983015bec9a22de99c57a43d97bd7d6
Blake Kostner <btkostner@gmail.com>
2016-08-08 14:55:43 -0700

    started api backend for new store

--- backend/config.example.php
+++ backend/config.example.php
@@ -12,6 +12,8 @@ $config = array(
 
     'slack_token' => 'asdf-1234567890-7418529630-a7854123692-8412487519',
 
+    'printful_key' => 'aaaaaaaa-bbbb-cccc:dddd-eeeeeeeeeeee',
+                      
     'twitter_consumer_key'    => 'test_ckey',
     'twitter_consumer_secret' => 'test_csecret',
     'twitter_access_token'    => 'test_atoken',
