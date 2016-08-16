ea419d919afe54ee69566907c988e26dd3433669
Lewis Goddard <lewisgoddard@eustasy.org>
2016-08-16 21:10:15 +0100

    Fix #421: Average Payments (#975)
    * Only log payments when they complete, not when they're started.
    
    * Add JSON API for Payments Data
    
    * Track payments (unsecured)
    
    * Run avg_pay
    
    * Fix URL ref
    
    * Conditional
    
    * Fix OS references
    
    * Create authenticatron.php
    
    * Update average-payments.php
    
    * Update average-payments.php
    
    * Update average-payments.php
    
    * Create authenticatron.load.php
    
    * Update index.php
    
    * Protect average-payments
    
    * Update homepage.js
    
    * Check the directory is writable.
    
    * Move everything behind the scenes
    
    * Remove older references
    
    * hidden data
    
    * Move database
    
    * remove count from returned data
    
    * Update average-payments.php
    
    * Remove duplicate tracking
    
    * Change read option
    
    * TEMP
    
    * Remove TEMP
    
    * Fix JS Issues
    
    * add some whitespace so it's easier to read
    
    * fix POST statement for payments
    
    * exit on error so we don't fall down the rabbit hole
    
    * reduce the json output to something more readable

--- backend/payment.php
+++ backend/payment.php
@@ -22,6 +22,7 @@ if (isset($_POST['token'])) {
         // Set an insecure, HTTP only cookie for 10 years in the future.
         $encoded = urlencode(str_replace(' ', '_', 'has_paid_'.$description));
         setcookie($encoded, $amount, time() + 315360000, '/', '', 0, 1);
+        require_once __DIR__.'/average-payments.php';
         echo 'OK';
     } catch(\Stripe\Error\Card $e) {
         echo 'error';
