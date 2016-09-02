f10a403aa81c9bc22439321cb5aca05c27f86e40
Lewis Goddard <lewisgoddard@eustasy.org>
2016-09-02 19:13:48 +0200

    Fix tests

--- scripts/download.js
+++ scripts/download.js
@@ -1,4 +1,4 @@
-/* global ga releaseTitle releaseVersion releaseFilename downloadLink downloadRegion stripeKey StripeCheckout WebTorrent */
+/* global ga releaseTitle releaseVersion releaseFilename downloadLink downloadRegion stripeKey StripeCheckout WebTorrent streamSaver */
 
 $(function () {
     // Set defaults
@@ -279,10 +279,10 @@ $(function () {
             clearInterval(interval)
             $('.counter').text('Complete')
             // Offer to save file.
-            /*file.getBlobURL(function (err, url) {
-                if (err) throw err
-                $('#js-save-webtorrent').removeClass('loading').addClass('suggested-action').attr('download', file.name).attr('href', url)
-            })*/
+            //file.getBlobURL(function (err, url) {
+            //    if (err) throw err
+            //    $('#js-save-webtorrent').removeClass('loading').addClass('suggested-action').attr('download', file.name).attr('href', url)
+            //})
         })
     }
 
