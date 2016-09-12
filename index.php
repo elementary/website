a5f7cfb454a7e0d47f262e949c96341c46d3e0c6
Daniel Foré <daniel@elementary.io>
2016-09-12 11:45:32 -0700

    rewrite download dialog

--- index.php
+++ index.php
@@ -369,19 +369,22 @@
         </section>
         <span id="translate-download" style="display:none;" hidden>Download elementary OS</span>
         <span id="translate-purchase" style="display:none;" hidden>Purchase elementary OS</span>
-        <div id="download-modal" class="modal">
-            <h3>Choose a Download</h3>
-            <p>For help and more info, see the <a class="read-more" href="docs/installation" target="_blank">installation guide</a></p>
-            <div class="row actions">
-                <div class="column">
-                    <a class="button close-modal" style="margin:0;" href="#">Cancel</a>
-                </div>
-                <div class="column linked">
+
+        <div id="download-modal" class="dialog">
+            <img alt="Download elementary OS icon" src="images/icons/apps/48/ubiquity.svg">
+            <div class="content-area">
+                <p class="primary">Choose a Download</p>
+                <p>Download from a localized server or by magnet link. For help and more info, see the <a class="read-more" href="docs/installation" target="_blank">installation guide</a></p>
+            </div>
+            <div class="action-area">
+                <a class="button close-modal" style="margin:0;" href="#">Cancel</a>
+                <div class="linked">
                     <a class="button suggested-action close-modal download-link http" href="<?php echo $download_link.$config['release_filename']; ?>">Download <?php echo $config['release_title']; ?></a>
                     <a class="button suggested-action close-modal download-link magnet" title="Torrent Magnet Link" href="<?php echo 'magnet:?xt=urn:btih:'.$config['release_magnet'].'&dn='.$config['release_filename']; ?>&tr=https%3A%2F%2Fashrise.com%3A443%2Fphoenix%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.ccc.de%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80%2Fannounce&ws=http:<?php echo $download_link.$config['release_filename']; ?>"><i class="fa fa-magnet"></i></a>
                 </div>
             </div>
         </div>
+
         <a style="display:none;" class="open-modal" href="#download-modal"></a>
         <!--[if lt IE 10]><script type="text/javascript" src="https://cdn.jsdelivr.net/g/classlist"></script><![endif]-->
 <?php
