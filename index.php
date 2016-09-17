2eb66960a5f0a3532d2c19ce9cd775568acd79c8
Lewis Goddard <lewisgoddard@eustasy.org>
2016-09-18 00:06:33 +0100

    Move streamsaver.js to jsdelivr

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     $page['title'] = 'Purchase elementary OS';
 
     $page['script-plugins'] = array(
-        'https://cdn.jsdelivr.net/g/jquery.leanmodal2@2.5,webtorrent@0.96.4,web-streams-polyfill@1.10',
+        'https://cdn.jsdelivr.net/g/jquery.leanmodal2@2.5,webtorrent@0.96.4,web-streams-polyfill@1.1.0,streamsaver.js@1',
         'scripts/streamSaver.min.js',
         'scripts/slider.js'
     );
