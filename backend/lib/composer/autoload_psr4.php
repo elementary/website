730f2700db9db96852fa3f041f820725ec5e32eb
Blake Kostner <btkostner@gmail.com>
2016-09-25 12:42:55 -0700

    Sentry (#1399)
    * install sentry library
    
    * add sentry configuration and install handler

--- backend/lib/composer/autoload_psr4.php
+++ backend/lib/composer/autoload_psr4.php
@@ -7,4 +7,6 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'Stripe\\' => array($vendorDir . '/stripe/stripe-php/lib'),
+    'Psr\\Log\\' => array($vendorDir . '/psr/log/Psr/Log'),
+    'Monolog\\' => array($vendorDir . '/monolog/monolog/src/Monolog'),
 );
