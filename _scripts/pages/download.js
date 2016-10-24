af65ac5fb1d6c8b02f85920a7077d73334251997
Blake Kostner <btkostner@gmail.com>
2016-10-24 13:16:46 -0700

    fix not sending OS to server on payment

--- _scripts/pages/download.js
+++ _scripts/pages/download.js
@@ -145,7 +145,7 @@ Promise.all([config, analytics, jQuery, Stripe, modal]).then(([config, ga, $, St
         // UTILITY: detectOS: Detect the OS
         function detectOS () {
             var ua = window.navigator.userAgent
-            if (ua == null || ua == false) return 'Other'
+            if (ua == null || ua === false) return 'Other'
             if (ua.indexOf('Android') >= 0) {
                 return 'Android'
             }
@@ -181,8 +181,9 @@ Promise.all([config, analytics, jQuery, Stripe, modal]).then(([config, ga, $, St
             paymentHTTP.send('description=' + encodeURIComponent(config.release.title + ' ' + config.release.version) +
                               '&amount=' + amount +
                               '&token=' + token.id +
-                              '&email=' + encodeURIComponent(token.email)) +
+                              '&email=' + encodeURIComponent(token.email) +
                               '&os=' + detectedOS
+            )
         }
 
         // ACTION: .download-http.click: Track download over HTTP
