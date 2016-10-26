88b356b3bd1d0214d07d32ba504d1571f34ea869
Blake Kostner <btkostner@gmail.com>
2016-10-26 15:19:12 -0700

    add logic for beta url config loading (#1450)

--- _scripts/config.js
+++ _scripts/config.js
@@ -10,8 +10,20 @@ import Promise from 'core-js/fn/promise'
 import jQuery from '~/lib/jquery'
 
 export default jQuery.then(($) => {
+    let configPath = '/api/config'
+
+    if (window.location.host === 'beta.elementary.io') {
+        const branch = window.location.pathname.split('/')[1]
+
+        if (branch == null || branch === '') {
+            console.error('Unable to determine branch name')
+        } else {
+            configPath = `/${branch}/api/config`
+        }
+    }
+
     return new Promise((resolve, reject) => {
-        $.getJSON('/api/config')
+        $.getJSON(configPath)
         .done((config) => {
             console.log('Sitewide configuration loaded')
             return resolve(config)
