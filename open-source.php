f42a13137f1f81ac28040b6e0384dc22ef7bae09
Daniel Foré <daniel@elementary.io>
2016-11-29 12:24:33 -0800

    start keeping track of the projects we donate to

--- open-source.php
+++ open-source.php
@@ -13,7 +13,7 @@
 
 <div class="grid">
     <div class="two-thirds">
-        <h1>Open Source</h1>
+        <h1>We're Built on Open Source</h1>
         <h4>The elementary OS platform is built upon a strong foundation of Free &amp; Open Source software. Without projects such as these, elementary OS would not exist.</h4>
         <a class="button suggested-action" href="get-involved#desktop-development">Get Involved</a>
     </div>
@@ -152,6 +152,32 @@
         <div>
             <a class="button sub-item" href="https://kernel.org/" target="_blank"><span>Kernel</span></a>
         </div>
+    </div>
+</div>
+
+<div class="grid">
+    <div class="whole">
+        <div class="two-thirds">
+            <h2>Projects We've Supported</h2>
+            <p>elementary makes a point to send funds back to the projects we rely on. When you purchase a copy of elementary OS, you're also supporting these great projects.</p>
+        </div>
+    </div>
+    <div class="donation-grid">
+        <a href="https://www.debian.org/donations" target="_blank">
+            <img class="oss-logo" src="images/open-source/debian.svg" alt="Debian">
+        </a>
+        <a href="http://fontawesome.io/community/" target="_blank">
+            <img class="oss-logo" src="images/open-source/fontawesome.svg" alt="Font Awesome">
+        </a>
+        <a href="https://inkscape.org/en/support-us/donate/" target="_blank">
+            <img class="oss-logo" src="images/open-source/inkscape.svg" alt="Inkscape">
+        </a>
+        <a href="http://www.rodsbooks.com/refind/" target="_blank">
+            <img class="oss-logo" src="images/open-source/refind.svg" alt="rEFInd">
+        </a>
+        <a href="http://wiki.osmfoundation.org/wiki/Donate" target="_blank">
+            <img class="oss-logo" src="images/open-source/osm.svg" alt="Open Street Map">
+        </a>
     </div>
 </div>
 
