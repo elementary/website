9a9753f708c008a8b7d692cd9246178933e59bee
Blake Kostner <btkostner@gmail.com>
2017-02-16 19:34:44 -0700

    inline press svg images

--- index.php
+++ index.php
@@ -23,15 +23,7 @@
         <section class="section--hero section--stretched">
             <div class="section__detail grid">
                 <div class="whole">
-                    <div id="logotype">
-
-                        <?php
-                            // Embed the SVG to fix scaling in WebKit 1.x,
-                            // while preserving CSS options for the image.
-                            include __DIR__.'/images/logotype-os.svg';
-                        ?>
-
-                    </div>
+                    <div id="logotype"><?php include __DIR__.'/images/logotype-os.svg'; ?></div>
                     <h4>A fast and open replacement for Windows and macOS</h4>
                 </div>
             </div>
@@ -74,18 +66,18 @@
                 </div>
             </div>
         </section>
-        <section class="grid">
-            <h4 id="the-press">What the press is saying about elementary OS:</h4>
+        <section class="grid" id="the-press">
+            <h4>What the press is saying about elementary OS:</h4>
             <div class="third">
-                <a href="http://www.wired.com/2013/11/elementaryos/" target="_blank"><img class="h1" src="images/thirdparty-logos/wired.svg" data-l10n-off alt="WIRED" /></a>
+                <a href="http://www.wired.com/2013/11/elementaryos/" target="_blank"><?php include __DIR__.'/images/thirdparty-logos/wired.svg'; ?></a>
                 <a class="inline-tweet" href="http://twitter.com/home/?status=&ldquo;elementary OS is different… a beautiful and powerful operating system that will run well even on old PCs&rdquo; — @WIRED http://elementary.io" data-tweet-suffix=" — @WIRED http://elementary.io" target="_blank">&ldquo;elementary OS is different… a beautiful and powerful operating system that will run well even on old PCs&rdquo;</a>
             </div>
             <div class="third">
-                <a href="https://web.archive.org/web/20150312112222/http://www.maclife.com/article/columns/future_os_x_may_be_more_elementary_ios_7" target="_blank"><img class="h1" src="images/thirdparty-logos/maclife.svg" data-l10n-off alt="Mac|Life" /></a>
+                <a href="https://web.archive.org/web/20150312112222/http://www.maclife.com/article/columns/future_os_x_may_be_more_elementary_ios_7" target="_blank"><?php include __DIR__.'/images/thirdparty-logos/maclife.svg'; ?></a>
                 <a class="inline-tweet" href="http://twitter.com/home/?status=&ldquo;a fast, low-maintenance platform that can be installed virtually anywhere&rdquo; —@MacLife http://elementary.io" data-tweet-suffix=" — @MacLife http://elementary.io" target="_blank">&ldquo;a fast, low-maintenance platform that can be installed virtually anywhere&rdquo;</a>
             </div>
             <div class="third">
-                <a href="http://lifehacker.com/how-to-move-on-after-windows-xp-without-giving-up-your-1556573928" target="_blank"><img class="h1" src="images/thirdparty-logos/lifehacker.svg" data-l10n-off alt="Lifehacker" /></a>
+                <a href="http://lifehacker.com/how-to-move-on-after-windows-xp-without-giving-up-your-1556573928" target="_blank"><?php include __DIR__.'/images/thirdparty-logos/lifehacker.svg'; ?></a>
                 <a class="inline-tweet" href="http://twitter.com/home/?status=&ldquo;Lightweight and fast… Completely community-based, and has a real flair for design and appearances.&rdquo; —@lifehacker http://elementary.io" data-tweet-suffix=" — @lifehacker http://elementary.io" target="_blank">&ldquo;Lightweight and fast… Completely community-based, and has a real flair for design and appearances.&rdquo;</a>
             </div>
         </section>
@@ -136,7 +128,7 @@
                     <div class="app-display">
                         <img class="app-display__image" src="images/screenshots/music.jpg" alt="music screenshot" />
                         <div class="app-display__description">
-                            <img src="images/icons/apps/64/multimedia-audio-player.jpg" alt="music icon" />
+                            <img src="images/icons/apps/64/multimedia-audio-player.svg" alt="music icon" />
                             <div>
                                 <h2>Music</h2>
                                 <p>Organize and listen to your music. Browse by albums, use lightning-fast search, and build playlists of your favorites.</p>
