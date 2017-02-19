538872a6d5dd3b5c08831539bbf84af9421b2864
Blake Kostner <btkostner@gmail.com>
2017-02-18 19:09:33 -0700

    add payment class for common stripe functions (#1532)
    * add payment class for common stripe functions
    
    * add array around token for download page
    
    * update translations (#1531)
    
    * Fix webpack compress error

---
+++ _scripts/widgets/payment.js
@@ -0,0 +1,100 @@
+/**
+ * _scripts/widgets/payment.js
+ * Handles transactions to Stripe
+ *
+ * @exports {Class} default - A purchase class for Stripe transactions
+ */
+
+import Promise from 'core-js/fn/promise'
+
+import { language } from '~/page'
+import config from '~/config'
+import Stripe from '~/lib/stripe'
+
+// All two letter langauge codes that Stripe currently supports
+const supportedLanguages = [
+    'da',
+    'de',
+    'en',
+    'es',
+    'fi',
+    'fr',
+    'it',
+    'ja',
+    'nl',
+    'no',
+    'sv',
+    'zh'
+]
+
+export default class Payment {
+
+    /**
+     * Payment
+     * Creates a new Payment class
+     *
+     * @param {String} desc - payment description
+     */
+    constructor (desc) {
+        this.description = desc
+
+        // This is used to store user details passed to Stripe, like an email
+        // address or phone number.
+        this.user = {}
+
+        // Start downloading the needed items when we construct the Payment class
+        config.then()
+        Stripe.then()
+    }
+
+    /**
+     * get language
+     * Finds the stripe locale setting
+     *
+     * @return {String} - two letter language code, or 'auto'
+     */
+    get language () {
+        let htmlLanguage = language()
+        if (htmlLanguage == null) htmlLanguage = ''
+
+        if (htmlLanguage.length > 2) {
+            htmlLanguage = htmlLanguage.substring(0, 2)
+        }
+
+        if (supportedLanguages.indexOf(htmlLanguage) !== -1) {
+            return htmlLanguage
+        } else {
+            return 'auto'
+        }
+    }
+
+    /**
+     * checkout
+     * Opens the Stripe checkout dialog
+     *
+     * @param {Number} amount - the payment amount
+     * @param {String} currency - Three letter currency code
+     * @returns {Promise} - a promise of token information
+     */
+    checkout (amount, currency) {
+        return Promise.all([config, Stripe])
+        .then(([config, stripe]) => {
+            return new Promise((resolve) => {
+                stripe.open(Object.assign(this.user, {
+                    token: (token, opts) => resolve([token, opts]),
+
+                    key: config.keys.stripe,
+                    name: 'elementary LLC.',
+                    description: this.description,
+                    locale: this.language,
+
+                    bitcoin: true,
+                    alipay: false,
+
+                    currency,
+                    amount
+                }))
+            })
+        })
+    }
+}
