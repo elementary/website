5603637e0f0e5a9809a773ab4ee35faf25267cb2
Lewis Goddard <lewisgoddard@eustasy.org>
2017-08-21 23:54:35 +0100

    Update highlight.js

--- _scripts/lib/highlight.js
+++ _scripts/lib/highlight.js
@@ -8,7 +8,7 @@
 import Script from 'scriptjs'
 
 export default new Promise((resolve, reject) => {
-    Script('https://cdn.jsdelivr.net/combine/gh/eustasy/highlight.js@9/highlight.min.js,gh/eustasy/highlight.js@9/languages/vala.min.js', () => {
+    Script('https://cdn.jsdelivr.net/combine/gh/eustasy/highlight.js@latest/highlight.min.js,gh/eustasy/highlight.js@latest/languages/vala.min.js', () => {
         console.log('highlight.js loaded')
         return resolve(window.hljs)
     })
