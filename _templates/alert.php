55f5c211fd5209077299b450aaa06798120748b1
Allan Nordhøy <epost@anotheragency.no>
2018-05-08 18:47:16 +0200

    HTTPS links (#1954)
    * HTTPS link
    
    * HTTPS links
    
    * HTTPS links
    
    * Fix font awesome link

--- _templates/alert.php
+++ _templates/alert.php
@@ -12,7 +12,7 @@ if ( getenv('PHPENV') !== 'production' ) {
                 </div>
                 <div class="icon-text">
                     <h3>This is a development site.</h3>
-                    <p>You are viewing a development version of our site. Some pages here may not work or act as you expect. If you got here by accident please go to <a href="http://elementary.io">elementary.io</a>, our actual website address.</p>
+                    <p>You are viewing a development version of our site. Some pages here may not work or act as you expect. If you got here by accident please go to <a href="https://elementary.io">elementary.io</a>, our actual website address.</p>
                 </div>
             </div>
         </div>
