fe7ba70b3ea707edfba1aa7ee0a2494cf8cabb55
Cassidy James Blaede <cassidyjames@users.noreply.github.com>
2018-05-08 16:23:27 -0600

    Update thank-you.php

--- thank-you.php
+++ thank-you.php
@@ -1,18 +1,27 @@
 <?php
     require_once __DIR__.'/_backend/preload.php';
-    $page['title'] = 'Thank You for Downloading';
+    $page['title'] = 'Thank You for Downloading elementary OS';
     $page['theme-color'] = '#3E4E54';   
     
     include $template['header'];
     include $template['alert'];
 ?>
 
+<div class="row">
+    <h1>
+        Thank You for Downloading
+        <?php
+            // Embed the SVG to fix scaling in WebKit 1.x,
+            // while preserving CSS options for the image.
+            include __DIR__.'/images/logotype-os.svg';
+        ?>
+    </h1>
+</div>
+
 <section class="hero">
     <div class="grid">
-        <img src="<?php echo $sitewide['lang-root'];?>images/brand/logomark.png" alt="logomark">
-        <h1>Thank you for downloading!</sup></h1>
-        <h4>For help and more info, please see the installation guide</h4>
-        <a class="button suggested-action" href="<?php echo $sitewide['lang-root'];?>/docs/installation#installation">Read the installation guide</a>
+        <h4>For help and more info, read the <a href="<?php echo $sitewide['lang-root'];?>/docs/installation#installation">installation guide</a>. If you purchased elementary OS, check your email for a receipt that includes your link to download elementary OS again for free.</h4>
+        <a class="button suggested-action" href="<?php echo $sitewide['lang-root'];?>/docs/installation#installation">Read Installation Guide</a>
     </div>
 </section>
 
