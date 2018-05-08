0801a21a069887b38de886334993742d1b61ed71
Cassidy James Blaede <cassidyjames@users.noreply.github.com>
2018-05-08 16:47:39 -0600

    Update thank-you.php

--- thank-you.php
+++ thank-you.php
@@ -7,21 +7,20 @@
     include $template['alert'];
 ?>
 
-<div class="row">
-    <h1>
-        Thank You for Downloading
-        <?php
-            // Embed the SVG to fix scaling in WebKit 1.x,
-            // while preserving CSS options for the image.
-            include __DIR__.'/images/logotype-os.svg';
-        ?>
-    </h1>
-</div>
-
 <section class="hero">
     <div class="grid">
-        <h4>For help and more info, read the <a href="<?php echo $sitewide['lang-root'];?>/docs/installation#installation">installation guide</a>. If you purchased elementary OS, check your email for a receipt that includes your link to download elementary OS again for free.</h4>
-        <a class="button suggested-action" href="<?php echo $sitewide['lang-root'];?>/docs/installation#installation">Read Installation Guide</a>
+        <div class="two-thirds">
+            <h1>
+                Thank You for Downloading
+                <?php
+                    // Embed the SVG to fix scaling in WebKit 1.x,
+                    // while preserving CSS options for the image.
+                    include __DIR__.'/images/logotype-os.svg';
+                ?>
+            </h1>
+            <p>For help and more info, read the <a href="<?php echo $sitewide['lang-root'];?>/docs/installation#installation">installation guide</a>. If you purchased elementary OS, check your email for a receipt that includes your link to download elementary OS again for free.</p>
+            <a class="button suggested-action" href="<?php echo $sitewide['lang-root'];?>/docs/installation#installation">Read Installation Guide</a>
+        </div>
     </div>
 </section>
 
