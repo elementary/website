66e8e7be9968e8428a87ec72287414c1187c8c5e
Ryo Nakano <26003928+ryonakano@users.noreply.github.com>
2018-06-12 20:05:24 +0900

    Fix #1688: Change a alt tag text 'Text Editor' to 'Scratch'

--- support.php
+++ support.php
@@ -65,7 +65,7 @@ include $template['alert'];
     </a>
 
     <a class="app" href="https://elementaryos.stackexchange.com/questions/tagged/scratch" target="_blank" rel="noopener">
-        <img width="64" height="64" src="images/icons/apps/64/accessories-text-editor.svg" alt="Text Editor"/>
+        <img width="64" height="64" src="images/icons/apps/64/accessories-text-editor.svg" alt="Scratch"/>
         <span>Scratch</span>
     </a>
 
