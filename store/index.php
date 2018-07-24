f8db79a94204743ae8bee84cb0157bfd82a5b89e
Ryo Nakano <26003928+ryonakano@users.noreply.github.com>
2018-07-24 17:54:52 +0900

    Use fa-4x class instead of adding a new class

--- store/index.php
+++ store/index.php
@@ -45,7 +45,7 @@
     <div class="row alert warning">
         <div class="column alert">
             <div class="icon">
-                <i class="warning fas fa-64 fa-exclamation-triangle"></i>
+                <i class="warning fas fa-4x fa-exclamation-triangle"></i>
             </div>
             <div class="icon-text">
                 <h3>You are missing API keys</h3>
