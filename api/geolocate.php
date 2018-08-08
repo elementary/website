5080e47bb49f87b54fb03b4e04e21c35fd1e3e02
Lewis Goddard <lewisgoddard@eustasy.org>
2018-08-08 12:51:26 +0100

    Spaces everywhere

--- api/geolocate.php
+++ api/geolocate.php
@@ -1,6 +1,6 @@
 <?php
 
-////	API for Geolocationg
+////    API for Geolocating
 
 // Option 1. Download
 // Parameters:
@@ -10,7 +10,7 @@
 //     "ip": "92.26.51.149",
 //     "download": {
 //         "region": "ams3",
-//         "download_link": "\/\/ams3.dl.elementary.io\/download\/MTUxNjMxNTg4Mg==\/"
+//         "timecode": "MTUzMzcyODk1MA=="
 //     }
 // }
 
@@ -46,52 +46,53 @@ if (!empty($_SERVER['HTTP_CF_CONNECTING_IP'])) {
 require_once __DIR__.'/../_backend/classify.functions.php';
 
 if ( isset($_GET['download']) ) {
-	$region = getDownloadRegion($ip);
-	if ( is_array($region) ) {
-	    $hash = getIPHash($ip);
-	    $region = $region[$hash];
-	}
-	date_default_timezone_set('UTC');
-	$download_link = '//'.$region.'.dl.elementary.io/download/'.base64_encode(time()).'/';
-	$result = array(
-	    'ip' => $ip,
-	    'download' => array(
-	        'region' => $region,
-	        'download_link' => $download_link,
-	    ),
-	);
-	echo json_encode($result);
+    $region = getDownloadRegion($ip);
+    if ( is_array($region) ) {
+        $hash = getIPHash($ip);
+        $region = $region[$hash];
+    }
+    date_default_timezone_set('UTC');
+    $timecode = base64_encode(time());
+    //$download_link = '//'.$region.'.dl.elementary.io/download/'.$timecode.'/';
+    $result = array(
+        'ip' => $ip,
+        'download' => array(
+            'region' => $region,
+            'timecode' => $timecode,
+        ),
+    );
+    echo json_encode($result);
 } else if ( isset($_GET['shipping']) ) {
-	$estimatedAddress = getCurrentLocation($ip);
+    $estimatedAddress = getCurrentLocation($ip);
 
-	$result = array(
-	    'ip' => $ip,
-	    'shipping' => array(
-			'address' => $estimatedAddress,
-		),
-	);
+    $result = array(
+        'ip' => $ip,
+        'shipping' => array(
+            'address' => $estimatedAddress,
+        ),
+    );
 
-	if ( !empty($_GET['item']) ) {
-		require_once __DIR__.'/../_backend/store/address.php';
-		require_once __DIR__.'/../_backend/store/api.php';
-		$address = new \Store\Address\Address();
-	    $address->set_line1('');
-	    $address->set_country($estimatedAddress['countryCode']);
-		$address->set_state($estimatedAddress['stateCode']);
-	    $address->set_city($estimatedAddress['city']);
-		$address->set_postal($estimatedAddress['postcode']);
-		$items = array(
-			array (
-				'quantity' => 1,
-		 		'variant_id' => $_GET['item'],
-			),
-		);
-		$result['shipping']['estimates'] = Store\Api\get_shipping($address, $items);
-	}
+    if ( !empty($_GET['item']) ) {
+        require_once __DIR__.'/../_backend/store/address.php';
+        require_once __DIR__.'/../_backend/store/api.php';
+        $address = new \Store\Address\Address();
+        $address->set_line1('');
+        $address->set_country($estimatedAddress['countryCode']);
+        $address->set_state($estimatedAddress['stateCode']);
+        $address->set_city($estimatedAddress['city']);
+        $address->set_postal($estimatedAddress['postcode']);
+        $items = array(
+            array (
+                'quantity' => 1,
+                 'variant_id' => $_GET['item'],
+            ),
+        );
+        $result['shipping']['estimates'] = Store\Api\get_shipping($address, $items);
+    }
 
-	echo json_encode($result, JSON_PRETTY_PRINT);
+    echo json_encode($result, JSON_PRETTY_PRINT);
 
 } else {
-	$result = array ('error' => 'No parameters were supplied, but some were expected.');
-	echo json_encode($result, JSON_PRETTY_PRINT);
+    $result = array ('error' => 'No parameters were supplied, but some were expected.');
+    echo json_encode($result, JSON_PRETTY_PRINT);
 }
