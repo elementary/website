1c2b5f025c8742340183aeaa4dac8a2fa3478f68
Cassidy James Blaede <cassidyjames@users.noreply.github.com>
2018-08-22 16:00:14 -0600

    Update Slack IDs
    Some people have become active again, and some new members should be under community

--- _backend/Service/Slack.php
+++ _backend/Service/Slack.php
@@ -20,10 +20,7 @@ class Slack
         'USLACKBOT', // slackbot
         'U0299PY5U', // David Gomes (Fix title!)
         'U0299C8QT', // teemperor (Fix title!)
-        'U02RCLE6C', // Gero (Remove "Elementary" from title)
         'U028XTDHM', // Kiran (inactive)
-        'U02AEMYH1', // Tom (inactive)
-        'U02AENUA1', // Mario (inactive)
     );
 
     /**
@@ -44,6 +41,10 @@ class Slack
         'U1DTKMUK1', // jancborchardt
         'U1E0QSPB2', // mhall119
         'U0886H1TM', // robert.ancell
+        'U9VND281F', // sarahpandabeara
+        'U9M4H3N9K', // shpurk
+        'U9W6C2SJF', // Carl Richell
+        'UBQ57D4LQ', // TingPing
     );
 
     /**
