63a770af72dca699269eb3835526ae4d1f194f31
Lewis Goddard <lewisgoddard@eustasy.org>
2018-11-05 21:35:09 +0000

    Add warning

--- previous.php
+++ previous.php
@@ -14,8 +14,19 @@
         include $template['alert'];
 ?>
 
-<section class="hero">
+<section>
     <div class="grid">
+        <div class="row alert warning">
+            <div class="column alert">
+                <div class="icon">
+                    <i class="warning fas fa-4x fa-exclamation-triangle"></i>
+                </div>
+                <div class="icon-text">
+                    <h3>The release you are downloading is outdated.</h3>
+                    <p>You are attempting to download an outdated release of elementary OS that you have purchased. For the latest updates and extended support please go to <a href="https://elementary.io">elementary.io</a></p>
+                </div>
+            </div>
+        </div>
         <div class="two-thirds">
             <h1>
                 Thank You for Downloading
@@ -32,7 +43,6 @@
                     <a class="button suggested-action download-link magnet" title="Torrent Magnet Link" href="<?php echo 'magnet:?xt=urn:btih:'.$config['previous_magnet'].'&dn='.$config['previous_filename']; ?>&tr=https%3A%2F%2Fashrise.com%3A443%2Fphoenix%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.ccc.de%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80%2Fannounce&ws=http:<?php echo $download_link.$config['previous_filename']; ?>"><i class="fa fa-magnet"></i></a>
                 </div>
             </div>
-            <!-- TODO Add warning about outdated version -->
             <p>For help and more info, read the <a href="<?php echo $sitewide['lang-root'];?>/docs/installation#installation">installation guide</a>. If you purchased elementary OS, check your email for a receipt that includes your link to download elementary OS again for free.</p>
             <a class="button suggested-action" href="<?php echo $sitewide['lang-root'];?>/docs/installation#installation">Read Installation Guide</a>
         </div>
