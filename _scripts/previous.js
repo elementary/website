e029f3c267a537f68444b817fbf647bb4e01c94f
Lewis Goddard <lewisgoddard@eustasy.org>
2018-12-04 04:39:37 +0000

    Update previous.js

--- _scripts/previous.js
+++ _scripts/previous.js
@@ -11,7 +11,29 @@ import config from '~/config'
 
 Promise.all([config, jQuery]).then(([config, $]) => {
     $(document).ready(() => {
-    
+        // UTILITY: detectOS: Detect the OS
+        function detectOS () {
+            var ua = window.navigator.userAgent
+            if (ua == null || ua === false) return 'Other'
+            if (ua.indexOf('Android') >= 0) {
+                return 'Android'
+            }
+            if (ua.indexOf('Mac OS X') >= 0 && ua.indexOf('Mobile') >= 0) {
+                return 'iOS'
+            }
+            if (ua.indexOf('Windows') >= 0) {
+                return 'Windows'
+            }
+            if (ua.indexOf('Mac_PowerPC') >= 0 || ua.indexOf('Macintosh') >= 0) {
+                return 'macOS'
+            }
+            if (ua.indexOf('Linux') >= 0) {
+                return 'Linux'
+            }
+            return 'Unknown'
+        }
+        var detectedOS = detectOS()
+
         // ACTION: .download-http.click: Track download over HTTP
         $('.download-link').click(function () {
             ga('send', 'event', config.previous.title + ' ' + config.previous.version + ' Download (OS)', 'Homepage', detectedOS)
