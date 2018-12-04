c6d74126a488dfefb4005b67fbacec958ac7bb7c
Lewis Goddard <lewisgoddard@eustasy.org>
2018-12-04 04:33:17 +0000

    Create previous.js

---
+++ _scripts/previous.js
@@ -0,0 +1,29 @@
+/**
+ * _scripts/pages/previous.js
+ * Tracking for download links
+ */
+
+/* global ga */
+
+import jQuery from '~/lib/jquery'
+
+import config from '~/config'
+
+Promise.all([config]).then(([config]) => {
+    $(document).ready(() => {
+    
+        // ACTION: .download-http.click: Track download over HTTP
+        $('.download-link').click(function () {
+            ga('send', 'event', config.previous.title + ' ' + config.previous.version + ' Download (OS)', 'Homepage', detectedOS)
+            ga('send', 'event', config.previous.title + ' ' + config.previous.version + ' Download (Region)', 'Homepage', config.user.region)
+        })
+        $('.download-link.http').click(function () {
+            ga('send', 'event', config.previous.title + ' ' + config.previous.version + ' Download (Method)', 'Homepage', 'HTTP')
+        })
+        $('.download-link.magnet').click(function () {
+            ga('send', 'event', config.previous.title + ' ' + config.previous.version + ' Download (Method)', 'Homepage', 'Magnet')
+        })
+
+        console.log('Loaded previous.js')
+    })
+})
