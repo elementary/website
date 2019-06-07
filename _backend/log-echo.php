e3960b2edeecca640b34d591e3a8ad03a22df61e
Lewis Goddard <lewis.goddard@eustasy.org>
2019-06-07 02:45:28 +0100

    Disable sentry on non-production deployments.

--- _backend/log-echo.php
+++ _backend/log-echo.php
@@ -3,7 +3,11 @@
 require_once __DIR__ . '/bootstrap.php';
 
 // Setup sentry error logging
-if (isset($config['sentry_key']) && $config['sentry_key'] !== false) {
+if (
+    getenv('PHPENV') == 'production' &&
+    isset($config['sentry_key']) &&
+    $config['sentry_key'] !== false
+) {
     $sentry = new Raven_Client($config['sentry_key']);
     $sentry->install();
 }
