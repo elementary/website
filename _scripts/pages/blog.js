344e600829973c53a390b54c8056661321198ddf
Lewis Goddard <lewis.goddard@eustasy.org>
2019-09-07 19:57:08 +0100

    Update blog.js

--- _scripts/pages/blog.js
+++ _scripts/pages/blog.js
@@ -8,34 +8,34 @@ import jQuery from '~/lib/jquery'
 jQuery.then(($) => {
     $(function () {
         $.getJSON('https://blog.elementary.io/feed.json', function (data) {
-            var blog_contents = ''
+            var blogContents = ''
             $.each(data.posts, function (n, post) {
-                var post_contents = ''
-                post_contents += '<a class="featured with-image" href="/' + post.url + '>'
-                post_contents += '<div class="featured-image" alt="Featured image" style="background-image: url(' + post.image + ');"></div>'
-                post_contents += '<header>'
-                post_contents += '<h2>' + post.title + '</h2>'
-                post_contents += '<h3>' + post.description + '</h3>'
-                post_contents += '<div class="byline">'
-                post_contents += '<div class="avatar">'
-                post_contents += '<img '
-                post_contents += 'srcset="https://www.gravatar.com/avatar/' + post.author.gravatar + '?s=96&d=blank 2x" '
-                post_contents += 'src="https://www.gravatar.com/avatar/' + post.author.gravatar + '?s=48&d=blank" '
-                post_contents += 'alt="Avatar for ' + post.author.name + '"/>'
-                post_contents += '</div>'
-                post_contents += '<div class="author">'
-                post_contents += '<span class="name">' + post.author.name + '</span>'
-                post_contents += '</div>'
-                post_contents += '<time class="post-date" datetime="' + post.pubDate + '">' + post.pubDate + '</time>'
-                post_contents += '<span class="read-time" title="Estimated read time">'
-                post_contents += post.read_time
-                post_contents += '</span>'
-                post_contents += '</div>'
-                post_contents += '</header>'
-                post_contents += '</a>'
-                blog_contents += post_contents
+                var postContents = ''
+                postContents += '<a class="featured with-image" href="/' + post.url + '>'
+                postContents += '<div class="featured-image" alt="Featured image" style="background-image: url(' + post.image + ');"></div>'
+                postContents += '<header>'
+                postContents += '<h2>' + post.title + '</h2>'
+                postContents += '<h3>' + post.description + '</h3>'
+                postContents += '<div class="byline">'
+                postContents += '<div class="avatar">'
+                postContents += '<img '
+                postContents += 'srcset="https://www.gravatar.com/avatar/' + post.author.gravatar + '?s=96&d=blank 2x" '
+                postContents += 'src="https://www.gravatar.com/avatar/' + post.author.gravatar + '?s=48&d=blank" '
+                postContents += 'alt="Avatar for ' + post.author.name + '"/>'
+                postContents += '</div>'
+                postContents += '<div class="author">'
+                postContents += '<span class="name">' + post.author.name + '</span>'
+                postContents += '</div>'
+                postContents += '<time class="post-date" datetime="' + post.pubDate + '">' + post.pubDate + '</time>'
+                postContents += '<span class="read-time" title="Estimated read time">'
+                postContents += post.read_time
+                postContents += '</span>'
+                postContents += '</div>'
+                postContents += '</header>'
+                postContents += '</a>'
+                blogContents += postContents
             })
-            $('#whats-new').html(blog_contents)
+            $('#whats-new').html(blogContents)
         })
 
         console.log('Loaded blog.js')
