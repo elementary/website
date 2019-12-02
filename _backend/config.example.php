f7dcc43f57220d8d639588ab9c4d5ef33acc3a8a
Cassidy James Blaede <cassidy@elementary.io>
2019-12-02 15:50:39 -0700

    Update config.example.php

--- _backend/config.example.php
+++ _backend/config.example.php
@@ -5,9 +5,9 @@
 $config = array(
     'release_title'    => 'Hera',
     'release_version'  => '5.1',
-    'release_filename' => 'elementaryos-5.1-stable.20191231.iso',
-    'release_size'     => '1.48 GB',
-    'release_magnet'   => 'f5b31b1bd67bf65fe97be298ec7c473cb2e3e201',
+    'release_filename' => 'elementaryos-5.1-stable.20191202.iso',
+    'release_size'     => '1.47 GB',
+    'release_magnet'   => '5b1599770c9f8c796ec8bbe096b8be545510e2c9',
 
     'previous_title'    => 'Juno',
     'previous_version'  => '5.0',
