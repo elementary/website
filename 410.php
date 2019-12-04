b25cc6741169def25c161372f7ebcdb585a9962d
Blake Kostner <btkostner@gmail.com>
2019-12-04 14:59:25 -0700

    Update 410.php
    Co-Authored-By: Cassidy James Blaede <cassidy@elementary.io>

--- 410.php
+++ 410.php
@@ -16,7 +16,7 @@
     </div>
     <div class="column alert">
         <h3>Whoops! That link has expired.</h3>
-        <p>If you’re trying to download elementary OS, please head back to <a href="/">the home page</a> for the most up-to-date version. If you were brought here from a third-party website, it is possible they were attempting to bypass the payment prompt or installation directions. Please inform them that download links are temporary, per-user, and load-balanced—the correct way to link to the download is to link to <a href="https://elementary.io">elementary.io</a>.</p>
+        <p>If you’re trying to download elementary OS, please head back to <a href="/">the home page</a> for the most up-to-date version. If you were brought here from a third-party website, it is possible they were attempting to bypass the payment prompt or installation directions. Please inform them that download links are temporary, per-user, and load-balanced. The correct way to link to the download is to link to <a href="https://elementary.io">elementary.io</a>.</p>
         <p>If you were looking for something else, feel free to <a href="https://github.com/elementary/website/issues/new">file an issue on our GitHub</a>.</p>
     </div>
     <div class="row">
