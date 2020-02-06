ae1a76b73519aee32c1c3684e73855d21aad92c4
Lewis Goddard <lewis.goddard@eustasy.org>
2020-02-06 19:31:27 +0000

    Update config.loader.php

--- _backend/config.loader.php
+++ _backend/config.loader.php
@@ -1,12 +1,17 @@
 <?php
 
+// Defaults from repository, contains actual non-secure values only. 
+require __DIR__.'/config.example.php';
+$defaultConfig = $config;
+    
+// Secure config set through ansible
 if ( is_readable(__DIR__.'/config.php') ) {
-    // for configured local
     require __DIR__.'/config.php';
+
+// Fallback to master if on testing branch
 } else if ( is_readable(__DIR__.'/../../master/_backend/config.php') ) {
-    // for configured hosted
     require __DIR__.'/../../master/_backend/config.php';
-} else {
-    // for un-configured local
-    require __DIR__.'/config.example.php';
 }
+
+// Merge configuration
+$config = array_merge($defaultConfig, $config);
