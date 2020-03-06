3fcb7f21f83b542ef2ee9b4be12c88df8d183a2d
Cassidy James Blaede <cassidy@elementary.io>
2020-03-06 13:45:53 -0700

    Get Involved: Add GitHub Sponsors, lots of design tweaks (#2323)

--- get-involved.php
+++ get-involved.php
@@ -41,7 +41,7 @@
     <div class="grid">
         <div class="two-thirds">
             <h2>Funding</h2>
-            <p class="text-center">With your help, we've been able to grow from a small group of passionate volunteers into a tiny company. Every little bit of help is one step closer to hiring another full-time contributor and tackling even more ambitious problems.</p>
+            <p class="text-center">With the help of our users and fans, we've been able to grow from a small group of passionate volunteers into a tiny but sustainable company. Every little bit of support helps us improve elementary OS and tackle even more ambitious problems.</p>
         </div>
     </div>
 
@@ -57,20 +57,23 @@
 
     <div class="grid">
         <div class="half">
-            <img class="logo" alt="Patreon" src="images/get-involved/patreon.svg">
-            <p>Patreon works like an ongoing crowdfunding campaign. Choose an amount to contribute each month to help us reach our goals. Plus, earn exclusive rewards and read exclusive content early. <a class="read-more" href="https://www.patreon.com/" target="_blank" rel="noopener">Learn More</a></p>
-            <a class="button flat" href="https://www.patreon.com/elementary" target="_blank" rel="noopener">Become a Patron</a>
+            <i class="fab fa-github" title="GitHub Sponsors"></i>
+            <p>Directly fund elementary and get a badge on your GitHub profile to show your support. GitHub Sponsors doesn't charge any fees, so it's a great way to make your contribution go farther.</p>
+            <a class="button flat suggested-action" href="https://github.com/sponsors/elementary" target="_blank" rel="noopener">Sponsor on GitHub</a>
         </div>
         <div class="half">
-            <img class="logo" alt="Bountysource" src="images/open-source/bountysource.svg">
-            <p>Directly fund the developers committing fixes or creating new features. Set a bounty on the issues that matter to you most, fund a specific app, or set up a recurring subscription. <a class="read-more" href="https://github.com/bountysource/frontend/wiki/Frequently-Asked-Questions" target="_blank" rel="noopener">Learn More</a></p>
-
-            <a class="button flat" href="https://github.com/search?utf8=%E2%9C%93&q=is%3Aopen+is%3Aissue+user%3Aelementary+label%3A%22bounty%22&type=">Bountied Bugs</a>
-            <a class="button flat suggested-action" href="https://www.bountysource.com/teams/elementary">BountySource</a>
+            <img class="logo" alt="Patreon" title="Patreon" src="images/get-involved/patreon.svg">
+            <p>Patreon works like an ongoing crowdfunding campaign. Choose an amount to contribute each month to help us reach our goals. Plus, earn exclusive rewards and read exclusive content early.</p>
+            <a class="button flat" href="https://www.patreon.com/elementary" target="_blank" rel="noopener">Back on Patreon</a>
         </div>
     </div>
 
     <div class="grid">
+        <div class="third">
+            <img class="logo" alt="Bountysource" title="Bountysource" src="images/open-source/bountysource.svg">
+            <p>Fund the developers committing fixes or new features. Set a bounty on specific issues or set up a recurring subscription.</p>
+            <a class="button flat" href="https://www.bountysource.com/teams/elementary">Support on Bountysource</a>
+        </div>
         <div class="third">
             <i class="fab fa-paypal"></i>
             <p>Easily use a debit card, credit card, or PayPal account. You can choose a one-time payment or set up recurring payments.</p>
@@ -84,11 +87,14 @@
         <div class="third">
             <img class="logo" alt="Liberapay" src="images/get-involved/liberapay_logo_black.svg">
             <p>Set up a recurring contribution through Liberapay, the open source and non-profit funding platform.</p>
-            <a class="button flat" title="Liberapay" href="https://liberapay.com/elementary/" target="_blank" rel="noopener">Donate</a>
+            <a class="button flat" title="Liberapay" href="https://liberapay.com/elementary/" target="_blank" rel="noopener">Contribute with Liberapay</a>
         </div>
-        <div class="third">
+    </div>
+
+    <div class="grid">
+        <div class="two-thirds">
             <i class="fa fa-shopping-cart"></i>
-            <p>Help us financially, plus get some exclusive elementary gear to show your support to friends, family, and coworkers.</p>
+            <p>Help us financially, plus get some exclusive elementary gear to show your support to friends, family, and coworkers. Every purchase goes towards developing elementary OS, its apps, and its services.</p>
             <a class="button flat" href="<?php echo $page['lang-root'].'store/'; ?>" target="_blank" rel="noopener">Visit Store</a>
         </div>
     </div>
@@ -123,20 +129,14 @@
         <div class="half">
             <h3>Question &amp; Answer</h3>
             <p>Stack Exchange is a Q&amp;A website built around finding the best answers to common questions. Anyone can create an account to start asking and answering.</p>
-
-            <div class="actions actions--column">
-                <a class="button flat suggested-action" href="https://elementaryos.stackexchange.com/unanswered">Unanswered Questions</a>
-                <a class="button flat" href="https://elementaryos.stackexchange.com/questions?sort=newest">New Questions</a>
-            </div>
+            <a class="button flat" href="https://elementaryos.stackexchange.com/questions?sort=newest">New Questions</a>
+            <a class="button flat suggested-action" href="https://elementaryos.stackexchange.com/unanswered">Unanswered Questions</a>
         </div>
         <div class="half">
             <h3>Documentation</h3>
             <p>elementary provides basic documentation for both users and developers. All of our documentation is written in Markdown and hosted on GitHub, so submitting a change or a new section is a piece of cake.</p>
-
-            <div class="actions actions--column">
-                <a class="button flat suggested-action" href="https://github.com/elementary/website/blob/master/docs/learning-the-basics.md" target="_blank" rel="noopener">Learning The Basics Guide</a>
-                <a class="button flat" href="https://github.com/elementary/website/blob/master/docs/code/getting-started.md" target="_blank" rel="noopener">Developer Docs</a>
-            </div>
+            <a class="button flat" href="https://github.com/elementary/website/blob/master/docs/learning-the-basics.md" target="_blank" rel="noopener">Learning the Basics Guide</a>
+            <a class="button flat" href="https://github.com/elementary/website/blob/master/docs/code/getting-started.md" target="_blank" rel="noopener">Developer Docs</a>
         </div>
     </div>
 </section>
@@ -150,10 +150,8 @@
         </div>
         <h2>Web Development</h2>
         <p>Our website is built using HTML, CSS, PHP, and JavaScript. We're always looking for people experienced in those areas who would like to contribute and make it even better.</p>
-        <div class="actions">
-            <a class="button flat suggested-action" href="https://github.com/elementary/website" target="_blank" rel="noopener">Fork Our Website on GitHub</a>
-            <a class="button flat" href="https://github.com/elementary/website/issues" target="_blank" rel="noopener">Report a Website Issue</a>
-        </div>
+        <a class="button flat" href="https://github.com/elementary/website/issues" target="_blank" rel="noopener">Report a Website Issue</a>
+        <a class="button flat suggested-action" href="https://github.com/elementary/website" target="_blank" rel="noopener">Fork the Website on GitHub</a>
     </div>
 </section>
 
@@ -161,73 +159,27 @@
     <div class="grid">
         <div class="two-thirds">
             <h2>Desktop Development</h2>
-            <p>Our desktop environment and all its apps are built using Vala, GTK+, Granite, and a number of other open libraries. We host our desktop code on GitHub, a free service for open source projects. If you've never developed for elementary OS before, we recommend you check out our Getting Started guide. <a href="/docs/code/getting-started" class="read-more">Learn More</a></p>
-            <?php
-                if ($config['chart_enable']) {
-                    echo'<p>This burndown chart shows our progress for the <a href="https://bugs.launchpad.net/'.$config['chart_link_project'].'/+milestone/'.$config['chart_link_milestone'].'">'.$config['chart_link_name'].'</a>, which is our current development focus. Soon after that milestone is reached, Loki will be publically released and work on the next version of elementary OS will begin. <a href="https://launchpad.net/elementaryos/+series" class="read-more">Learn More</a></p>';
-                }
-            ?>
+            <p>Our desktop environment and all its apps are built using Vala, GTK, Granite, and a number of other open libraries. We host all of our code and do all development on GitHub. If you've never developed for elementary OS before, we recommend you check out our Getting Started guide.</p>
+            <a class="button flat" href="https://docs.elementary.io/develop/" target="_blank" rel="noopener">Getting Started Guide</a>
+            <a class="button flat suggested-action" href="https://github.com/elementary" target="_blank" rel="noopener">Browse Code</a>
         </div>
-        <?php
-            if ($config['chart_enable']) {
-        ?>
-        <div class="whole">
-            <div class="charts">
-                <div class="barchart-ctn">
-                    <canvas width="750" height="400"></canvas>
-                </div>
-                <div class="doughnuts-ctn">
-                    <div id="date"></div>
-                    <div class="doughnut fixed">
-                        <canvas id="fixed-chart" width="90" height="90"></canvas>
-                        <div class="doughnut-label">
-                            <span class="doughnut-count">0</span><br>
-                            Fixed
-                        </div>
-                    </div>
-                    <div class="doughnut in-progress">
-                        <canvas id="in-progress-chart" width="90" height="90"></canvas>
-                        <div class="doughnut-label">
-                            <span class="doughnut-count">0</span><br>
-                            In Progress
-                        </div>
-                    </div>
-                    <div class="doughnut created">
-                        <canvas id="created-chart" width="90" height="90"></canvas>
-                        <div class="doughnut-label">
-                            <span class="doughnut-count">0</span><br>
-                            Unfixed
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <?php
-            }
-        ?>
-        <div class="whole">
-            <div class="actions">
-                <a class="button flat suggested-action" href="https://github.com/elementary" target="_blank" rel="noopener">Browse Our Code</a>
-            </div>
-        </div>
+    </div>
+    <div class="grid">
         <div class="third">
             <h3>Report issues</h3>
-            <p>Help out by tracking down issues and reporting them, or by clarifying and cleaning up existing issues. <a class="read-more" href="/docs/code/reference#reporting-bugs" target="_blank" rel="noopener">Learn More</a></p>
+            <p>Help out by tracking down issues and reporting them. Or help by confirming, clarifying, and cleaning up existing issues.</p>
+            <a class="button flat" href="/docs/code/reference#reporting-bugs">Read the Guide</a>
         </div>
         <div class="third">
             <h3>Fix issues</h3>
-            <p>Contribute to elementary OS by fixing issues, improving functionality or by implementing new features. <a class="read-more" href="/docs/code/os-dev" target="_blank" rel="noopener">Learn More</a></p>
-            <div class="actions">
-                <a class="button flat" href="https://github.com/search?utf8=%E2%9C%93&q=is%3Aopen+is%3Aissue+user%3Aelementary+label%3A%22bitesize%22&type=" target="_blank" rel="noopener">Bitesized Bugs</a>
-                <a class="button flat" href="https://github.com/search?utf8=%E2%9C%93&q=is%3Aopen+is%3Aissue+user%3Aelementary+label%3A%22bounty%22&type=" target="_blank" rel="noopener">Bountied Bugs</a>
-            </div>
+            <p>Contribute to elementary OS by fixing issues, improving functionality or implementing new features. <a class="read-more" href="/docs/code/os-dev" target="_blank" rel="noopener">Learn More</a></p>
+            <a class="button flat" href="https://github.com/search?utf8=%E2%9C%93&q=is%3Aopen+is%3Aissue+user%3Aelementary+label%3A%22bitesize%22&type=" target="_blank" rel="noopener">Bitesize Issues</a>
+            <a class="button flat" href="https://github.com/search?utf8=%E2%9C%93&q=is%3Aopen+is%3Aissue+user%3Aelementary+label%3A%22bounty%22&type=" target="_blank" rel="noopener">Bountied Issues</a>
         </div>
         <div class="third">
             <h3>Create apps</h3>
-            <p>Improve the overall elementary OS ecosystem by creating great apps. <a class="read-more" href="/docs/code/os-dev" target="_blank" rel="noopener">Learn More</a></p>
-            <div class="actions">
-                <a class="button flat suggested-action" href="https://developer.elementary.io/" target="_blank" rel="noopener">Publish on AppCenter</a>
-            </div>
+            <p>Improve the overall elementary OS ecosystem and earn money by creating great new apps for AppCenter.</p>
+            <a class="button flat" href="https://developer.elementary.io/" target="_blank" rel="noopener">Publish on AppCenter</a>
         </div>
     </div>
 </section>
@@ -236,24 +188,24 @@
     <div class="grid">
         <div class="two-thirds">
             <h2>Design</h2>
-            <p class="text-center">Our Design Team turns ideas into clear issue reports and deliverable assets. Before diving in, it's recommended to read about our design workflow. <a class="read-more" href="/docs/code/reference#proposing-design-changes" target="_blank" rel="noopener">Learn More</a></p>
+            <p class="text-center">Our design team turns ideas into clear issue reports and deliverable assets. Before diving in, it's recommended to read about our design workflow. <a class="read-more" href="/docs/code/reference#proposing-design-changes" target="_blank" rel="noopener">Learn More</a></p>
         </div>
     </div>
     <div class="design-links">
         <div>
+            <img src="images/get-involved/bugs.svg">
+            <p>Our design team tracks “Needs Design” issues in GitHub. These might need further design discussion, wireframes, or deliverable assets.</p>
+            <a class="button flat suggested-action" href="https://github.com/search?utf8=%E2%9C%93&q=is%3Aopen+is%3Aissue+user%3Aelementary+label%3A%22Needs+Design%22" target="_blank" rel="noopener">See ‘Needs Design’ Issues</a>
+        </div>
+        <div>
             <img src="images/get-involved/stylesheet.svg">
-            <p>elementary OS uses a system stylesheet written in CSS. It defines how everything is drawn, from buttons and toolbars to the panel and menus.</p>
-            <a class="button flat" href="https://github.com/elementary/stylesheet" target="_blank" rel="noopener">Browse Our Stylesheet</a>
+            <p>elementary OS uses a system stylesheet written in CSS. It defines how the interface—like buttons, toolbars, and menus—is displayed.</p>
+            <a class="button flat" href="https://github.com/elementary/stylesheet" target="_blank" rel="noopener">View Stylesheet</a>
         </div>
         <div>
             <img src="images/get-involved/icons.svg">
-            <p>elementary OS comes with a set of vector icons that are used across all pre-installed apps and are available to third party developers too.</p>
-            <a class="button flat" href="https://github.com/elementary/icons" target="_blank" rel="noopener">Browse Our Icons</a>
-        </div>
-        <div>
-            <img src="images/get-involved/bugs.svg">
-            <p>Our design team tracks "Needs Design" issues in GitHub. These might need further design discussion, wireframes, or deliverable assets.</p>
-            <a class="button flat suggested-action" href="https://github.com/search?utf8=%E2%9C%93&q=is%3Aopen+is%3Aissue+user%3Aelementary+label%3A%22Needs+Design%22" target="_blank" rel="noopener">Browse 'Needs Design' Issues</a>
+            <p>elementary OS comes with a set of vector icons that are used across all default apps and are made available to third-party apps.</p>
+            <a class="button flat" href="https://github.com/elementary/icons" target="_blank" rel="noopener">Browse Icons</a>
         </div>
     </div>
 </section>
