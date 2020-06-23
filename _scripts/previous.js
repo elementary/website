85a8b2bdc79cfe20607a8479b3664b900f7771fd
Lewis Goddard <lewis.goddard@eustasy.org>
2020-06-23 06:06:54 +0100

    Update previous.js

--- _scripts/previous.js
+++ _scripts/previous.js
@@ -11,12 +11,10 @@ import config from '~/config'
 
 Promise.all([config, jQuery]).then(([config, $]) => {
     $(document).ready(() => {
-
         // ACTION: .download-http.click: Track download over HTTP
         $('.download-link').click(function () {
             plausible(config.previous.title + ' ' + config.previous.version + ' Download of Previous Version')
         })
-
         console.log('Loaded previous.js')
     })
 })
