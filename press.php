10510ef505df4e5242938d1a64b97b2d2777a90b
Cassidy James Blaede <c@ssidyjam.es>
2020-09-17 16:49:58 -0600

    Clean up Press page

--- press.php
+++ press.php
@@ -18,7 +18,7 @@
     </div>
 </div>
 
-<div class="grid">
+<div class="grid flex">
     <div class="two-thirds">
         <h2>Join Our Press List</h2>
         <p>Be the first to know about new releases and significant developments. We send early access to press releases and press kits, including high resolution screenshots. This is a <strong>very low volume</strong> list; we send you the biggest news around once a year.</p>
@@ -54,7 +54,7 @@
                     <div style="position: absolute; left: -5000px;" aria-hidden="true">
                         <input type="text" name="b_6815d99e5893b4e213cdb00d2_142e260a91" tabindex="-1" value="">
                     </div>
-                    <div class="clear">
+                    <div class="clear text-center">
                         <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button suggested-action">
                     </div>
                 </div>
@@ -65,33 +65,41 @@
 </div>
 
 <div class="grey">
-    <div class="grid">
+    <div class="grid flex">
         <div class="two-thirds">
             <h2>elementary OS 5.1 Hera</h2>
             <p>Hera is a major update on a solid foundation. Featuring a completely redesigned login and lockscreen greeter, a new onboarding experience, new ways to sideload and install apps, major System Settings updates, improved core apps, and desktop refinements.</p>
-            <a class="button" href="https://blog.elementary.io/introducing-elementary-os-5-1-hera/" target="_blank" rel="noopener" class="read-more">Read Announcement</a>
-            <a class="button" href="https://github.com/elementary/press-kit/archive/5.1-hera.zip" target="_blank" rel="noopener" class="read-more">Download Press Kit</a>
+            <div class="text-center">
+                <a class="button" href="https://blog.elementary.io/introducing-elementary-os-5-1-hera/" target="_blank" rel="noopener" class="read-more">Read Announcement</a>
+                <a class="button" href="https://github.com/elementary/press-kit/archive/5.1-hera.zip" target="_blank" rel="noopener" class="read-more">Download Press Kit</a>
+            </div>
         </div>
     </div>
 </div>
 
-<div class="grid">
+<div class="grid flex">
     <div class="third">
         <h3>News &amp; Announcements</h3>
         <p>We share frequent updates on development, major announcements, tips for developers, featured apps, and other new content via our official blog.</p>
-        <a class="button" href="https://blog.elementary.io/" target="_blank" rel="noopener">Visit Our Blog</a>
+        <div class="text-center">
+            <a class="button" href="https://blog.elementary.io/" target="_blank" rel="noopener">Visit Our Blog</a>
+        </div>
     </div>
 
     <div class="third">
         <h3>Brand Resources</h3>
         <p>View the elementary logos, brand usage guidelines, color palette, and community logo. Plus download the official high-resolution and vector elementary logo assets.</p>
-        <a class="button" href="brand">View Brand Resources</a>
+        <div class="text-center">
+            <a class="button" href="brand">View Brand Resources</a>
+        </div>
     </div>
 
     <div class="third">
         <h3>Get in Touch</h3>
         <p>Talk directly with the team by emailing us at <a href="mailto:press@elementary.io">press@elementary.io</a>. We welcome requests for interviews, podcast appearances, or just general press inquiries.</p>
-        <a class="button" href="mailto:press@elementary.io">Send an Email</a>
+        <div class="text-center">
+            <a class="button" href="mailto:press@elementary.io">Send an Email</a>
+        </div>
     </div>
 </div>
 
