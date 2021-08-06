c7e4417efc4e4f3c62be2135671ca5aa81881129
Cassidy James Blaede <cassidy@elementary.io>
2021-08-06 16:28:33 -0600

    Use local time and format for toast (#2787)
    * Use local time and format for toast
    
    * Use local day
    
    * Consistent quotes; simplify

--- _templates/event.php
+++ _templates/event.php
@@ -9,13 +9,22 @@ require_once __DIR__ . '/../_backend/event.php';
 
 $l10n->set_domain('layout');
 
-if (event_active('odin 6.0 release')) { ?>
+const EVENT_NAME = 'odin 6.0 release';
+
+if (event_active(EVENT_NAME)) { ?>
   <div class="overlay">
     <div class="overlay__content toast">
       <div class="toast__close"><i class="fas fa-times"></i></div>
-      <span class="toast__text"><strong>Something new is coming.</strong> Check back Tuesday after 1600 UTC.</span>
+      <span class="toast__text"><strong>Something new is coming.</strong> Check back <time id="odin-day">Tues</time> after <time id="odin-time">1600 UTC</time>.</span>
     </div>
   </div>
+  <script>
+    const utcDate = new Date("<?php echo date(DATE_ISO8601, date_timestamp_get($event_expires[EVENT_NAME][1])) ?>");
+    const lang = "<?php echo $page['lang']; ?>";
+
+    document.getElementById("odin-day").innerHTML = new Intl.DateTimeFormat(lang, {weekday: "long"}).format(utcDate);
+    document.getElementById("odin-time").innerHTML = new Intl.DateTimeFormat(lang, {hour: "numeric", minute: "numeric"}).format(utcDate);
+  </script>
 
 <?php }
 
