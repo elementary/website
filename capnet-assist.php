79c077ab60dfbd9a6215bc415ae4f46c9ca7f6e7
Cassidy James Blaede <cassidy@elementary.io>
2021-12-08 13:44:49 -0700

    Capnet Assist: use typographic apostrophe

--- capnet-assist.php
+++ capnet-assist.php
@@ -13,7 +13,7 @@
 ?>
 
 <div class="row">
-		<h1>You're connected!</h1>
+		<h1>You’re connected!</h1>
 		<p>Your Internet connection appears to be working. You can safely close this window and continue using your device.</p>
 		<h2>Why is this window appearing?</h2>
 		<p>elementary OS automatically checks your Internet connection when you connect to a new Wi-Fi network. If it detects there is not an Internet connection (i.e. if you are connecting to a captive portal at a coffee shop or other public location), this window will appear and display the login page.</p>
