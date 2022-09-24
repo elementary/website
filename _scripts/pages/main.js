35e21048575ab5bba52a6a9f33cc18ceb0c155a5
Colin Kiama <colinkiama@gmail.com>
2022-09-24 22:41:00 +0100

    Fix linting errors

--- _scripts/pages/main.js
+++ _scripts/pages/main.js
@@ -3,28 +3,28 @@
  * Loads all of the site wide snippets
  */
 
-import jQuery from "~/lib/jquery";
+import jQuery from '~/lib/jquery'
 
-import "~/external-links";
-import "~/popover";
-import "~/smooth-scrolling";
+import '~/external-links'
+import '~/popover'
+import '~/smooth-scrolling'
 
 jQuery.then(($) => {
-    $(".toast__close").on("click", function (e) {
-        $(this).closest(".toast").hide();
-    });
+    $('.toast__close').on('click', function (e) {
+        $(this).closest('.toast').hide()
+    })
 });
 
 /* Can't wait for JQuery. This needs to run ASAP to prevent flashes */
 (function (body) {
-    let menuButton = body.querySelector("nav .menu-button");
-    menuButton.classList.add("enabled");
+    const menuButton = body.querySelector('nav .menu-button')
+    menuButton.classList.add('enabled')
 
-    menuButton.addEventListener("click", function (e) {
-        if (menuButton.getAttribute("aria-expanded") === "true") {
-            menuButton.setAttribute("aria-expanded", "false");
+    menuButton.addEventListener('click', function (e) {
+        if (menuButton.getAttribute('aria-expanded') === 'true') {
+            menuButton.setAttribute('aria-expanded', 'false')
         } else {
-            menuButton.setAttribute("aria-expanded", "true");
+            menuButton.setAttribute('aria-expanded', 'true')
         }
-    });
-})(document.body);
+    })
+})(document.body)
