fe37e4a8620c8c7e40bacdbd917c52eefc94e26c
David Hewitt <davidmhewitt@gmail.com>
2022-11-05 18:35:15 +0000

    Bump sentry to latest

--- _backend/log-echo.php
+++ _backend/log-echo.php
@@ -5,17 +5,15 @@ require_once __DIR__ . '/bootstrap.php';
 // Setup sentry error logging
 if (
     getenv('PHPENV') == 'production' &&
-    isset($config['sentry_key']) &&
-    $config['sentry_key'] !== false
+    isset($config['sentry_dsn']) &&
+    $config['sentry_dsn'] !== false
 ) {
-    $sentry = new Raven_Client($config['sentry_key']);
-    $sentry->install();
+    \Sentry\init(['dsn' => $config['sentry_dsn'],]);
 }
 
 // Log an error and also echo it.
 function log_echo($msg, $echo = false) {
-    global $sentry;
     error_log($msg);
-    if ( $sentry ) $sentry->captureMessage($msg);
+    \Sentry\captureMessage($msg);
     if ( $echo ) echo $msg.PHP_EOL;
 }
