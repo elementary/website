1bd74a93744ca5f97675bf059bab14ef024e1fd4
David Hewitt <davidmhewitt@users.noreply.github.com>
2022-11-07 19:35:09 +0000

    Migrate to Stripe Checkout for OS payments (#3100)

---
+++ api/create-checkout-session.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once __DIR__ . '/../_backend/bootstrap.php';
+require_once __DIR__ . '/../_backend/preload.php';
+
+$stripe = new \Stripe\StripeClient($config['stripe_sk']);
+
+if (isset($_POST['amount'])) {
+    $amount      = intval($_POST['amount']);
+    $description = $_POST['description'];
+
+    if (isset($_SERVER['HTTPS']) &&
+        ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) ||
+        isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
+        $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+        $protocol = 'https://';
+    }
+    else {
+        $protocol = 'http://';
+    }
+
+    $checkout_session = $stripe->checkout->sessions->create([
+        'line_items' => [[
+            'price_data' => [
+                'currency' => 'usd',
+                'product_data' => [
+                    'name' => $description,
+                ],
+                'unit_amount' => $amount,
+            ],
+            'quantity' => 1,
+        ]],
+
+        'mode' => 'payment',
+        'success_url' => "$protocol$_SERVER[HTTP_HOST]$sitewide[root]?checkout_session_id={CHECKOUT_SESSION_ID}",
+        'cancel_url' => "$protocol$_SERVER[HTTP_HOST]$sitewide[root]?checkout_session_id={CHECKOUT_SESSION_ID}",
+    ]);
+
+    $stripe->paymentIntents->update(
+        $checkout_session['payment_intent'],
+        ['metadata' => array(
+            'receipt' => 'false',
+            'products' => json_encode(array('ISO-' . $config['release_version']))
+        )]
+    );
+
+    header("HTTP/1.1 303 See Other");
+    header("Location: " . $checkout_session->url);
+} else {
+    echo $config['stripe_pk'];
+}
+?>
