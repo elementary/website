3925d7859664b3dbd6fc5ab854c108311ab775d3
Danielle Foré <danielle@elementary.io>
2022-11-08 08:03:28 -0800

    config

--- _backend/config.example.php
+++ _backend/config.example.php
@@ -5,18 +5,18 @@
 
 return array(
   'release_title'    => 'Jólnir',
-  'release_version'  => '6.1',
+  'release_version'  => '7',
   'release_filename' => 'elementaryos-6.1-stable.20211218-rc.iso',
   'release_size'     => '2.47 GB',
   'release_magnet'   => '79c043454fc643b05aea16de848e5dce6eb8c9d9',
   'release_sha256'   => '35c8086bc0af8ccc5ef8629225b215b1742b3a90b475f9f47723b18a315d86a7',
-  'release_faq'      => 'https://github.com/elementary/os/wiki/elementary-OS-6.1-Jólnir-FAQ',
+  'release_faq'      => 'https://github.com/elementary/os/wiki/elementary-OS-7-Jólnir-FAQ',
 
-  'previous_title'    => 'Hera',
-  'previous_version'  => '5.1.7',
-  'previous_filename' => 'elementaryos-5.1-stable.20200814.iso',
-  'previous_size'     => '1.49 GB',
-  'previous_magnet'   => '73e9c0288c0b62c2646b695219b550fd231fede4',
+  'previous_title'    => 'Jólnir',
+  'previous_version'  => '6.1',
+  'previous_filename' => 'elementaryos-6.1-stable.20211218-rc.iso',
+  'previous_size'     => '2.47 GB',
+  'previous_magnet'   => '79c043454fc643b05aea16de848e5dce6eb8c9d9',
 
   'chart_enable'         => false,
   'chart_link_project'   => 'elementary',
