75a264139132b58b616ffb1695d1d773faf6a100
Danielle Foré <daniel@elementary.io>
2023-01-30 09:39:41 -0800

    Update config.example.php

--- _backend/config.example.php
+++ _backend/config.example.php
@@ -6,10 +6,10 @@
 return array(
   'release_title'    => 'Horus',
   'release_version'  => '7',
-  'release_filename' => 'elementaryos-6.1-stable.20211218-rc.iso',
+  'release_filename' => 'elementaryos-7.0-stable.20230129rc.iso',
   'release_size'     => '2.8 GB',
-  'release_magnet'   => '79c043454fc643b05aea16de848e5dce6eb8c9d9',
-  'release_sha256'   => '35c8086bc0af8ccc5ef8629225b215b1742b3a90b475f9f47723b18a315d86a7',
+  'release_magnet'   => '',
+  'release_sha256'   => '2bdffe0b0944405e87d6612fe7879b7af770ced20346453b9ef4b6057f4dd0af',
   'release_faq'      => 'https://github.com/elementary/os/wiki/OS-7-Horus-FAQ',
 
   'previous_title'    => 'Jólnir',
