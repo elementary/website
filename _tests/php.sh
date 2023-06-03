bff1572bfa0ea5c9e496c5b015957aac5e0f6cc5
David Hewitt <davidmhewitt@gmail.com>
2023-06-03 13:21:05 +0100

    Fix lint command

--- _tests/php.sh
+++ _tests/php.sh
@@ -9,10 +9,10 @@ echo "####################"
 
 find . -path './_backend/vendor' -prune -o -name "*.php" -print0 | xargs -0 -n1 -P8 php -l
 
-cd ../_backend && ./vendor/bin/phpcs --ignore="*/vendor/*,*/node_modules/*" \
+./_backend/vendor/bin/phpcs --ignore="*/vendor/*,*/node_modules/*" \
   --exclude=Generic.Files.LineLength,PSR1.Files.SideEffects \
   --extensions=php \
-  --standard=PSR2 ../.
+  --standard=PSR2 .
 
 echo "#####################"
 echo "PHP linting complete!"
