f6544c1b61c6cdf5cc6d80682f8fcb6b8488b7e5
Lewis Goddard <lewisgoddard@eustasy.org>
2025-03-17 19:28:46 +0000

    Update config.example.php

--- _backend/config.example.php
+++ _backend/config.example.php
@@ -8,7 +8,7 @@ return array(
   'release_version'  => '8.0.1',
   'release_filename' => 'elementaryos-8.0-stable.20250314rc.iso',
   'release_size'     => '3.3 GB',
-  'release_magnet'   => '',
+  'release_magnet'   => 'ca69606c5767ec131cc6d6618885b8920ca3f8e8',
   'release_sha256'   => 'fb9f6bbbba8c2b4dd9d11de2c072b46fc35341b8c0a44ee323c095e865c47551',
   'release_faq'      => 'https://github.com/elementary/os/wiki/OS-8-FAQ',
 
