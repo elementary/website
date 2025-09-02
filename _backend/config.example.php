72a92e458b9e2decb4074e5f33909ac7be6abe72
Danielle Foré <danielle@elementary.io>
2025-09-02 10:18:36 -0700

    Update config.example.php

--- _backend/config.example.php
+++ _backend/config.example.php
@@ -6,10 +6,10 @@
 return array(
   'release_title'    => 'Circe',
   'release_version'  => '8.0.2',
-  'release_filename' => 'elementaryos-8.0-stable.20250801rc.iso',
-  'release_size'     => '3.7 GB',
-  'release_magnet'   => '9fc5fc91c60d5cd9b3979105fc7470a3489840c2',
-  'release_sha256'   => 'ebcb36a3889bb45ba9f73e54b0412b41e0ace9374138e23649e462b26e644c85',
+  'release_filename' => 'elementaryos-8.0-stable-amd64.20250902rc.iso',
+  'release_size'     => '3.5 GB',
+  'release_magnet'   => '19e3fa024c654d4e644a77dd1bb43756fd905ae7',
+  'release_sha256'   => 'a8c22b3cfd4b2432e74a1db5894b433f76cad130c1f41ee1f0d2e98a5c48e02e',
   'release_faq'      => 'https://github.com/elementary/os/wiki/OS-8-FAQ',
 
   'previous_title'    => 'Circe',
