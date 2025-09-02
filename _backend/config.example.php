cb11396cfe8ecf173d92b4eb54d11f01d4385fb3
Danielle Foré <danielle@elementary.io>
2025-09-02 09:57:33 -0700

    Update for OS 8.0.2

--- _backend/config.example.php
+++ _backend/config.example.php
@@ -5,18 +5,18 @@
 
 return array(
   'release_title'    => 'Circe',
-  'release_version'  => '8.0.1',
-  'release_filename' => 'elementaryos-8.0-stable.20250314rc.iso',
-  'release_size'     => '3.3 GB',
-  'release_magnet'   => 'ca69606c5767ec131cc6d6618885b8920ca3f8e8',
-  'release_sha256'   => 'fb9f6bbbba8c2b4dd9d11de2c072b46fc35341b8c0a44ee323c095e865c47551',
+  'release_version'  => '8.0.2',
+  'release_filename' => 'elementaryos-8.0-stable.20250801rc.iso',
+  'release_size'     => '3.7 GB',
+  'release_magnet'   => '9fc5fc91c60d5cd9b3979105fc7470a3489840c2',
+  'release_sha256'   => 'ebcb36a3889bb45ba9f73e54b0412b41e0ace9374138e23649e462b26e644c85',
   'release_faq'      => 'https://github.com/elementary/os/wiki/OS-8-FAQ',
 
   'previous_title'    => 'Circe',
-  'previous_version'  => '8',
-  'previous_filename' => 'elementaryos-8.0-stable.20241122rc.iso',
-  'previous_size'     => '3.2 GB',
-  'previous_magnet'   => 'd52eea45cd83f947983192e1ce2ef723ce992fec',
+  'previous_version'  => '8.0.1',
+  'previous_filename' => 'elementaryos-8.0-stable.20250314rc.iso',
+  'previous_size'     => '3.3 GB',
+  'previous_magnet'   => 'ca69606c5767ec131cc6d6618885b8920ca3f8e8',
 
   'stripe_sk' => 'sk_test_hoigesrjgoisrhgilgjrsfjs',
   'stripe_pk' => 'pk_test_hoigesrjgoisrhgilgjrsfjs',
