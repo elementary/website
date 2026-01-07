401b331ab36c458626846b0339c3ab77efe1988a
Danielle Foré <danielle@elementary.io>
2026-01-07 09:14:19 -0800

    Update for OS 8.1 20260107
    New iso with fix for installer window stacking order

--- _backend/config.example.php
+++ _backend/config.example.php
@@ -9,13 +9,13 @@ return array(
   'release_size'     => '3.3 GB',
   'release_faq'      => 'https://github.com/elementary/os/wiki/OS-8-FAQ',
 
-  'release_filename' => 'elementaryos-8.1-stable-amd64.20251211.iso',
-  'release_magnet'   => 'addc43b83201d733d7558e431b692c10e2f53f84',
-  'release_sha256'   => 'eee6cad081664717681bec767fbfe1aa1fd920938fedad6c83b41fd341e8f306',
+  'release_filename' => 'elementaryos-8.1-stable-amd64.20260107.iso',
+  'release_magnet'   => '',
+  'release_sha256'   => '24cc821cefa2600ce54686f406e6782eafe658ce56e190dea331a60c6cd13c99',
   
-  'release_arm_filename' => 'elementaryos-8.1-stable-arm64.20251211.iso',
-  'release_arm_magnet'   => 'b52b2a0195015e351db1ac1183f5d9263db9a254',
-  'release_arm_sha256'   => '2be4ebcb8a5c60b230f0e9eab9fdaf80694a2395dc2a7b128321f8354253046a',
+  'release_arm_filename' => 'elementaryos-8.1-stable-arm64.20260107.iso',
+  'release_arm_magnet'   => '',
+  'release_arm_sha256'   => '',
 
   'previous_title'    => 'Circe',
   'previous_version'  => '8.0.2',
