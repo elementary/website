8c5e360d41e963c80071d4a250dfb9097c2a459a
Lewis Goddard <lewis.goddard@eustasy.org>
2026-01-08 14:15:54 +0000

    leave detectOS in code for usability in doc hinting

--- _scripts/page.js
+++ _scripts/page.js
@@ -40,6 +40,33 @@ export function url () {
 }
 
 /**
+ * detectedOS
+ * Returns the OS of the user
+ *
+ * @return {String} - OS of the user
+ */
+export function detectedOS () {
+    const ua = window.navigator.userAgent
+    if (ua == null || ua === false) return 'Other'
+    if (ua.indexOf('Android') >= 0) {
+        return 'Android'
+    }
+    if (ua.indexOf('Mac OS X') >= 0 && ua.indexOf('Mobile') >= 0) {
+        return 'iOS'
+    }
+    if (ua.indexOf('Windows') >= 0) {
+        return 'Windows'
+    }
+    if (ua.indexOf('Mac_PowerPC') >= 0 || ua.indexOf('Macintosh') >= 0) {
+        return 'macOS'
+    }
+    if (ua.indexOf('Linux') >= 0) {
+        return 'Linux'
+    }
+    return 'Unknown'
+}
+
+/**
  * detectedArchitecture
  * Returns the architecture of the user's device using User-Agent Client Hints API
  *
