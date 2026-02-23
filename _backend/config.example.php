4796bce0b20682e089476a0a509d8ea96a72b6b7
Lewis Goddard <lewisgoddard@eustasy.org>
2026-02-23 16:12:07 +0000

    Add magnet links for release and ARM versions

--- _backend/config.example.php
+++ _backend/config.example.php
@@ -10,11 +10,11 @@ return array(
   'release_faq'      => 'https://github.com/elementary/os/wiki/OS-8-FAQ',
 
   'release_filename' => 'elementaryos-8.1-stable-amd64.20260219.iso',
-  'release_magnet'   => '',
+  'release_magnet'   => '03148319face5909b193f1f980d0bcf9139a09ec',
   'release_sha256'   => 'bda93040d08c05911fb159f8150bf8f4ef2db6567ef6e2acd197cb6f395d3446',
   
   'release_arm_filename' => 'elementaryos-8.1-stable-arm64.20260219.iso',
-  'release_arm_magnet'   => '',
+  'release_arm_magnet'   => '90b3382caff769f4c7779ef90a5ab30eedda73d4',
   'release_arm_sha256'   => '85116d48c406ae7cd60c936050a099d4b8610321273f6f0a694796db4d4e86ba',
 
   'previous_title'    => 'Circe',
