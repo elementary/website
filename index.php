34ec3ec9852029855a8dbbc90bb77fde24affa65
Lewis Goddard <lewisgoddard@eustasy.org>
2026-09-10 12:04:00 +0100

    Update torrent magnet links to new tracker URL

--- index.php
+++ index.php
@@ -481,7 +481,7 @@ include $template['alert'];
             <h3 class="dialog-option-title">ARM® 64-bit</h3>
             <p class="dialog-option-subtitle">Apple Silicon, Raspberry Pi, etc</p>
           </div>
-          <a class="button download-link magnet arm" title="Torrent Magnet Link" href="<?php echo 'magnet:?xt=urn:btih:'.$config['release_arm_magnet'].'&dn='.$config['release_arm_filename']; ?>&tr=https%3A%2F%2Fashrise.com%3A443%2Fphoenix%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&ws=<?php echo urlencode($download_link_arm.$config['release_arm_filename']); ?>&xs=<?php echo urlencode($download_link_arm.$config['release_arm_filename'].'.torrent'); ?>"><i class="fa fa-magnet"></i></a>
+          <a class="button download-link magnet arm" title="Torrent Magnet Link" href="<?php echo 'magnet:?xt=urn:btih:'.$config['release_arm_magnet'].'&dn='.$config['release_arm_filename']; ?>&tr=https%3A%2F%2Ftracker.ashrise.com%2Fannounce&ws=<?php echo urlencode($download_link_arm.$config['release_arm_filename']); ?>&xs=<?php echo urlencode($download_link_arm.$config['release_arm_filename'].'.torrent'); ?>"><i class="fa fa-magnet"></i></a>
           <a class="button download-link http arm" href="<?php echo $download_link_arm.$config['release_arm_filename']; ?>">Download</a>
         </div>
         <div class="dialog-option">
@@ -489,7 +489,7 @@ include $template['alert'];
             <h3 class="dialog-option-title">Intel or AMD 64-bit</h3>
             <p class="dialog-option-subtitle">Most PCs and older Macs</p>
           </div>
-          <a class="button download-link magnet x86" title="Torrent Magnet Link" href="<?php echo 'magnet:?xt=urn:btih:'.$config['release_magnet'].'&dn='.$config['release_filename']; ?>&tr=https%3A%2F%2Fashrise.com%3A443%2Fphoenix%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&ws=<?php echo urlencode($download_link_x86.$config['release_filename']); ?>&xs=<?php echo urlencode($download_link_x86.$config['release_filename'].'.torrent'); ?>"><i class="fa fa-magnet"></i></a>
+          <a class="button download-link magnet x86" title="Torrent Magnet Link" href="<?php echo 'magnet:?xt=urn:btih:'.$config['release_magnet'].'&dn='.$config['release_filename']; ?>&tr=https%3A%2F%2Ftracker.ashrise.com%2Fannounce&ws=<?php echo urlencode($download_link_x86.$config['release_filename']); ?>&xs=<?php echo urlencode($download_link_x86.$config['release_filename'].'.torrent'); ?>"><i class="fa fa-magnet"></i></a>
           <a autofocus class="button download-link http x86" href="<?php echo $download_link_x86.$config['release_filename']; ?>">Download</a>
         </div>
       </div>
